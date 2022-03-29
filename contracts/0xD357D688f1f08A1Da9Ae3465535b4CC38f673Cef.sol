7f34daac18b02a4539f67875965dcd420d60ee77
bot <abagoforgans@users.noreply.github.com>
2022-03-29 11:57:47 +0000

    autocommit

---
+++ contracts/0xD357D688f1f08A1Da9Ae3465535b4CC38f673Cef.sol
@@ -0,0 +1,277 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+mapping of uint256 balanceOf;
+mapping of uint256 allowance;
+uint256 totalSupply;
+array of uint256 name;
+array of uint256 symbol;
+uint8 decimals;
+address governanceAddress; offset 8
+address sFI_minterAddress;
+uint256 MAX_TOKENS;
+
+function name() payable {
+    return name[0 len name.length]
+}
+
+function totalSupply() payable {
+    return totalSupply
+}
+
+function decimals() payable {
+    return decimals
+}
+
+function governance() payable {
+    return governanceAddress
+}
+
+function balanceOf(address arg1) payable {
+    require calldata.size - 4 >= 32
+    return balanceOf[address(arg1)]
+}
+
+function symbol() payable {
+    return symbol[0 len symbol.length]
+}
+
+function SFI_minter() payable {
+    return sFI_minterAddress
+}
+
+function allowance(address arg1, address arg2) payable {
+    require calldata.size - 4 >= 64
+    return allowance[address(arg1)][address(arg2)]
+}
+
+function MAX_TOKENS() payable {
+    return MAX_TOKENS
+}
+
+function _fallback() payable {
+    revert
+}
+
+function set_minter(address arg1) payable {
+    require calldata.size - 4 >= 32
+    if governanceAddress != msg.sender:
+        revert with 0, 'must be governance'
+    sFI_minterAddress = arg1
+}
+
+function set_governance(address arg1) payable {
+    require calldata.size - 4 >= 32
+    if governanceAddress != msg.sender:
+        revert with 0, 'must be governance'
+    governanceAddress = arg1
+}
+
+function approve(address arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    36,
+                    0x7345524332303a20617070726f76652066726f6d20746865207a65726f20616464726573,
+                    mem[200 len 28]
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    34,
+                    0x7345524332303a20617070726f766520746f20746865207a65726f20616464726573,
+                    mem[198 len 30]
+    allowance[address(msg.sender)][address(arg1)] = arg2
+    emit Approval(arg2, msg.sender, arg1);
+    return 1
+}
+
+function decreaseAllowance(address arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    if arg2 > allowance[address(msg.sender)][address(arg1)]:
+        revert with 0, 
+                    32,
+                    37,
+                    0x6445524332303a2064656372656173656420616c6c6f77616e63652062656c6f77207a6572,
+                    mem[165 len 27],
+                    mem[219 len 5]
+    if not msg.sender:
+        revert with 0, 32, 36, 0x7345524332303a20617070726f76652066726f6d20746865207a65726f20616464726573, mem[296 len 28]
+    if not arg1:
+        revert with 0, 32, 34, 0x7345524332303a20617070726f766520746f20746865207a65726f20616464726573, mem[294 len 30]
+    allowance[address(msg.sender)][address(arg1)] -= arg2
+    emit Approval((allowance[address(msg.sender)][address(arg1)] - arg2), msg.sender, arg1);
+    return 1
+}
+
+function mint_SFI(address arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    if sFI_minterAddress != msg.sender:
+        revert with 0, 'must be SFI_minter'
+    require ext_code.size(this.address)
+    staticcall this.address.0x18160ddd with:
+            gas gas_remaining wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    if ext_call.return_data[0] + arg2 >= MAX_TOKENS:
+        revert with 0, 'cannot mint more than MAX_TOKENS'
+    if not arg1:
+        revert with 0, 'ERC20: mint to the zero address'
+    if arg2 + totalSupply < totalSupply:
+        revert with 0, 'SafeMath: addition overflow'
+    totalSupply += arg2
+    if arg2 + balanceOf[address(arg1)] < balanceOf[address(arg1)]:
+        revert with 0, 'SafeMath: addition overflow'
+    balanceOf[address(arg1)] += arg2
+    emit Transfer(arg2, 0, arg1);
+}
+
+function increaseAllowance(address arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    if arg2 + allowance[address(msg.sender)][address(arg1)] < allowance[address(msg.sender)][address(arg1)]:
+        revert with 0, 'SafeMath: addition overflow'
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    36,
+                    0x7345524332303a20617070726f76652066726f6d20746865207a65726f20616464726573,
+                    mem[200 len 28]
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    34,
+                    0x7345524332303a20617070726f766520746f20746865207a65726f20616464726573,
+                    mem[198 len 30]
+    allowance[address(msg.sender)][address(arg1)] += arg2
+    emit Approval((arg2 + allowance[address(msg.sender)][address(arg1)]), msg.sender, arg1);
+    return 1
+}
+
+function transfer(address arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    37,
+                    0x6545524332303a207472616e736665722066726f6d20746865207a65726f20616464726573,
+                    mem[201 len 27]
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    35,
+                    0xfe45524332303a207472616e7366657220746f20746865207a65726f20616464726573,
+                    mem[199 len 29]
+    if arg2 > balanceOf[address(msg.sender)]:
+        revert with 0, 
+                    32,
+                    38,
+                    0x7345524332303a207472616e7366657220616d6f756e7420657863656564732062616c616e63,
+                    mem[166 len 26],
+                    mem[218 len 6]
+    balanceOf[address(msg.sender)] -= arg2
+    if arg2 + balanceOf[arg1] < balanceOf[arg1]:
+        revert with 0, 'SafeMath: addition overflow'
+    balanceOf[address(arg1)] = arg2 + balanceOf[arg1]
+    emit Transfer(arg2, msg.sender, arg1);
+    return 1
+}
+
+function transferFrom(address arg1, address arg2, uint256 arg3) payable {
+    require calldata.size - 4 >= 96
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    37,
+                    0x6545524332303a207472616e736665722066726f6d20746865207a65726f20616464726573,
+                    mem[201 len 27]
+    if not arg2:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    35,
+                    0xfe45524332303a207472616e7366657220746f20746865207a65726f20616464726573,
+                    mem[199 len 29]
+    if arg3 > balanceOf[address(arg1)]:
+        revert with 0, 
+                    32,
+                    38,
+                    0x7345524332303a207472616e7366657220616d6f756e7420657863656564732062616c616e63,
+                    mem[166 len 26],
+                    mem[218 len 6]
+    balanceOf[address(arg1)] -= arg3
+    if arg3 + balanceOf[arg2] < balanceOf[arg2]:
+        revert with 0, 'SafeMath: addition overflow'
+    balanceOf[address(arg2)] = arg3 + balanceOf[arg2]
+    emit Transfer(arg3, arg1, arg2);
+    if arg3 > allowance[address(arg1)][address(msg.sender)]:
+        revert with 0, 
+                    32,
+                    40,
+                    0x6c45524332303a207472616e7366657220616d6f756e74206578636565647320616c6c6f77616e63,
+                    mem[264 len 24],
+                    mem[312 len 8]
+    if not arg1:
+        revert with 0, 32, 36, 0x7345524332303a20617070726f76652066726f6d20746865207a65726f20616464726573, mem[392 len 28]
+    if not msg.sender:
+        revert with 0, 32, 34, 0x7345524332303a20617070726f766520746f20746865207a65726f20616464726573, mem[390 len 30]
+    allowance[address(arg1)][address(msg.sender)] -= arg3
+    emit Approval((allowance[address(arg1)][address(msg.sender)] - arg3), arg1, msg.sender);
+    return 1
+}
+
+function erc_sweep(address arg1, address arg2) payable {
+    require calldata.size - 4 >= 64
+    if governanceAddress != msg.sender:
+        revert with 0, 'must be governance'
+    require ext_code.size(arg1)
+    staticcall arg1.0x70a08231 with:
+            gas gas_remaining wei
+           args this.address
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    if eth.balance(this.address) < 0:
+        revert with 0, 32, 38, 0x65416464726573733a20696e73756666696369656e742062616c616e636520666f722063616c, mem[366 len 26]
+    if not ext_code.size(arg1):
+        revert with 0, 'Address: call to non-contract'
+    mem[260 len 64] = unknown_0xa9059cbb(?????), address(arg2) << 64, 0, ext_call.return_data[0 len 28]
+    mem[324 len 0] = 0
+    call arg1 with:
+         gas gas_remaining wei
+        args Mask(480, -256, ext_call.return_data[0 len 28]) << 256, mem[324 len 4]
+    if not return_data.size:
+        if not ext_call.success:
+            revert with unknown_0xa9059cbb(?????), address(arg2) << 64, 0, ext_call.return_data[0]
+        if not unknown_0xa9059cbb(?????), address(arg2) << 64:
+            revert with 0, 32, 42, 0x735361666545524332303a204552433230206f7065726174696f6e20646964206e6f7420737563636565, mem[370 len 22]
+        emit ErcSwept(address rg1, address rg2, address rg3, uint256 rg4):
+                      msg.sender,
+                      arg2,
+                      0,
+                      address(arg1),
+                      ext_call.return_data[0],
+    else:
+        mem[292 len return_data.size] = ext_call.return_data[0 len return_data.size]
+        if not ext_call.success:
+            if return_data.size:
+                revert with ext_call.return_data[0 len return_data.size]
+            revert with 0, 'SafeERC20: low-level call failed'
+        if return_data.size:
+            require return_data.size >= 32
+            if not mem[292]:
+                revert with 0, 
+                            32,
+                            42,
+                            0x735361666545524332303a204552433230206f7065726174696f6e20646964206e6f7420737563636565,
+                            mem[ceil32(return_data.size) + 371 len 22]
+        emit ErcSwept(msg.sender, address(arg2), address(arg1), ext_call.return_data[0]);
+}
+
+
+
+}
