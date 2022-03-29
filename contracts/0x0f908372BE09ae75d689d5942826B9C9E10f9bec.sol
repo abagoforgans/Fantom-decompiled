444b820eddf1596b92433bbb9b19335302278fa7
bot <abagoforgans@users.noreply.github.com>
2022-03-29 22:04:47 +0000

    autocommit

---
+++ contracts/0x0f908372BE09ae75d689d5942826B9C9E10f9bec.sol
@@ -0,0 +1,18 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+const sub_7cacb1d6(?) = ext_call.return_data[0]
+
+
+function _fallback() payable {
+    revert
+}
+
+
+
+}
