90099a56dc70eaeb7272fe2cdf365a78640a3b50
bot <abagoforgans@users.noreply.github.com>
2022-03-30 12:25:50 +0000

    autocommit

---
+++ contracts/0x62C3Cf63678fd6A0ace94A48F608F353C16191D5.sol
@@ -0,0 +1,26 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+const getName = 'YRegistry', 0
+
+const getBump = 32, 44, 1, 96, 6, Mask(48, 208, 'gerald') >> 208, 0
+
+
+address governanceAddress;
+
+function governance() payable {
+    return governanceAddress
+}
+
+function _fallback() payable {
+    revert
+}
+
+
+
+}
