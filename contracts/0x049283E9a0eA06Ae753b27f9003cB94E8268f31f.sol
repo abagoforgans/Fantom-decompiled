96e6efaa0cce7e242b69debf3d1296a88e5e4412
bot <abagoforgans@users.noreply.github.com>
2022-04-02 18:52:46 +0000

    autocommit

---
+++ contracts/0x049283E9a0eA06Ae753b27f9003cB94E8268f31f.sol
@@ -0,0 +1,21 @@
+contract main {
+
+
+// =======================  Init code  ======================
+
+
+const _fallback = code.data[27 len 53]
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+function _fallback() payable {
+    revert
+}
+
+
+
+}
