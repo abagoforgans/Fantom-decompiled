0acfd87f084b17d9bd270bf8662a90aac4dc8d49
bot <abagoforgans@users.noreply.github.com>
2022-04-04 23:39:26 +0000

    autocommit

---
+++ contracts/0x7e017C048CdA5F9A5Ae3a24F3679B93DbEE8be64.sol
@@ -0,0 +1,153 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+address owner;
+address sub_f85af418Address;
+address pairAddress;
+uint256 stor6;
+
+function owner() payable {
+    return owner
+}
+
+function pair() payable {
+    return pairAddress
+}
+
+function sub_f85af418(?) payable {
+    return sub_f85af418Address
+}
+
+function _fallback() payable {
+    revert
+}
+
+function renounceOwnership() payable {
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    emit OwnershipTransferred(owner, 0);
+    owner = 0
+}
+
+function withdrawLostEther() payable {
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    call owner with:
+       value eth.balance(this.address) wei
+         gas 2300 * is_zero(value) wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+}
+
+function excludeAccount(address arg1) payable {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    require ext_code.size(sub_f85af418Address)
+    call sub_f85af418Address.0xf2cc0c18 with:
+         gas gas_remaining wei
+        args arg1
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+}
+
+function includeAccount(address arg1) payable {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    require ext_code.size(sub_f85af418Address)
+    call sub_f85af418Address.0xf84354f1 with:
+         gas gas_remaining wei
+        args arg1
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+}
+
+function transferOwnership(address arg1) payable {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    38,
+                    0xfe4f776e61626c653a206e6577206f776e657220697320746865207a65726f20616464726573,
+                    mem[202 len 26]
+    emit OwnershipTransferred(owner, arg1);
+    owner = arg1
+}
+
+function withdrawLPTokens() payable {
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    if block.timestamp < stor6 + (720 * 24 * 3600):
+        revert with 0, 'You can't do this'
+    require ext_code.size(pairAddress)
+    staticcall pairAddress.0x70a08231 with:
+            gas gas_remaining wei
+           args this.address
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_code.size(pairAddress)
+    call pairAddress.0xa9059cbb with:
+         gas gas_remaining wei
+        args owner, ext_call.return_data[0]
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+}
+
+function withdrawTokens(address arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    if arg1 != sub_f85af418Address:
+        require ext_code.size(arg1)
+        if arg2:
+            call arg1.0xa9059cbb with:
+                 gas gas_remaining wei
+                args owner, arg2
+        else:
+            staticcall arg1.0x70a08231 with:
+                    gas gas_remaining wei
+                   args this.address
+            if not ext_call.success:
+                revert with ext_call.return_data[0 len return_data.size]
+            require return_data.size >= 32
+            require ext_code.size(arg1)
+            call arg1.0xa9059cbb with:
+                 gas gas_remaining wei
+                args owner, ext_call.return_data[0]
+    else:
+        if block.timestamp < stor6 + (4320 * 24 * 3600):
+            revert with 0, 'You can't do this'
+        require ext_code.size(sub_f85af418Address)
+        if arg2:
+            call sub_f85af418Address.0xa9059cbb with:
+                 gas gas_remaining wei
+                args owner, arg2
+        else:
+            staticcall sub_f85af418Address.0x70a08231 with:
+                    gas gas_remaining wei
+                   args this.address
+            if not ext_call.success:
+                revert with ext_call.return_data[0 len return_data.size]
+            require return_data.size >= 32
+            require ext_code.size(sub_f85af418Address)
+            call sub_f85af418Address.0xa9059cbb with:
+                 gas gas_remaining wei
+                args owner, ext_call.return_data[0]
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+}
+
+
+
+}
