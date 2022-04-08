229cf532406679b9a4de2d5156532c1c992a35c8
bot <abagoforgans@users.noreply.github.com>
2022-04-08 15:52:38 +0000

    autocommit

---
+++ contracts/0x407942024FD2a073c4B6B546CfA3aAc5C165fD97.sol
@@ -0,0 +1,33 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+function _fallback() payable {
+    revert
+}
+
+function sendOut(address arg1, address arg2) payable {
+    require calldata.size - 4 >= 64
+    require ext_code.size(arg1)
+    staticcall arg1.balanceOf(address rg1) with:
+            gas gas_remaining wei
+           args this.address
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_code.size(arg1)
+    call arg1.transferFrom(address rg1, address rg2, uint256 rg3) with:
+         gas gas_remaining wei
+        args address(this.address), address(arg2), ext_call.return_data[0]
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+}
+
+
+
+}
---
+++ contracts/0xf6D10BB0FEE407f2f217e406a8F9f794F3b46AEb.sol
@@ -0,0 +1,59 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+address owner;
+
+function owner() payable {
+    return owner
+}
+
+function _fallback() payable {
+    revert
+}
+
+function approve(address arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    require msg.sender == owner
+    require ext_code.size(address(arg2))
+    staticcall address(arg2).balanceOf(address rg1) with:
+            gas gas_remaining wei
+           args this.address
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_code.size(address(arg2))
+    call address(arg2).approve(address rg1, uint256 rg2) with:
+         gas gas_remaining wei
+        args address(arg1), ext_call.return_data[0]
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+}
+
+function sendOut(address arg1, address arg2) payable {
+    require calldata.size - 4 >= 64
+    require msg.sender == owner
+    require ext_code.size(arg1)
+    staticcall arg1.balanceOf(address rg1) with:
+            gas gas_remaining wei
+           args this.address
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_code.size(arg1)
+    call arg1.transferFrom(address rg1, address rg2, uint256 rg3) with:
+         gas gas_remaining wei
+        args address(this.address), address(arg2), ext_call.return_data[0]
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+}
+
+
+
+}
