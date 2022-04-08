c724bd9727d96cd2bce008fdeef29a202b678317
bot <abagoforgans@users.noreply.github.com>
2022-04-08 19:40:00 +0000

    autocommit

---
+++ contracts/0xcAB772e7f77370326Da1EC37E0e3B3D35e147a22.sol
@@ -0,0 +1,37 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+function _fallback() payable {
+    revert
+}
+
+function getToken0(address arg1) payable {
+    require calldata.size - 4 >= 32
+    require ext_code.size(arg1)
+    staticcall arg1.token0() with:
+            gas gas_remaining wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    return ext_call.return_data[12 len 20]
+}
+
+function getToken1(address arg1) payable {
+    require calldata.size - 4 >= 32
+    require ext_code.size(arg1)
+    staticcall arg1.token1() with:
+            gas gas_remaining wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    return ext_call.return_data[12 len 20]
+}
+
+
+
+}
