9b9eea5e5619282e34753160d2289a6780cbb09b
bot <abagoforgans@users.noreply.github.com>
2022-04-08 08:28:51 +0000

    autocommit

---
+++ contracts/0xcEabD1A0214d09948E881EE87EB990e452e1751b.sol
@@ -0,0 +1,34 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+function _fallback() payable {
+    revert
+}
+
+function test(uint256 arg1) {
+    s = 0
+    idx = 0
+    s = 0
+    while idx < arg1:
+        require ext_code.size(0xc35dadb65012ec5796536bd9864ed8773abc74c4)
+        call 0xc35dadb65012ec5796536bd9864ed8773abc74c4.allPairsLength() with:
+             gas gas_remaining wei
+        mem[96] = ext_call.return_data[0]
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+        require return_data.size >= 32
+        s = ext_call.return_data[0]
+        idx = idx + 1
+        s = s + ext_call.return_data[0]
+        continue 
+    return (s * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1 * arg1)
+}
+
+
+
+}
