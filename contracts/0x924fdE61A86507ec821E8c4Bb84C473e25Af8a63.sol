88d6cf40e6abd2412dfb4192570687d40eacfe3e
bot <abagoforgans@users.noreply.github.com>
2022-04-08 13:48:41 +0000

    autocommit

---
+++ contracts/0x924fdE61A86507ec821E8c4Bb84C473e25Af8a63.sol
@@ -0,0 +1,84 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+address owner;
+
+function owner() {
+    return owner
+}
+
+function _fallback() payable {
+    revert
+}
+
+function renounceOwnership() {
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    emit OwnershipTransferred(owner, 0);
+    owner = 0
+}
+
+function withdrawFTM(address arg1, uint256 arg2) {
+    require calldata.size - 4 >= 64
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    call arg1 with:
+       value arg2 wei
+         gas 2300 * is_zero(value) wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+}
+
+function transferOwnership(address arg1) {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    38,
+                    0xfe4f776e61626c653a206e6577206f776e657220697320746865207a65726f20616464726573,
+                    mem[202 len 26]
+    emit OwnershipTransferred(owner, arg1);
+    owner = arg1
+}
+
+function withdrawTokens(address arg1, address arg2, uint256 arg3) {
+    require calldata.size - 4 >= 96
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    if ext_code.size(arg1) <= 0:
+        revert with 0, 'Address: call to non-contract'
+    mem[260 len 64] = transfer(address rg1, uint256 rg2), address(arg2) << 64, 0, Mask(224, 32, arg3) >> 32
+    call arg1 with:
+         gas gas_remaining wei
+        args Mask(224, 32, arg3) << 224, mem[324 len 4]
+    if not return_data.size:
+        if not ext_call.success:
+            revert with transfer(address rg1, uint256 rg2), address(arg2) << 64, 0, arg3
+        if not transfer(address rg1, uint256 rg2), address(arg2) << 64:
+            revert with 0, 32, 42, 0x735361666545524332303a204552433230206f7065726174696f6e20646964206e6f7420737563636565, mem[370 len 22]
+    else:
+        mem[292 len return_data.size] = ext_call.return_data[0 len return_data.size]
+        if not ext_call.success:
+            if return_data.size > 0:
+                revert with ext_call.return_data[0 len return_data.size]
+            revert with 0, 'SafeERC20: low-level call failed'
+        if return_data.size > 0:
+            require return_data.size >= 32
+            if not mem[292]:
+                revert with 0, 
+                            32,
+                            42,
+                            0x735361666545524332303a204552433230206f7065726174696f6e20646964206e6f7420737563636565,
+                            mem[ceil32(return_data.size) + 371 len 22]
+}
+
+
+
+}
