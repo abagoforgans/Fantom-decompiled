4181c20883fb61d2fec5036b661e071cad7de6d5
bot <abagoforgans@users.noreply.github.com>
2022-04-09 06:42:55 +0000

    autocommit

---
+++ contracts/0x5d7652B559d8D9572E177650c7BBBbfd1Eb1eB92.sol
@@ -0,0 +1,108 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+address owner;
+uint256 totalUnlocked;
+uint256 lastClaimedTimestamp;
+uint256 sub_6d1b1af9;
+uint256 c;
+uint256 a;
+uint8 stor6;
+uint8 stor6; offset 8
+uint256 stor6; offset 8
+
+function a() payable {
+    return a
+}
+
+function sub_6d1b1af9(?) payable {
+    return sub_6d1b1af9
+}
+
+function owner() payable {
+    return owner
+}
+
+function farmingStarted() payable {
+    return bool(uint8(stor6.field_8))
+}
+
+function totalUnlocked() payable {
+    return totalUnlocked
+}
+
+function c() payable {
+    return c
+}
+
+function lastClaimedTimestamp() payable {
+    return lastClaimedTimestamp
+}
+
+function _fallback() payable {
+    revert
+}
+
+function setDeprecated() payable {
+    if owner != msg.sender:
+        revert with 0, 'Caller is not owner'
+    uint8(stor6.field_0) = 0
+}
+
+function transferOwnership(address arg1) payable {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    if owner != msg.sender:
+        revert with 0, 'Caller is not owner'
+    owner = arg1
+}
+
+function startFarming() payable {
+    if owner != msg.sender:
+        revert with 0, 'Caller is not owner'
+    Mask(248, 0, stor6.field_8) = 1
+    sub_6d1b1af9 = block.timestamp
+    lastClaimedTimestamp = sub_6d1b1af9
+}
+
+function unlockAsset() payable {
+    if not uint8(stor6.field_8):
+        revert with 0, 'farming is not started yet'
+    if not uint8(stor6.field_0):
+        revert with 0, 'This contract is deprecated.'
+    if not c:
+        revert with 'NH{q', 18
+    if lastClaimedTimestamp > -(a / c) - 1:
+        revert with 'NH{q', 17
+    if lastClaimedTimestamp + (a / c) < sub_6d1b1af9:
+        revert with 'NH{q', 17
+    if a and 10^18 > -1 / a:
+        revert with 'NH{q', 17
+    if not lastClaimedTimestamp + (a / c) - sub_6d1b1af9:
+        revert with 'NH{q', 18
+    if not c:
+        revert with 'NH{q', 18
+    if block.timestamp > -(a / c) - 1:
+        revert with 'NH{q', 17
+    if block.timestamp + (a / c) < sub_6d1b1af9:
+        revert with 'NH{q', 17
+    if a and 10^18 > -1 / a:
+        revert with 'NH{q', 17
+    if not block.timestamp + (a / c) - sub_6d1b1af9:
+        revert with 'NH{q', 18
+    if 10^18 * a / lastClaimedTimestamp + (a / c) - sub_6d1b1af9 < 10^18 * a / block.timestamp + (a / c) - sub_6d1b1af9:
+        revert with 'NH{q', 17
+    lastClaimedTimestamp = block.timestamp
+    if totalUnlocked > -(10^18 * a / lastClaimedTimestamp + (a / c) - sub_6d1b1af9) + (10^18 * a / block.timestamp + (a / c) - sub_6d1b1af9) - 1:
+        revert with 'NH{q', 17
+    totalUnlocked = totalUnlocked + (10^18 * a / lastClaimedTimestamp + (a / c) - sub_6d1b1af9) - (10^18 * a / block.timestamp + (a / c) - sub_6d1b1af9)
+}
+
+
+
+}
