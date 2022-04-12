9e4caf7df1dba853ce338bc67480bcce9ef54b10
bot <abagoforgans@users.noreply.github.com>
2022-04-12 12:56:50 +0000

    autocommit

---
+++ contracts/0x0BcE157b32018C4A76edB2d862fd0d7D2BbCeB0D.sol
@@ -0,0 +1,180 @@
+contract main {
+
+
+// =======================  Init code  ======================
+
+
+address stor0;
+array of uint256 stor2;
+array of uint256 stor3;
+uint8 stor4;
+uint256 stor5;
+mapping of uint256 stor6;
+
+function _fallback() {
+    stor0 = msg.sender
+    bool(stor2.length) = 0
+    stor2.length.field_1 = 5
+    stor2.length.field_8 = 'SNTVT' / 256
+    idx = 0
+    while stor2.length + 31 / 32 > idx:
+        stor2[idx].field_0 = 0
+        idx = idx + 1
+        continue 
+    bool(stor3.length) = 0
+    stor3.length.field_1 = 9
+    stor3.length.field_8 = 'Sentivate' / 256
+    idx = 0
+    while stor3.length + 31 / 32 > idx:
+        stor3[idx].field_0 = 0
+        idx = idx + 1
+        continue 
+    stor4 = 18
+    stor5 = 4200000000 * 10^18
+    stor6[0x2f3a5962357058e89fd6c86890d3d0b22b8983b1] = stor5
+    emit Transfer(address rg1, address rg2, uint256 rg3):
+                  stor5,
+    emit 0x0: stor5
+    emit 0x2f3a5962: stor5
+    return code.data[694 len 5596]
+}
+
+
+
+// =====================  Runtime code  =====================
+
+
+address owner;
+address newOwner;
+array of uint256 symbol;
+array of uint256 name;
+uint8 decimals;
+uint256 _totalSupply;
+mapping of uint256 balanceOf;
+mapping of uint256 allowance;
+
+function name() {
+    return name[0 len name.length]
+}
+
+function decimals() {
+    return decimals
+}
+
+function _totalSupply() {
+    return _totalSupply
+}
+
+function balanceOf(address arg1) {
+    return balanceOf[address(arg1)]
+}
+
+function owner() {
+    return owner
+}
+
+function symbol() {
+    return symbol[0 len symbol.length]
+}
+
+function newOwner() {
+    return newOwner
+}
+
+function allowance(address arg1, address arg2) {
+    return allowance[address(arg1)][address(arg2)]
+}
+
+function _fallback() payable {
+    revert
+}
+
+function totalSupply() {
+    return (_totalSupply - balanceOf[0])
+}
+
+function transferOwnership(address arg1) {
+    require msg.sender == owner
+    newOwner = arg1
+}
+
+function safeSub(uint256 arg1, uint256 arg2) {
+    require arg2 <= arg1
+    return (arg1 - arg2)
+}
+
+function safeAdd(uint256 arg1, uint256 arg2) {
+    require arg1 + arg2 >= arg1
+    return (arg1 + arg2)
+}
+
+function safeDiv(uint256 arg1, uint256 arg2) {
+    require arg2 > 0
+    require arg2
+    return (arg1 / arg2)
+}
+
+function safeMul(uint256 arg1, uint256 arg2) {
+    if arg1:
+        require arg1
+        require arg1 * arg2 / arg1 == arg2
+    return (arg1 * arg2)
+}
+
+function acceptOwnership() {
+    require msg.sender == newOwner
+    emit OwnershipTransferred(owner, newOwner);
+    owner = newOwner
+    newOwner = 0
+}
+
+function approve(address arg1, uint256 arg2) {
+    allowance[address(msg.sender)][address(arg1)] = arg2
+    emit Approval(arg2, msg.sender, arg1);
+    return 1
+}
+
+function transferAnyERC20Token(address arg1, uint256 arg2) {
+    require msg.sender == owner
+    require ext_code.size(arg1)
+    call arg1.0xa9059cbb with:
+         gas gas_remaining - 710 wei
+        args owner, arg2
+    require ext_call.success
+    return bool(ext_call.return_data[0])
+}
+
+function transfer(address arg1, uint256 arg2) {
+    require arg2 <= balanceOf[address(msg.sender)]
+    balanceOf[address(msg.sender)] -= arg2
+    require balanceOf[address(arg1)] + arg2 >= balanceOf[address(arg1)]
+    balanceOf[address(arg1)] += arg2
+    emit Transfer(arg2, msg.sender, arg1);
+    return 1
+}
+
+function transferFrom(address arg1, address arg2, uint256 arg3) {
+    require arg3 <= balanceOf[address(arg1)]
+    balanceOf[address(arg1)] -= arg3
+    require arg3 <= allowance[address(arg1)][address(msg.sender)]
+    allowance[address(arg1)][address(msg.sender)] -= arg3
+    require balanceOf[address(arg2)] + arg3 >= balanceOf[address(arg2)]
+    balanceOf[address(arg2)] += arg3
+    emit Transfer(arg3, arg1, arg2);
+    return 1
+}
+
+function approveAndCall(address arg1, uint256 arg2, bytes arg3) {
+    allowance[address(msg.sender)][address(arg1)] = arg2
+    emit Approval(arg2, msg.sender, arg1);
+    require ext_code.size(arg1)
+    call arg1.receiveApproval(address rg1, uint256 rg2, address rg3, bytes rg4) with:
+         gas gas_remaining - 710 wei
+        args msg.sender, arg2, address(this.address), Array(len=arg3.length, data=arg3[all])
+    require ext_call.success
+    return 1
+}
+
+
+
+}
