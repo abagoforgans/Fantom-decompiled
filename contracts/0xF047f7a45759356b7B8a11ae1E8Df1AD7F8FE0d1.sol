53d47ea479a444aa615f2829a3bbab2c2432814c
bot <abagoforgans@users.noreply.github.com>
2022-04-15 15:51:29 +0000

    autocommit

---
+++ contracts/0xF047f7a45759356b7B8a11ae1E8Df1AD7F8FE0d1.sol
@@ -0,0 +1,269 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+function _fallback() payable {
+    revert
+}
+
+function sub_2c1ff159(?) payable {
+    require calldata.size - 4 >= 64
+    require ext_code.size(arg1)
+    staticcall arg1.0x70a08231 with:
+            gas gas_remaining wei
+           args arg2
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_code.size(0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c)
+    staticcall 0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c.0x70a08231 with:
+            gas gas_remaining wei
+           args arg2
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    if not ext_call.return_data[0]:
+        if ext_call.return_data[0] <= 0:
+            revert with 0, 'SafeMath: division by zero'
+        if ext_call.return_data[0]:
+            return (0 / ext_call.return_data[0])
+    else:
+        if 10^18 * ext_call.return_data[0] / ext_call.return_data[0] != 10^18:
+            revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                        32,
+                        33,
+                        0xfe536166654d6174683a206d756c7469706c69636174696f6e206f766572666c6f,
+                        mem[197 len 31]
+        if ext_call.return_data[0] <= 0:
+            revert with 0, 'SafeMath: division by zero'
+        if ext_call.return_data[0]:
+            return (10^18 * ext_call.return_data[0] / ext_call.return_data[0])
+    ('iszero', ('ext_call.return_data', 0, 32))
+    revert
+}
+
+function bnbPriceInUSD() payable {
+    require ext_code.size(0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c)
+    staticcall 0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c.0x70a08231 with:
+            gas gas_remaining wei
+           args 0x58f876857a02d6762e0101bb5c46a8c1ed44dc16
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_code.size(0xe9e7cea3dedca5984780bafc599bd69add087d56)
+    staticcall 0xe9e7cea3dedca5984780bafc599bd69add087d56.0x70a08231 with:
+            gas gas_remaining wei
+           args 0x58f876857a02d6762e0101bb5c46a8c1ed44dc16
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    if not ext_call.return_data[0]:
+        if ext_call.return_data[0] <= 0:
+            revert with 0, 'SafeMath: division by zero'
+        if ext_call.return_data[0]:
+            return (0 / ext_call.return_data[0])
+    else:
+        if 10^18 * ext_call.return_data[0] / ext_call.return_data[0] != 10^18:
+            revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                        32,
+                        33,
+                        0xfe536166654d6174683a206d756c7469706c69636174696f6e206f766572666c6f,
+                        mem[197 len 31]
+        if ext_call.return_data[0] <= 0:
+            revert with 0, 'SafeMath: division by zero'
+        if ext_call.return_data[0]:
+            return (10^18 * ext_call.return_data[0] / ext_call.return_data[0])
+    ('iszero', ('ext_call.return_data', 0, 32))
+    revert
+}
+
+function sub_d3a7fa74(?) payable {
+    require calldata.size - 4 >= 64
+    require ext_code.size(arg2)
+    staticcall arg2.getPair(address rg1, address rg2) with:
+            gas gas_remaining wei
+           args address(arg1), 0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_code.size(arg1)
+    staticcall arg1.0x313ce567 with:
+            gas gas_remaining wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_code.size(arg1)
+    staticcall arg1.0x70a08231 with:
+            gas gas_remaining wei
+           args address(ext_call.return_data[0])
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_code.size(0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c)
+    staticcall 0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c.0x70a08231 with:
+            gas gas_remaining wei
+           args address(ext_call.return_data[0])
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    if not ext_call.return_data[0]:
+        if ext_call.return_data[0] <= 0:
+            revert with 0, 'SafeMath: division by zero'
+        if ext_call.return_data[0]:
+            return (0 / ext_call.return_data[0])
+    else:
+        if 10^uint8(ext_call.return_data[0]) * ext_call.return_data[0] / ext_call.return_data[0] != 10^uint8(ext_call.return_data[0]):
+            revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                        32,
+                        33,
+                        0xfe536166654d6174683a206d756c7469706c69636174696f6e206f766572666c6f,
+                        mem[197 len 31]
+        if ext_call.return_data[0] <= 0:
+            revert with 0, 'SafeMath: division by zero'
+        if ext_call.return_data[0]:
+            return (10^uint8(ext_call.return_data[0]) * ext_call.return_data[0] / ext_call.return_data[0])
+    ('iszero', ('ext_call.return_data', 0, 32))
+    revert
+}
+
+function sub_1087ac05(?) payable {
+    require calldata.size - 4 >= 64
+    require ext_code.size(arg2)
+    staticcall arg2.getPair(address rg1, address rg2) with:
+            gas gas_remaining wei
+           args address(arg1), 0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_code.size(arg1)
+    staticcall arg1.0x313ce567 with:
+            gas gas_remaining wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_code.size(arg1)
+    staticcall arg1.0x70a08231 with:
+            gas gas_remaining wei
+           args address(ext_call.return_data[0])
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_code.size(0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c)
+    staticcall 0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c.0x70a08231 with:
+            gas gas_remaining wei
+           args address(ext_call.return_data[0])
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    if not ext_call.return_data[0]:
+        if ext_call.return_data[0] <= 0:
+            revert with 0, 'SafeMath: division by zero'
+        require ext_call.return_data[0]
+        require ext_code.size(0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c)
+        staticcall 0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c.0x70a08231 with:
+                gas gas_remaining wei
+               args 0x58f876857a02d6762e0101bb5c46a8c1ed44dc16
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+        require return_data.size >= 32
+        require ext_code.size(0xe9e7cea3dedca5984780bafc599bd69add087d56)
+        staticcall 0xe9e7cea3dedca5984780bafc599bd69add087d56.0x70a08231 with:
+                gas gas_remaining wei
+               args 0x58f876857a02d6762e0101bb5c46a8c1ed44dc16
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+        require return_data.size >= 32
+        if not ext_call.return_data[0]:
+            if ext_call.return_data[0] <= 0:
+                revert with 0, 'SafeMath: division by zero'
+            require ext_call.return_data[0]
+            if not 0 / ext_call.return_data[0]:
+                return 0
+            if 0 / ext_call.return_data[0] * 0 / ext_call.return_data[0] / 0 / ext_call.return_data[0] != 0 / ext_call.return_data[0]:
+                revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                            32,
+                            33,
+                            0xfe536166654d6174683a206d756c7469706c69636174696f6e206f766572666c6f,
+                            mem[197 len 31]
+            return (0 / ext_call.return_data[0] * 0 / ext_call.return_data[0])
+        if 10^18 * ext_call.return_data[0] / ext_call.return_data[0] != 10^18:
+            revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                        32,
+                        33,
+                        0xfe536166654d6174683a206d756c7469706c69636174696f6e206f766572666c6f,
+                        mem[197 len 31]
+        if ext_call.return_data[0] <= 0:
+            revert with 0, 'SafeMath: division by zero'
+        require ext_call.return_data[0]
+        if not 10^18 * ext_call.return_data[0] / ext_call.return_data[0]:
+            return 0
+        if 0 / ext_call.return_data[0] * 10^18 * ext_call.return_data[0] / ext_call.return_data[0] / 10^18 * ext_call.return_data[0] / ext_call.return_data[0] != 0 / ext_call.return_data[0]:
+            revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                        32,
+                        33,
+                        0xfe536166654d6174683a206d756c7469706c69636174696f6e206f766572666c6f,
+                        mem[197 len 31]
+        return (0 / ext_call.return_data[0] * 10^18 * ext_call.return_data[0] / ext_call.return_data[0])
+    if 10^uint8(ext_call.return_data[0]) * ext_call.return_data[0] / ext_call.return_data[0] != 10^uint8(ext_call.return_data[0]):
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    33,
+                    0xfe536166654d6174683a206d756c7469706c69636174696f6e206f766572666c6f,
+                    mem[197 len 31]
+    if ext_call.return_data[0] <= 0:
+        revert with 0, 'SafeMath: division by zero'
+    require ext_call.return_data[0]
+    require ext_code.size(0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c)
+    staticcall 0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c.0x70a08231 with:
+            gas gas_remaining wei
+           args 0x58f876857a02d6762e0101bb5c46a8c1ed44dc16
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_code.size(0xe9e7cea3dedca5984780bafc599bd69add087d56)
+    staticcall 0xe9e7cea3dedca5984780bafc599bd69add087d56.0x70a08231 with:
+            gas gas_remaining wei
+           args 0x58f876857a02d6762e0101bb5c46a8c1ed44dc16
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    if not ext_call.return_data[0]:
+        if ext_call.return_data[0] <= 0:
+            revert with 0, 'SafeMath: division by zero'
+        require ext_call.return_data[0]
+        if not 0 / ext_call.return_data[0]:
+            return 0
+        if 10^uint8(ext_call.return_data[0]) * ext_call.return_data[0] / ext_call.return_data[0] * 0 / ext_call.return_data[0] / 0 / ext_call.return_data[0] != 10^uint8(ext_call.return_data[0]) * ext_call.return_data[0] / ext_call.return_data[0]:
+            revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                        32,
+                        33,
+                        0xfe536166654d6174683a206d756c7469706c69636174696f6e206f766572666c6f,
+                        mem[197 len 31]
+        return (10^uint8(ext_call.return_data[0]) * ext_call.return_data[0] / ext_call.return_data[0] * 0 / ext_call.return_data[0])
+    if 10^18 * ext_call.return_data[0] / ext_call.return_data[0] != 10^18:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    33,
+                    0xfe536166654d6174683a206d756c7469706c69636174696f6e206f766572666c6f,
+                    mem[197 len 31]
+    if ext_call.return_data[0] <= 0:
+        revert with 0, 'SafeMath: division by zero'
+    require ext_call.return_data[0]
+    if not 10^18 * ext_call.return_data[0] / ext_call.return_data[0]:
+        return 0
+    if 10^uint8(ext_call.return_data[0]) * ext_call.return_data[0] / ext_call.return_data[0] * 10^18 * ext_call.return_data[0] / ext_call.return_data[0] / 10^18 * ext_call.return_data[0] / ext_call.return_data[0] != 10^uint8(ext_call.return_data[0]) * ext_call.return_data[0] / ext_call.return_data[0]:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    33,
+                    0xfe536166654d6174683a206d756c7469706c69636174696f6e206f766572666c6f,
+                    mem[197 len 31]
+    return (10^uint8(ext_call.return_data[0]) * ext_call.return_data[0] / ext_call.return_data[0] * 10^18 * ext_call.return_data[0] / ext_call.return_data[0])
+}
+
+
+
+}
