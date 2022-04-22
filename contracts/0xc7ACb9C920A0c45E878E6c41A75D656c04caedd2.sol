37d2b0aa19b4dd285f239f40201ec49d82e968c6
bot <abagoforgans@users.noreply.github.com>
2022-04-22 22:45:10 +0000

    autocommit

---
+++ contracts/0xc7ACb9C920A0c45E878E6c41A75D656c04caedd2.sol
@@ -0,0 +1,34 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+mapping of address tokensToExchange;
+
+function tokensToExchange(address arg1, address arg2, uint256 arg3) payable {
+    require calldata.size - 4 >= 96
+    return tokensToExchange[arg1][arg2][arg3]
+}
+
+function _fallback() payable {
+    revert
+}
+
+function createExchange(address arg1, address arg2, uint256 arg3) payable {
+    require calldata.size - 4 >= 96
+    if tokensToExchange[address(arg1)][address(arg2)][arg3]:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 32, 54, code.data[18456 len 54], mem[218 len 10]
+    create contract with 0 wei
+                    code: code.data[627 len 17829], address(arg1), address(arg2), arg3
+    if not create.new_address:
+        revert with ext_call.return_data[0 len return_data.size]
+    tokensToExchange[address(arg1)][address(arg2)][arg3] = address(create.new_address)
+    emit NewExchange(address(create.new_address), arg1, arg2, arg3);
+}
+
+
+
+}
