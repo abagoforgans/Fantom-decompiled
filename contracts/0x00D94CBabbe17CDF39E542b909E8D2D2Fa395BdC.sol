3ccb013fe282c91e7656d71ac6a817d3926980bb
bot <abagoforgans@users.noreply.github.com>
2022-04-25 09:36:44 +0000

    autocommit

---
+++ contracts/0x00D94CBabbe17CDF39E542b909E8D2D2Fa395BdC.sol
@@ -0,0 +1,31 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+mapping of uint256 allowance;
+
+function allowance(address arg1, address arg2) payable {
+    require calldata.size - 4 >= 64
+    require arg1 == arg1
+    require arg2 == arg2
+    return allowance[arg1][arg2]
+}
+
+function _fallback() payable {
+    revert
+}
+
+function approve(address arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    require arg1 == arg1
+    allowance[msg.sender][address(arg1)] = arg2
+    return 0
+}
+
+
+
+}
