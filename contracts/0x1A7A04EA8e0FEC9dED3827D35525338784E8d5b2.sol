4f9a8e17cb4320c3eb7154e0730f3957b1eb77ae
bot <abagoforgans@users.noreply.github.com>
2022-04-26 15:59:16 +0000

    autocommit

---
+++ contracts/0x1A7A04EA8e0FEC9dED3827D35525338784E8d5b2.sol
@@ -0,0 +1,37 @@
+contract main {
+
+
+// =======================  Init code  ======================
+
+
+array of uint256 stor0;
+
+function _fallback() {
+    mem[96 len -709] = code.data[961 len -709]
+    mem[64] = -613
+    stor0[] = Array(len=mem[mem[96] + 96], data=mem[mem[96] + 128 len mem[mem[96] + 96]])
+    return code.data[252 len 709]
+}
+
+
+
+// =====================  Runtime code  =====================
+
+
+array of uint256 message;
+
+function message() {
+    return message[0 len message.length]
+}
+
+function _fallback() payable {
+    revert
+}
+
+function sub_3a40f87e(?) {
+    message[] = Array(len=arg1.length, data=arg1[all])
+}
+
+
+
+}
