e095c89fea98fc047a766412321161bdf6c97103
bot <abagoforgans@users.noreply.github.com>
2022-04-27 23:53:39 +0000

    autocommit

---
+++ contracts/0x5169eE735cC5b665A8702d104b258b2913986557.sol
@@ -0,0 +1,81 @@
+contract main {
+
+
+// =======================  Init code  ======================
+
+
+uint256 stor0;
+mapping of uint256 stor1;
+
+function _fallback() {
+    stor0 = 59 * 10^12
+    stor1[address(msg.sender)] = stor0
+    return code.data[113 len 3072]
+}
+
+
+
+// =====================  Runtime code  =====================
+
+
+const name = 'TokenPocket Token'
+
+const decimals = 4
+
+const symbol = 'TPT'
+
+
+uint256 totalSupply;
+mapping of uint256 balanceOf;
+mapping of uint256 allowance;
+
+function totalSupply() {
+    return totalSupply
+}
+
+function balanceOf(address arg1) {
+    return balanceOf[address(arg1)]
+}
+
+function allowance(address arg1, address arg2) {
+    return allowance[address(arg1)][address(arg2)]
+}
+
+function _fallback() payable {
+    revert
+}
+
+function approve(address arg1, uint256 arg2) {
+    if arg2:
+        require not allowance[address(msg.sender)][address(arg1)]
+    allowance[address(msg.sender)][address(arg1)] = arg2
+    emit Approval(arg2, msg.sender, arg1);
+    return 1
+}
+
+function transfer(address arg1, uint256 arg2) {
+    require arg1
+    require arg2 <= balanceOf[address(msg.sender)]
+    balanceOf[address(msg.sender)] -= arg2
+    require balanceOf[address(arg1)] + arg2 >= balanceOf[address(arg1)]
+    balanceOf[address(arg1)] += arg2
+    emit Transfer(arg2, msg.sender, arg1);
+    return 1
+}
+
+function transferFrom(address arg1, address arg2, uint256 arg3) {
+    require arg2
+    require arg3 <= allowance[address(arg1)][address(msg.sender)]
+    require arg3 <= balanceOf[address(arg1)]
+    balanceOf[address(arg1)] -= arg3
+    require balanceOf[address(arg2)] + arg3 >= balanceOf[address(arg2)]
+    balanceOf[address(arg2)] += arg3
+    require arg3 <= allowance[address(arg1)][address(msg.sender)]
+    allowance[address(arg1)][address(msg.sender)] -= arg3
+    emit Transfer(arg3, arg1, arg2);
+    return 1
+}
+
+
+
+}
