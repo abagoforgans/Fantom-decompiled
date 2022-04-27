8bd63ec7dae79517508950ced443f60da7b3b692
bot <abagoforgans@users.noreply.github.com>
2022-04-27 14:04:30 +0000

    autocommit

---
+++ contracts/0x20f1f299B0D032560287E5881AfeFff0996bC4Fa.sol
@@ -0,0 +1,151 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+#
+#  - swap(uint256 arg1, uint256 arg2, address arg3, address[] arg4, address arg5, address[] arg6, address arg7, bool arg8)
+#  - swapByMyRouter(uint256 arg1, address[] arg2, address[] arg3, bool arg4, uint256 arg5)
+#
+address owner;
+
+function owner() payable {
+    return owner
+}
+
+function _fallback() payable {
+    revert
+}
+
+function renounceOwnership() payable {
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    emit OwnershipTransferred(owner, 0);
+    owner = 0
+}
+
+function withdraw(uint256 arg1) payable {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    call msg.sender with:
+       value arg1 wei
+         gas 2300 * is_zero(value) wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+}
+
+function myadmincall(address arg1, uint256 arg2, bytes arg3) payable {
+    require calldata.size - 4 >= 96
+    require arg3 <= 4294967296
+    require arg3 + 36 <= calldata.size
+    require arg3.length <= 4294967296 and arg3 + arg3.length + 36 <= calldata.size
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    call arg1 with:
+       value arg2 wei
+         gas gas_remaining wei
+        args arg3[all]
+}
+
+function withdrawToken(address arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    require ext_code.size(arg1)
+    call arg1.transfer(address rg1, uint256 rg2) with:
+         gas gas_remaining wei
+        args msg.sender, arg2
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+}
+
+function withdrawTokenTo(address arg1, uint256 arg2, address arg3) payable {
+    require calldata.size - 4 >= 96
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    require ext_code.size(arg1)
+    call arg1.transfer(address rg1, uint256 rg2) with:
+         gas gas_remaining wei
+        args address(arg3), arg2
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+}
+
+function transferOwnership(address arg1) payable {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    38,
+                    0xfe4f776e61626c653a206e6577206f776e657220697320746865207a65726f20616464726573,
+                    mem[202 len 26]
+    emit OwnershipTransferred(owner, arg1);
+    owner = arg1
+}
+
+function uint2str(uint256 arg1) payable {
+    require calldata.size - 4 >= 32
+    if not arg1:
+        return '0'
+    s = 0
+    idx = arg1
+    while idx:
+        s = s + 1
+        idx = idx / 10
+        continue 
+    require s <= test266151307()
+    mem[96] = s
+    mem[64] = ceil32(s) + 128
+    if not s:
+        t = s - 1
+        idx = arg1
+        while idx:
+            require t < mem[96]
+            mem[t + 128 len 8] = Mask(8, -(('mask_shl', 8, 0, 245, ('add', 48, ('mod', ('var', 0), 10))), 0) + 256, 0) << (('mask_shl', 8, 0, 245, ('add', 48, ('mod', ('var', 0), 10))), 0) - 256
+            t = t - 1
+            idx = idx / 10
+            continue 
+        _32 = mem[64]
+        mem[mem[64]] = 32
+        mem[mem[64] + 32] = mem[96]
+        _34 = mem[96]
+        mem[mem[64] + 64 len ceil32(mem[96])] = mem[128 len ceil32(mem[96])]
+        if not mem[96] % 32:
+            return 32, mem[mem[64] + 32 len mem[96] + 32]
+        mem[floor32(mem[96]) + mem[64] + 64] = mem[floor32(mem[96]) + mem[64] + -(mem[96] % 32) + 96 len mem[96] % 32]
+        return memory
+          from mem[64]
+           len floor32(_34) + _32 + -mem[64] + 96
+    mem[128 len s] = call.data[calldata.size len s]
+    t = s - 1
+    idx = arg1
+    while idx:
+        require t < mem[96]
+        mem[t + 128 len 8] = Mask(8, -(('mask_shl', 8, 0, 245, ('add', 48, ('mod', ('var', 0), 10))), 0) + 256, 0) << (('mask_shl', 8, 0, 245, ('add', 48, ('mod', ('var', 0), 10))), 0) - 256
+        t = t - 1
+        idx = idx / 10
+        continue 
+    _35 = mem[64]
+    mem[mem[64]] = 32
+    mem[mem[64] + 32] = mem[96]
+    _37 = mem[96]
+    mem[mem[64] + 64 len ceil32(mem[96])] = mem[128 len ceil32(mem[96])]
+    if not mem[96] % 32:
+        return 32, mem[mem[64] + 32 len mem[96] + 32]
+    mem[floor32(mem[96]) + mem[64] + 64] = mem[floor32(mem[96]) + mem[64] + -(mem[96] % 32) + 96 len mem[96] % 32]
+    return memory
+      from mem[64]
+       len floor32(_37) + _35 + -mem[64] + 96
+}
+
+
+
+}
