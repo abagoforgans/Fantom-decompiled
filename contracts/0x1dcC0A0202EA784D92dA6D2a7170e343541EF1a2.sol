7a3e053d26fee2d606474b5821618a822379c03a
bot <abagoforgans@users.noreply.github.com>
2022-05-01 17:10:33 +0000

    autocommit

---
+++ contracts/0x1dcC0A0202EA784D92dA6D2a7170e343541EF1a2.sol
@@ -0,0 +1,73 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+address owner;
+uint256 sub_fb22198b;
+address brushAddress;
+
+function brush() payable {
+    return brushAddress
+}
+
+function owner() payable {
+    return owner
+}
+
+function sub_fb22198b(?) payable {
+    return sub_fb22198b
+}
+
+function _fallback() payable {
+    revert
+}
+
+function renounceOwnership() payable {
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    emit OwnershipTransferred(owner, 0);
+    owner = 0
+}
+
+function sub_7e7de151(?) payable {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    sub_fb22198b = arg1
+    emit 0x124b2aaf: sub_fb22198b, arg1
+}
+
+function transferOwnership(address arg1) payable {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'Ownable: new owner is the zero address'
+    emit OwnershipTransferred(owner, arg1);
+    owner = arg1
+}
+
+function burn() payable {
+    require ext_code.size(brushAddress)
+    staticcall brushAddress.balanceOf(address arg1) with:
+            gas gas_remaining wei
+           args this.address
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_code.size(brushAddress)
+    call brushAddress.burn(uint256 arg1) with:
+         gas gas_remaining wei
+        args ext_call.return_data[0]
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+}
+
+
+
+}
