9b5a278edfbf21157e97dfbf51f4e1f7d14bcddc
bot <abagoforgans@users.noreply.github.com>
2022-05-06 07:33:04 +0000

    autocommit

---
+++ contracts/0x4EF9a52313Ed19f74f3194D9114eEF63e0611eB8.sol
@@ -0,0 +1,263 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+function _fallback() payable {
+  stop
+}
+
+function sub_3aa99232(?) {
+    require calldata.size - 4 >= 128
+    require arg1 == address(arg1)
+    require arg2 <= test266151307()
+    require arg2 + 35 < calldata.size
+    require arg2.length <= test266151307()
+    require arg2 + arg2.length + 36 <= calldata.size
+    mem[128 len arg2.length] = arg2[all]
+    mem[arg2.length + 128] = 0
+    _3 = mem[128]
+    mem[ceil32(arg2.length) + 128] = mem[128]
+    require ext_code.size(address(_3))
+    staticcall address(_3).0x70a08231 with:
+            gas gas_remaining wei
+           args this.address
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_code.size(address(_3))
+    staticcall address(_3).0x70a08231 with:
+            gas gas_remaining wei
+           args this.address
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    if 0, ext_call.return_data[4 len 28] != mem[160]:
+        if ext_call.return_data[0] != mem[160]:
+            revert with 0, 'token has tnx fee'
+    else:
+        if ext_call.return_data[0] == mem[160]:
+            revert with 0, 'passed'
+}
+
+function sub_ce1fea8e(?) {
+    require calldata.size - 4 >= 128
+    require arg1 == address(arg1)
+    require arg2 <= test266151307()
+    require arg2 + 35 < calldata.size
+    require arg2.length <= test266151307()
+    require arg2 + arg2.length + 36 <= calldata.size
+    mem[128 len arg2.length] = arg2[all]
+    mem[arg2.length + 128] = 0
+    _3 = mem[128]
+    mem[ceil32(arg2.length) + 128] = mem[128]
+    require ext_code.size(address(_3))
+    staticcall address(_3).0x70a08231 with:
+            gas gas_remaining wei
+           args this.address
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_code.size(address(_3))
+    staticcall address(_3).0x70a08231 with:
+            gas gas_remaining wei
+           args this.address
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    if 0, ext_call.return_data[4 len 28] != mem[160]:
+        if ext_call.return_data[0] != mem[160]:
+            revert with 0, 'token has tnx fee'
+    else:
+        if ext_call.return_data[0] == mem[160]:
+            revert with 0, 'passed'
+}
+
+function elkCall(address arg1, uint256 arg2, uint256 arg3, bytes arg4) {
+    require calldata.size - 4 >= 128
+    require arg1 == arg1
+    require arg4 <= test266151307()
+    require arg4 + 35 < calldata.size
+    require arg4.length <= test266151307()
+    require arg4 + arg4.length + 36 <= calldata.size
+    mem[128 len arg4.length] = arg4[all]
+    mem[arg4.length + 128] = 0
+    _3 = mem[128]
+    mem[ceil32(arg4.length) + 128] = mem[128]
+    require ext_code.size(address(_3))
+    staticcall address(_3).0x70a08231 with:
+            gas gas_remaining wei
+           args this.address
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_code.size(address(_3))
+    staticcall address(_3).0x70a08231 with:
+            gas gas_remaining wei
+           args this.address
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    if 0, ext_call.return_data[4 len 28] != mem[160]:
+        if ext_call.return_data[0] != mem[160]:
+            revert with 0, 'token has tnx fee'
+    else:
+        if ext_call.return_data[0] == mem[160]:
+            revert with 0, 'passed'
+}
+
+function pancakeCall(address arg1, uint256 arg2, uint256 arg3, bytes arg4) {
+    require calldata.size - 4 >= 128
+    require arg1 == arg1
+    require arg4 <= test266151307()
+    require arg4 + 35 < calldata.size
+    require arg4.length <= test266151307()
+    require arg4 + arg4.length + 36 <= calldata.size
+    mem[128 len arg4.length] = arg4[all]
+    mem[arg4.length + 128] = 0
+    _3 = mem[128]
+    mem[ceil32(arg4.length) + 128] = mem[128]
+    require ext_code.size(address(_3))
+    staticcall address(_3).0x70a08231 with:
+            gas gas_remaining wei
+           args this.address
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_code.size(address(_3))
+    staticcall address(_3).0x70a08231 with:
+            gas gas_remaining wei
+           args this.address
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    if 0, ext_call.return_data[4 len 28] != mem[160]:
+        if ext_call.return_data[0] != mem[160]:
+            revert with 0, 'token has tnx fee'
+    else:
+        if ext_call.return_data[0] == mem[160]:
+            revert with 0, 'passed'
+}
+
+function wakaSwapCall(address arg1, uint256 arg2, uint256 arg3, bytes arg4) {
+    require calldata.size - 4 >= 128
+    require arg1 == arg1
+    require arg4 <= test266151307()
+    require arg4 + 35 < calldata.size
+    require arg4.length <= test266151307()
+    require arg4 + arg4.length + 36 <= calldata.size
+    mem[128 len arg4.length] = arg4[all]
+    mem[arg4.length + 128] = 0
+    _3 = mem[128]
+    mem[ceil32(arg4.length) + 128] = mem[128]
+    require ext_code.size(address(_3))
+    staticcall address(_3).0x70a08231 with:
+            gas gas_remaining wei
+           args this.address
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_code.size(address(_3))
+    staticcall address(_3).0x70a08231 with:
+            gas gas_remaining wei
+           args this.address
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    if 0, ext_call.return_data[4 len 28] != mem[160]:
+        if ext_call.return_data[0] != mem[160]:
+            revert with 0, 'token has tnx fee'
+    else:
+        if ext_call.return_data[0] == mem[160]:
+            revert with 0, 'passed'
+}
+
+function uniswapV2Call(address arg1, uint256 arg2, uint256 arg3, bytes arg4) {
+    require calldata.size - 4 >= 128
+    require arg1 == arg1
+    require arg4 <= test266151307()
+    require arg4 + 35 < calldata.size
+    require arg4.length <= test266151307()
+    require arg4 + arg4.length + 36 <= calldata.size
+    mem[128 len arg4.length] = arg4[all]
+    mem[arg4.length + 128] = 0
+    _3 = mem[128]
+    mem[ceil32(arg4.length) + 128] = mem[128]
+    require ext_code.size(address(_3))
+    staticcall address(_3).0x70a08231 with:
+            gas gas_remaining wei
+           args this.address
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_code.size(address(_3))
+    staticcall address(_3).0x70a08231 with:
+            gas gas_remaining wei
+           args this.address
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    if 0, ext_call.return_data[4 len 28] != mem[160]:
+        if ext_call.return_data[0] != mem[160]:
+            revert with 0, 'token has tnx fee'
+    else:
+        if ext_call.return_data[0] == mem[160]:
+            revert with 0, 'passed'
+}
+
+function sub_7ccd1719(?) {
+    require calldata.size - 4 >= 64
+    require arg1 == address(arg1)
+    require arg2 == address(arg2)
+    require ext_code.size(address(arg2))
+    staticcall address(arg2).getReserves() with:
+            gas gas_remaining wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 96
+    require ext_call.return_data[0] == ext_call.return_data[18 len 14]
+    require ext_call.return_data[32] == ext_call.return_data[50 len 14]
+    require ext_call.return_data[64] == ext_call.return_data[92 len 4]
+    require ext_code.size(address(arg2))
+    staticcall address(arg2).token0() with:
+            gas gas_remaining wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_call.return_data[0] == ext_call.return_data[12 len 20]
+    require ext_code.size(address(arg2))
+    staticcall address(arg2).token0() with:
+            gas gas_remaining wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_call.return_data[0] == ext_call.return_data[12 len 20]
+    require ext_code.size(address(arg2))
+    if address(arg1) == ext_call.return_data[12 len 20]:
+        if address(arg1) == ext_call.return_data[12 len 20]:
+            call address(arg2).swap(uint256 arg1, uint256 arg2, address arg3, bytes arg4) with:
+                 gas gas_remaining wei
+                args Mask(112, 0, ext_call.return_data[0]) / 10, 0, address(this.address), 128, 64, arg1, Mask(112, 0, ext_call.return_data[0]) / 10
+        else:
+            call address(arg2).swap(uint256 arg1, uint256 arg2, address arg3, bytes arg4) with:
+                 gas gas_remaining wei
+                args 0, Mask(112, 0, ext_call.return_data[0]) / 10, address(this.address), 128, 64, arg1, Mask(112, 0, ext_call.return_data[0]) / 10
+    else:
+        if address(arg1) == ext_call.return_data[12 len 20]:
+            call address(arg2).swap(uint256 arg1, uint256 arg2, address arg3, bytes arg4) with:
+                 gas gas_remaining wei
+                args Mask(112, 0, ext_call.return_data[32]) / 10, 0, address(this.address), 128, 64, arg1, Mask(112, 0, ext_call.return_data[32]) / 10
+        else:
+            call address(arg2).swap(uint256 arg1, uint256 arg2, address arg3, bytes arg4) with:
+                 gas gas_remaining wei
+                args 0, Mask(112, 0, ext_call.return_data[32]) / 10, address(this.address), 128, 64, arg1, Mask(112, 0, ext_call.return_data[32]) / 10
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+}
+
+
+
+}
