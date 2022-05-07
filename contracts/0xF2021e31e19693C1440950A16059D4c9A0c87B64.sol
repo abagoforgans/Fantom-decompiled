67a94fc299df6a45906a4891776c0fea8041c7e0
bot <abagoforgans@users.noreply.github.com>
2022-05-07 14:38:51 +0000

    autocommit

---
+++ contracts/0xF2021e31e19693C1440950A16059D4c9A0c87B64.sol
@@ -0,0 +1,78 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+address owner;
+address stor1;
+address stor2;
+address stor3;
+
+function owner() payable {
+    return owner
+}
+
+function _fallback() payable {
+    revert
+}
+
+function renounceOwnership() payable {
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    emit OwnershipTransferred(owner, 0);
+    owner = 0
+}
+
+function sub_81657031(?) payable {
+    require calldata.size - 4 >= 96
+    require arg1 == address(arg1)
+    require arg2 == address(arg2)
+    require arg3 == address(arg3)
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    stor1 = address(arg1)
+    stor2 = address(arg2)
+    stor3 = address(arg3)
+}
+
+function transferOwnership(address arg1) payable {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'Ownable: new owner is the zero address'
+    emit OwnershipTransferred(owner, arg1);
+    owner = arg1
+}
+
+function getPrice() payable {
+    require ext_code.size(stor3)
+    staticcall stor3.balanceOf(address arg1) with:
+            gas gas_remaining wei
+           args stor1
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    if ext_call.return_data[0] > 0x119799812dea11197f27f0f6e885c8ba7eb31f476caf7411a863387:
+        revert with 0, 17
+    require ext_code.size(stor2)
+    staticcall stor2.balanceOf(address arg1) with:
+            gas gas_remaining wei
+           args stor1
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    if ext_call.return_data[0] and 10^18 > -1 / ext_call.return_data[0]:
+        revert with 0, 17
+    if not 10^12 * ext_call.return_data[0]:
+        revert with 0, 18
+    return (10^18 * ext_call.return_data[0] / 10^12 * ext_call.return_data[0])
+}
+
+
+
+}
