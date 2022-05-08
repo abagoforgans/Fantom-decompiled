908e287aadf6b57b9c17a33f67432318015cfff0
bot <abagoforgans@users.noreply.github.com>
2022-05-08 18:19:22 +0000

    autocommit

---
+++ contracts/0x5f41a02fb7c981E49dCc58F7116728C3d5Bb5183.sol
@@ -0,0 +1,233 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+mapping of uint256 balances;
+address owner;
+uint32 stor2;
+address routerAddress;
+uint256 stor2;
+uint32 stor3;
+address masonAddress;
+uint256 stor3;
+address sub_69e92367Address;
+
+function balances(address arg1) {
+    require calldata.size - 4 >= 32
+    return balances[arg1]
+}
+
+function sub_69e92367(?) {
+    return sub_69e92367Address
+}
+
+function owner() {
+    return owner
+}
+
+function mason() {
+    return address(masonAddress)
+}
+
+function router() {
+    return address(routerAddress)
+}
+
+function _fallback() payable {
+    revert
+}
+
+function withdraw() payable {
+    require msg.sender == owner
+    call msg.sender with:
+       value 10^18 * eth.balance(this.address) wei
+         gas 2300 * is_zero(value) wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    balances[msg.sender] = 0
+}
+
+function withdraw(uint256 arg1) {
+    require calldata.size - 4 >= 32
+    require msg.sender == owner
+    require eth.balance(this.address) >= 10^18 * arg1
+    call msg.sender with:
+       value 10^18 * arg1 wei
+         gas 2300 * is_zero(value) wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    balances[msg.sender] += -1 * 10^18 * arg1
+}
+
+function sub_8f7e0e97(?) {
+    require calldata.size - 4 >= 64
+    require msg.sender == owner
+    require ext_code.size(address(routerAddress))
+    call address(routerAddress).swapExactTokensForETHSupportingFeeOnTransferTokens(uint256 arg1, uint256 arg2, address[] arg3, address arg4, uint256 arg5) with:
+         gas gas_remaining wei
+        args 0, arg1, arg2, 160, this.address, block.timestamp, 0
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+}
+
+function sub_42d7eb9e(?) {
+    require calldata.size - 4 >= 128
+    require msg.sender == owner
+    require ext_code.size(address(routerAddress))
+    call address(routerAddress).removeLiquidityETHSupportingFeeOnTransferTokens(address arg1, uint256 arg2, uint256 arg3, uint256 arg4, address arg5, uint256 arg6) with:
+         gas gas_remaining wei
+        args 0, 0, arg1, arg2, arg3, this.address, block.timestamp
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    return ext_call.return_data[0]
+}
+
+function removeLiquidity(address arg1, address arg2, uint256 arg3, uint256 arg4, uint256 arg5) {
+    require calldata.size - 4 >= 160
+    require msg.sender == owner
+    require ext_code.size(address(routerAddress))
+    call address(routerAddress).removeLiquidity(address arg1, address arg2, uint256 arg3, uint256 arg4, uint256 arg5, address arg6, uint256 arg7) with:
+         gas gas_remaining wei
+        args 0, 0, address(arg2), arg3, arg4, arg5, this.address, block.timestamp
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 64
+    return ext_call.return_data[0], ext_call.return_data[32]
+}
+
+function sub_0320c213(?) payable {
+    require calldata.size - 4 >= 64
+    require arg2 <= 4294967296
+    require arg2 + 36 <= calldata.size
+    require arg2.length <= 4294967296 and arg2 + (32 * arg2.length) + 36 <= calldata.size
+    require msg.sender == owner
+    require ext_code.size(address(routerAddress))
+    call address(routerAddress).swapExactETHForTokensSupportingFeeOnTransferTokens(uint256 arg1, address[] arg2, address arg3, uint256 arg4) with:
+         gas gas_remaining wei
+        args 0, arg1, 128, this.address, block.timestamp, arg2.length, call.data[arg2 + 36 len floor32(arg2.length)]
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+}
+
+function sub_41f50925(?) {
+    require calldata.size - 4 >= 64
+    require msg.sender == owner
+    mem[196 len 64] = approve(address arg1, uint256 arg2), Mask(224, 0, stor3), uint32(stor3), 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+    call arg2 with:
+       funct uint32(stor3)
+         gas gas_remaining wei
+        args 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffff0000000000000000000000000000000000000000000000000000000000000000, mem[260 len 4]
+    if not return_data.size:
+        if not ext_call.success:
+            revert with 0, '!safeApprove'
+        if not approve(address arg1, uint256 arg2), Mask(224, 0, stor3):
+            revert with 0, '!safeApprove'
+    else:
+        mem[228 len return_data.size] = ext_call.return_data[0 len return_data.size]
+        if not ext_call.success:
+            revert with 0, '!safeApprove'
+        if return_data.size:
+            require return_data.size >= 32
+            if not mem[228]:
+                revert with 0, '!safeApprove'
+    require ext_code.size(address(masonAddress))
+    call address(masonAddress).stake(uint256 arg1) with:
+         gas gas_remaining wei
+        args arg1
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+}
+
+function swapExactTokensForTokensSupportingFeeOnTransferTokens(address arg1, address arg2, uint256 arg3) {
+    require calldata.size - 4 >= 96
+    require msg.sender == owner
+    require ext_code.size(arg1)
+    staticcall arg1.0xdd62ed3e with:
+            gas gas_remaining wei
+           args this.address, address(routerAddress)
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    if ext_call.return_data[0] >= arg3:
+        mem[388 len 0] = None
+        require ext_code.size(address(routerAddress))
+        call address(routerAddress).swapExactTokensForTokensSupportingFeeOnTransferTokens(uint256 arg1, uint256 arg2, address[] arg3, address arg4, uint256 arg5) with:
+             gas gas_remaining wei
+            args arg3, 0, 160, this.address, block.timestamp, 2, mem[388 len 64]
+    else:
+        mem[292 len 64] = approve(address arg1, uint256 arg2), Mask(224, 0, stor2), uint32(stor2), 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+        call arg1 with:
+           funct uint32(stor2)
+             gas gas_remaining wei
+            args 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffff0000000000000000000000000000000000000000000000000000000000000000, mem[356 len 4]
+        if not return_data.size:
+            require not ext_call.success
+            revert with 0, '!safeApprove'
+        mem[324 len return_data.size] = ext_call.return_data[0 len return_data.size]
+        if not ext_call.success:
+            revert with 0, '!safeApprove'
+        if return_data.size:
+            require return_data.size >= 32
+            if not mem[324]:
+                revert with 0, '!safeApprove'
+        mem[ceil32(return_data.size) + 489 len 0] = None
+        require ext_code.size(address(routerAddress))
+        call address(routerAddress).swapExactTokensForTokensSupportingFeeOnTransferTokens(uint256 arg1, uint256 arg2, address[] arg3, address arg4, uint256 arg5) with:
+             gas gas_remaining wei
+            args arg3, 0, 160, this.address, block.timestamp, 2, mem[ceil32(return_data.size) + 489 len 64]
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+}
+
+function addLiquidityETH(address arg1, uint256 arg2, uint256 arg3, uint256 arg4) payable {
+    require calldata.size - 4 >= 128
+    require msg.sender == owner
+    require ext_code.size(arg1)
+    staticcall arg1.0xdd62ed3e with:
+            gas gas_remaining wei
+           args this.address, address(routerAddress)
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    if ext_call.return_data[0] >= arg2:
+        require ext_code.size(address(routerAddress))
+        call address(routerAddress).addLiquidityETH(address arg1, uint256 arg2, uint256 arg3, uint256 arg4, address arg5, uint256 arg6) with:
+             gas gas_remaining wei
+            args 0, 0, arg2, arg3, arg4, this.address, block.timestamp
+    else:
+        mem[196 len 64] = approve(address arg1, uint256 arg2), Mask(224, 0, stor2), uint32(stor2), 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+        call arg1 with:
+           funct uint32(stor2)
+             gas gas_remaining wei
+            args 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffff0000000000000000000000000000000000000000000000000000000000000000, mem[260 len 4]
+        if not return_data.size:
+            if not ext_call.success:
+                revert with 0, '!safeApprove'
+            if not approve(address arg1, uint256 arg2), Mask(224, 0, stor2):
+                revert with 0, '!safeApprove'
+        else:
+            mem[228 len return_data.size] = ext_call.return_data[0 len return_data.size]
+            if not ext_call.success:
+                revert with 0, '!safeApprove'
+            if return_data.size:
+                require return_data.size >= 32
+                if not mem[228]:
+                    revert with 0, '!safeApprove'
+        require ext_code.size(address(routerAddress))
+        call address(routerAddress).addLiquidityETH(address arg1, uint256 arg2, uint256 arg3, uint256 arg4, address arg5, uint256 arg6) with:
+             gas gas_remaining wei
+            args address(arg1), arg2, arg3, arg4, this.address, block.timestamp
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 96
+    return ext_call.return_data[64]
+}
+
+
+
+}
