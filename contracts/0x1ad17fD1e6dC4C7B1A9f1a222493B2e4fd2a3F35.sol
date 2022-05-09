7c37d62895802c167d3e299a1e675ad89bf14017
bot <abagoforgans@users.noreply.github.com>
2022-05-09 01:20:55 +0000

    autocommit

---
+++ contracts/0x1ad17fD1e6dC4C7B1A9f1a222493B2e4fd2a3F35.sol
@@ -0,0 +1,104 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+#
+#  - sub_1b71afbd(?)
+#  - sub_35e8b7d5(?)
+#  - sub_5cc7647c(?)
+#  - sub_88f61f9f(?)
+#  - sub_89fecbd5(?)
+#  - sub_8e8d8152(?)
+#  - sub_ba2ebf96(?)
+#  - sub_cee6202c(?)
+#
+address owner;
+mapping of address stor2;
+mapping of uint8 stor3;
+
+function owner() {
+    return owner
+}
+
+function sub_caaae7f5(?) {
+    require calldata.size - 4 >= 32
+    require arg1 == address(arg1)
+    return bool(stor3[arg1])
+}
+
+function _fallback() payable {
+    revert
+}
+
+function renounceOwnership() {
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    emit OwnershipTransferred(owner, 0);
+    owner = 0
+}
+
+function setIsFeeOnTransfer(address arg1) {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    stor3[address(arg1)] = 1
+}
+
+function sub_5424888b(?) {
+    require calldata.size - 4 >= 96
+    require arg1 == address(arg1)
+    require arg2 == address(arg2)
+    require arg3 == address(arg3)
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    stor2[address(arg1)][address(arg2)] = address(arg3)
+}
+
+function transferOwnership(address arg1) {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'Ownable: new owner is the zero address'
+    emit OwnershipTransferred(owner, arg1);
+    owner = arg1
+}
+
+function withdraw(address arg1) {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    if not arg1:
+        call owner with:
+           value eth.balance(this.address) wei
+             gas 2300 * is_zero(value) wei
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+    else:
+        require ext_code.size(arg1)
+        staticcall arg1.balanceOf(address arg1) with:
+                gas gas_remaining wei
+               args this.address
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+        require return_data.size >= 32
+        require ext_code.size(arg1)
+        call arg1.transfer(address arg1, uint256 arg2) with:
+             gas gas_remaining wei
+            args owner, ext_call.return_data[0]
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+        require return_data.size >= 32
+        require ext_call.return_data[0] == bool(ext_call.return_data[0])
+}
+
+
+
+}
