51f9f52907af9dda2c0672975ee09d20cd3df02b
bot <abagoforgans@users.noreply.github.com>
2022-05-10 08:42:22 +0000

    autocommit

---
+++ contracts/0xBaf105C5123e52E3AB5C6EEeCf13ce9b95e22eB4.sol
@@ -0,0 +1,29 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+address owner;
+
+function getOwner() payable {
+    return owner
+}
+
+function _fallback() payable {
+    revert
+}
+
+function changeOwner(address arg1) payable {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'Caller is not owner'
+    emit OwnerSet(owner, arg1);
+    owner = arg1
+}
+
+
+
+}
