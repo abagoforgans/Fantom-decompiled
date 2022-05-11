518a3fe2d5da402b6ec2355114dfb14b270b057e
bot <abagoforgans@users.noreply.github.com>
2022-05-11 11:10:44 +0000

    autocommit

---
+++ contracts/0x19f34E3379002554A89BbE8Ac4423C26F8043925.sol
@@ -0,0 +1,302 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+#
+#  - harvest()
+#
+const sub_bf4b5ecc(?) = 800
+
+const TREASURY_FEE = 200
+
+const MAX_FEE = 1000
+
+
+address owner;
+address gaspriceAddress;
+address sub_9935d7beAddress;
+address sub_68f997b6Address;
+address treasuryAddress;
+address rewardPoolAddress;
+uint32 stor6;
+address unirouterAddress;
+array of address sub_ad381466;
+
+function unirouter() payable {
+    return address(unirouterAddress)
+}
+
+function treasury() payable {
+    return treasuryAddress
+}
+
+function rewardPool() payable {
+    return rewardPoolAddress
+}
+
+function sub_68f997b6(?) payable {
+    return sub_68f997b6Address
+}
+
+function gasprice() payable {
+    return gaspriceAddress
+}
+
+function owner() payable {
+    return owner
+}
+
+function sub_9935d7be(?) payable {
+    return sub_9935d7beAddress
+}
+
+function sub_ad381466(?) payable {
+    require calldata.size - 4 >= 32
+    require arg1 < sub_ad381466.length
+    return sub_ad381466[arg1]
+}
+
+function _fallback() payable {
+    revert
+}
+
+function renounceOwnership() payable {
+    if owner != msg.sender:
+        revert with 0, 'wOwnable: caller is not the owne'
+    emit OwnershipTransferred(owner, 0);
+    owner = 0
+}
+
+function setTreasury(address arg1) payable {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'wOwnable: caller is not the owne'
+    emit NewTreasury(treasuryAddress, arg1);
+    treasuryAddress = arg1
+}
+
+function setRewardPool(address arg1) payable {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'wOwnable: caller is not the owne'
+    emit NewRewardPool(rewardPoolAddress, arg1);
+    rewardPoolAddress = arg1
+}
+
+function transferOwnership(address arg1) payable {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'wOwnable: caller is not the owne'
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    38,
+                    0xfe4f776e61626c653a206e6577206f776e657220697320746865207a65726f20616464726573,
+                    mem[202 len 26]
+    emit OwnershipTransferred(owner, arg1);
+    owner = arg1
+}
+
+function inCaseTokensGetStuck(address arg1) payable {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'wOwnable: caller is not the owne'
+    if sub_9935d7beAddress == arg1:
+        revert with 0, '!safe'
+    require ext_code.size(arg1)
+    staticcall arg1.0x70a08231 with:
+            gas gas_remaining wei
+           args this.address
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    if eth.balance(this.address) < 0:
+        revert with 0, 32, 38, 0x73416464726573733a20696e73756666696369656e742062616c616e636520666f722063616c, mem[366 len 26]
+    if not ext_code.size(arg1):
+        revert with 0, 'Address: call to non-contract'
+    mem[260 len 64] = unknown_0xa9059cbb(?????), msg.sender, ext_call.return_data[0 len 28]
+    mem[324 len 0] = 0
+    call arg1 with:
+       funct uint32(msg.sender)
+         gas gas_remaining wei
+        args Mask(480, -256, ext_call.return_data[0 len 28]) << 256, mem[324 len 4]
+    if not return_data.size:
+        if not ext_call.success:
+            revert with unknown_0xa9059cbb(?????), msg.sender, ext_call.return_data[0]
+        if not unknown_0xa9059cbb(?????), Mask(224, 32, msg.sender) >> 32:
+            revert with 0, 32, 42, 0x725361666545524332303a204552433230206f7065726174696f6e20646964206e6f7420737563636565, mem[370 len 22]
+    else:
+        mem[292 len return_data.size] = ext_call.return_data[0 len return_data.size]
+        if not ext_call.success:
+            if return_data.size:
+                revert with ext_call.return_data[0 len return_data.size]
+            revert with 0, 'SafeERC20: low-level call failed'
+        if return_data.size:
+            require return_data.size >= 32
+            if not mem[292]:
+                revert with 0, 
+                            32,
+                            42,
+                            0x725361666545524332303a204552433230206f7065726174696f6e20646964206e6f7420737563636565,
+                            mem[ceil32(return_data.size) + 371 len 22]
+}
+
+function setUnirouter(address arg1) payable {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'wOwnable: caller is not the owne'
+    emit NewUnirouter(address(unirouterAddress), arg1);
+    require ext_code.size(sub_9935d7beAddress)
+    staticcall sub_9935d7beAddress.0xdd62ed3e with:
+            gas gas_remaining wei
+           args this.address, arg1
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    if ext_call.return_data[0]:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    54,
+                    0x645361666545524332303a20617070726f76652066726f6d206e6f6e2d7a65726f20746f206e6f6e2d7a65726f20616c6c6f77616e63,
+                    mem[218 len 10]
+    if eth.balance(this.address) < 0:
+        revert with 0, 32, 38, 0x73416464726573733a20696e73756666696369656e742062616c616e636520666f722063616c, mem[366 len 26]
+    if not ext_code.size(sub_9935d7beAddress):
+        revert with 0, 'Address: call to non-contract'
+    mem[260 len 64] = approve(address arg1, uint256 arg2), address(arg1) << 64, 0, 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+    mem[324 len 0] = 0
+    call sub_9935d7beAddress with:
+         gas gas_remaining wei
+        args Mask(480, -256, approve(address arg1, uint256 arg2), address(arg1) << 64, 0, 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffff) << 256, mem[324 len 4]
+    if not return_data.size:
+        if not ext_call.success:
+            revert with approve(address arg1, uint256 arg2), address(arg1) << 64, 0, -1
+        if not approve(address arg1, uint256 arg2), address(arg1) << 64:
+            revert with 0, 32, 42, 0x725361666545524332303a204552433230206f7065726174696f6e20646964206e6f7420737563636565, mem[370 len 22]
+        if eth.balance(this.address) < 0:
+            revert with 0, 32, 38, 0x73416464726573733a20696e73756666696369656e742062616c616e636520666f722063616c, mem[530 len 26]
+        if not ext_code.size(sub_9935d7beAddress):
+            revert with 0, 'Address: call to non-contract'
+        mem[424 len 64] = 0, address(unirouterAddress), 0
+        call sub_9935d7beAddress with:
+           funct uint32(stor6)
+             gas gas_remaining wei
+            args 0, mem[360 len 28], mem[488 len 4]
+        if not return_data.size:
+            if not ext_call.success:
+                revert with approve(address arg1, uint256 arg2), address(arg1) << 64, 0, -1
+            if not approve(address arg1, uint256 arg2), address(arg1) << 64:
+                revert with 0, 32, 42, 0x725361666545524332303a204552433230206f7065726174696f6e20646964206e6f7420737563636565, mem[534 len 22]
+        else:
+            mem[456 len return_data.size] = ext_call.return_data[0 len return_data.size]
+            if not ext_call.success:
+                if return_data.size:
+                    revert with ext_call.return_data[0 len return_data.size]
+                revert with 0, 'SafeERC20: low-level call failed'
+            if return_data.size:
+                require return_data.size >= 32
+                if not mem[456]:
+                    revert with 0, 
+                                32,
+                                42,
+                                0x725361666545524332303a204552433230206f7065726174696f6e20646964206e6f7420737563636565,
+                                mem[ceil32(return_data.size) + 535 len 22]
+    else:
+        mem[292 len return_data.size] = ext_call.return_data[0 len return_data.size]
+        if not ext_call.success:
+            if return_data.size:
+                revert with ext_call.return_data[0 len return_data.size]
+            revert with 0, 'SafeERC20: low-level call failed'
+        if not return_data.size:
+            if eth.balance(this.address) < 0:
+                revert with 0, 
+                            32,
+                            38,
+                            0x73416464726573733a20696e73756666696369656e742062616c616e636520666f722063616c,
+                            mem[ceil32(return_data.size) + 531 len 26]
+            if not ext_code.size(sub_9935d7beAddress):
+                revert with 0, 'Address: call to non-contract'
+            mem[ceil32(return_data.size) + 425 len 64] = 0, address(unirouterAddress), 0
+            call sub_9935d7beAddress with:
+               funct uint32(stor6)
+                 gas gas_remaining wei
+                args 0, mem[ceil32(return_data.size) + 361 len 28], mem[ceil32(return_data.size) + 489 len 4]
+            if not return_data.size:
+                if not ext_call.success:
+                    revert with approve(address arg1, uint256 arg2), address(arg1) << 64, 0, -1
+                if not approve(address arg1, uint256 arg2), address(arg1) << 64:
+                    revert with 0, 
+                                32,
+                                42,
+                                0x725361666545524332303a204552433230206f7065726174696f6e20646964206e6f7420737563636565,
+                                mem[ceil32(return_data.size) + 535 len 22]
+            else:
+                mem[ceil32(return_data.size) + 457 len return_data.size] = ext_call.return_data[0 len return_data.size]
+                if not ext_call.success:
+                    if return_data.size:
+                        revert with ext_call.return_data[0 len return_data.size]
+                    revert with 0, 'SafeERC20: low-level call failed'
+                if return_data.size:
+                    require return_data.size >= 32
+                    if not mem[ceil32(return_data.size) + 457]:
+                        revert with 0, 
+                                    32,
+                                    42,
+                                    0x725361666545524332303a204552433230206f7065726174696f6e20646964206e6f7420737563636565,
+                                    mem[(2 * ceil32(return_data.size)) + 536 len 22]
+        else:
+            require return_data.size >= 32
+            if not mem[292]:
+                revert with 0, 
+                            32,
+                            42,
+                            0x725361666545524332303a204552433230206f7065726174696f6e20646964206e6f7420737563636565,
+                            mem[ceil32(return_data.size) + 371 len 22]
+            if eth.balance(this.address) < 0:
+                revert with 0, 
+                            32,
+                            38,
+                            0x73416464726573733a20696e73756666696369656e742062616c616e636520666f722063616c,
+                            mem[ceil32(return_data.size) + 531 len 26]
+            if not ext_code.size(sub_9935d7beAddress):
+                revert with 0, 'Address: call to non-contract'
+            mem[ceil32(return_data.size) + 425 len 64] = 0, address(unirouterAddress), 0
+            call sub_9935d7beAddress with:
+               funct uint32(stor6)
+                 gas gas_remaining wei
+                args 0, mem[ceil32(return_data.size) + 361 len 28], mem[ceil32(return_data.size) + 489 len 4]
+            if not return_data.size:
+                if not ext_call.success:
+                    revert with approve(address arg1, uint256 arg2), address(arg1) << 64, 0, -1
+                if not approve(address arg1, uint256 arg2), address(arg1) << 64:
+                    revert with 0, 
+                                32,
+                                42,
+                                0x725361666545524332303a204552433230206f7065726174696f6e20646964206e6f7420737563636565,
+                                mem[ceil32(return_data.size) + 535 len 22]
+            else:
+                mem[ceil32(return_data.size) + 457 len return_data.size] = ext_call.return_data[0 len return_data.size]
+                if not ext_call.success:
+                    if return_data.size:
+                        revert with ext_call.return_data[0 len return_data.size]
+                    revert with 0, 
+                                'SafeERC20: low-level call failed',
+                                mem[(2 * ceil32(return_data.size)) + 526 len (2 * ceil32(return_data.size)) - (2 * ceil32(return_data.size))]
+                if return_data.size:
+                    require return_data.size >= 32
+                    if not mem[ceil32(return_data.size) + 457]:
+                        revert with 0, 
+                                    32,
+                                    42,
+                                    0x725361666545524332303a204552433230206f7065726174696f6e20646964206e6f7420737563636565,
+                                    mem[(2 * ceil32(return_data.size)) + 536 len (2 * ceil32(return_data.size)) + (-2 * ceil32(return_data.size)) + 22]
+    address(unirouterAddress) = arg1
+}
+
+
+
+}
