fec95eab0929b1f6b1394d1df0bc08227943e75c
bot <abagoforgans@users.noreply.github.com>
2022-05-13 21:34:43 +0000

    autocommit

---
+++ contracts/0xC6Cd71D680258FCb028e296d7e30F2CC5639F97c.sol
@@ -0,0 +1,18 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+const sayHello = 'hello world', 0
+
+
+function _fallback() payable {
+    revert
+}
+
+
+
+}
