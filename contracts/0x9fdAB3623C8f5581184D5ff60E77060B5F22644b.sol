f804a47a3e55aa55f3d78ddb337330eeac5aba25
bot <abagoforgans@users.noreply.github.com>
2022-05-15 22:57:11 +0000

    autocommit

---
+++ contracts/0x9fdAB3623C8f5581184D5ff60E77060B5F22644b.sol
@@ -0,0 +1,35 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+mapping of uint256 stor0;
+address sub_12ee98bbAddress;
+
+function sub_12ee98bb(?) payable {
+    return sub_12ee98bbAddress
+}
+
+function _fallback() payable {
+    revert
+}
+
+function sub_8a4c998b(?) payable {
+    require calldata.size - 4 >= 32
+    return ('signextend', 15, ('signextend', 15, ('stor', ('map', ('param', 'arg1'), ('name', 'stor0', 0)))))
+}
+
+function swap(address arg1, address arg2, uint256 arg3, uint256 arg4) payable {
+    require calldata.size - 4 >= 128
+    call sub_12ee98bbAddress with:
+       funct uint32(arg3)
+         gas gas_remaining wei
+        args Mask(224, 32, arg4) << 736, mem[388 len 4]
+}
+
+
+
+}
---
+++ contracts/0xCD88B5022699D3848949DD9b15981A1a2e84AEc6.sol
@@ -0,0 +1,39 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+mapping of uint256 stor0;
+address sub_12ee98bbAddress;
+
+function sub_12ee98bb(?) payable {
+    return sub_12ee98bbAddress
+}
+
+function _fallback() payable {
+    revert
+}
+
+function sub_8a4c998b(?) payable {
+    require calldata.size - 4 >= 32
+    return ('signextend', 15, ('signextend', 15, ('stor', ('map', ('param', 'arg1'), ('name', 'stor0', 0)))))
+}
+
+function swap(address arg1, address arg2, uint256 arg3, uint256 arg4) payable {
+    require calldata.size - 4 >= 128
+    require ext_code.size(sub_12ee98bbAddress)
+    call sub_12ee98bbAddress.exchange(int128 arg1, int128 arg2, uint256 arg3, uint256 arg4) with:
+         gas gas_remaining wei
+        args 0, uint32(('signextend', 15, ('signextend', 15, ('stor', ('map', ('mask_shl', 160, 0, 0, ('param', 'arg1')), ('name', 'stor0', 0)))))), ('signextend', 15, ('signextend', 15, ('stor', ('map', ('mask_shl', 160, 0, 0, ('param', 'arg2')), ('name', 'stor0', 0))))), arg3, arg4
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    return ext_call.return_data[0]
+}
+
+
+
+}
