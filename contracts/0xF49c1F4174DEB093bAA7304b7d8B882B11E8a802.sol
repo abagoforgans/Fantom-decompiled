016ec8a626d18559d672973e76f30a0521987110
bot <abagoforgans@users.noreply.github.com>
2022-05-17 00:48:50 +0000

    autocommit

---
+++ contracts/0xF49c1F4174DEB093bAA7304b7d8B882B11E8a802.sol
@@ -0,0 +1,36 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+mapping of uint256 stor0;
+address sub_12ee98bbAddress;
+
+function sub_12ee98bb(?) payable {
+    return sub_12ee98bbAddress
+}
+
+function _fallback() payable {
+    revert
+}
+
+function sub_8a4c998b(?) payable {
+    require calldata.size - 4 >= 32
+    return ('signextend', 15, ('signextend', 15, ('stor', ('map', ('param', 'arg1'), ('name', 'stor0', 0)))))
+}
+
+function sub_4b26af79(?) payable {
+    require calldata.size - 4 >= 128
+    delegate sub_12ee98bbAddress with:
+       funct arg1
+         gas gas_remaining wei
+        args Mask(224, 32, arg2) << 736, mem[388 len 4]
+    return 1234
+}
+
+
+
+}
