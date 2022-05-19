67a9a251eb088078053b8aa377553cf6dd33d96f
bot <abagoforgans@users.noreply.github.com>
2022-05-19 18:40:15 +0000

    autocommit

---
+++ contracts/0xe028c1C786ac8E442b965356fdf8D3CD44EE3F86.sol
@@ -0,0 +1,461 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+#
+#  - transferFrom(address arg1, address arg2, uint256 arg3)
+#  - transfer(address arg1, uint256 arg2)
+#
+const decimals = 18
+
+
+mapping of uint256 balanceOf;
+mapping of uint256 allowance;
+uint256 totalSupply;
+array of uint256 name;
+array of uint256 symbol;
+address owner;
+address uniswapV2RouterAddress;
+address uniswapV2PairAddress;
+address dividendTrackerAddress;
+address deadWalletAddress;
+address sub_b5ee45c1Address;
+uint256 swapTokensAtAmount;
+mapping of uint8 stor12;
+uint256 sub_5ccd370e;
+uint256 liquidityFee;
+uint256 marketingFee;
+uint256 totalFees;
+address _marketingWalletAddress;
+uint256 gasForProcessing;
+mapping of uint8 stor19;
+mapping of uint8 stor20;
+
+function name() {
+    return name[0 len name.length]
+}
+
+function totalFees() {
+    return totalFees
+}
+
+function uniswapV2Router() {
+    return uniswapV2RouterAddress
+}
+
+function totalSupply() {
+    return totalSupply
+}
+
+function _isBlacklisted(address arg1) {
+    require calldata.size - 4 >= 32
+    return bool(stor12[arg1])
+}
+
+function dividendTracker() {
+    return dividendTrackerAddress
+}
+
+function _marketingWalletAddress() {
+    return _marketingWalletAddress
+}
+
+function uniswapV2Pair() {
+    return uniswapV2PairAddress
+}
+
+function isExcludedFromFees(address arg1) {
+    require calldata.size - 4 >= 32
+    return bool(stor19[address(arg1)])
+}
+
+function sub_5ccd370e(?) {
+    return sub_5ccd370e
+}
+
+function marketingFee() {
+    return marketingFee
+}
+
+function balanceOf(address arg1) {
+    require calldata.size - 4 >= 32
+    return balanceOf[address(arg1)]
+}
+
+function deadWallet() {
+    return deadWalletAddress
+}
+
+function owner() {
+    return owner
+}
+
+function symbol() {
+    return symbol[0 len symbol.length]
+}
+
+function liquidityFee() {
+    return liquidityFee
+}
+
+function gasForProcessing() {
+    return gasForProcessing
+}
+
+function sub_b5ee45c1(?) {
+    return sub_b5ee45c1Address
+}
+
+function automatedMarketMakerPairs(address arg1) {
+    require calldata.size - 4 >= 32
+    return bool(stor20[arg1])
+}
+
+function allowance(address arg1, address arg2) {
+    require calldata.size - 4 >= 64
+    return allowance[address(arg1)][address(arg2)]
+}
+
+function swapTokensAtAmount() {
+    return swapTokensAtAmount
+}
+
+function _fallback() payable {
+    revert
+}
+
+function renounceOwnership() {
+    if owner != msg.sender:
+        revert with 0, 'eOwnable: caller is not the owne'
+    emit OwnershipTransferred(owner, 0);
+    owner = 0
+}
+
+function setMarketingWallet(address arg1) {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'eOwnable: caller is not the owne'
+    _marketingWalletAddress = arg1
+}
+
+function blacklistAddress(address arg1, bool arg2) {
+    require calldata.size - 4 >= 64
+    if owner != msg.sender:
+        revert with 0, 'eOwnable: caller is not the owne'
+    stor12[address(arg1)] = uint8(arg2)
+}
+
+function getClaimWait() {
+    require ext_code.size(dividendTrackerAddress)
+    staticcall dividendTrackerAddress.claimWait() with:
+            gas gas_remaining wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    return ext_call.return_data[0]
+}
+
+function claim() {
+    require ext_code.size(dividendTrackerAddress)
+    call dividendTrackerAddress.processAccount(address arg1, bool arg2) with:
+         gas gas_remaining wei
+        args msg.sender, 0
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+}
+
+function getLastProcessedIndex() {
+    require ext_code.size(dividendTrackerAddress)
+    staticcall dividendTrackerAddress.0xe7841ec0 with:
+            gas gas_remaining wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    return ext_call.return_data[0]
+}
+
+function getTotalDividendsDistributed() {
+    require ext_code.size(dividendTrackerAddress)
+    staticcall dividendTrackerAddress.totalDividendsDistributed() with:
+            gas gas_remaining wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    return ext_call.return_data[0]
+}
+
+function getNumberOfDividendTokenHolders() {
+    require ext_code.size(dividendTrackerAddress)
+    staticcall dividendTrackerAddress.getNumberOfTokenHolders() with:
+            gas gas_remaining wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    return ext_call.return_data[0]
+}
+
+function sub_4aae9b48(?) {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'eOwnable: caller is not the owne'
+    sub_5ccd370e = arg1
+    if liquidityFee + arg1 < arg1:
+        revert with 0, 'SafeMath: addition overflow'
+    if marketingFee < 0:
+        revert with 0, 'SafeMath: addition overflow'
+    totalFees = marketingFee + liquidityFee + arg1
+}
+
+function updateClaimWait(uint256 arg1) {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'eOwnable: caller is not the owne'
+    require ext_code.size(dividendTrackerAddress)
+    call dividendTrackerAddress.0xe98030c7 with:
+         gas gas_remaining wei
+        args arg1
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+}
+
+function dividendTokenBalanceOf(address arg1) {
+    require calldata.size - 4 >= 32
+    require ext_code.size(dividendTrackerAddress)
+    staticcall dividendTrackerAddress.0x70a08231 with:
+            gas gas_remaining wei
+           args arg1
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    return ext_call.return_data[0]
+}
+
+function withdrawableDividendOf(address arg1) {
+    require calldata.size - 4 >= 32
+    require ext_code.size(dividendTrackerAddress)
+    staticcall dividendTrackerAddress.0xa8b9d240 with:
+            gas gas_remaining wei
+           args arg1
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    return ext_call.return_data[0]
+}
+
+function excludeFromDividends(address arg1) {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'eOwnable: caller is not the owne'
+    require ext_code.size(dividendTrackerAddress)
+    call dividendTrackerAddress.0x31e79db0 with:
+         gas gas_remaining wei
+        args arg1
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+}
+
+function setLiquiditFee(uint256 arg1) {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'eOwnable: caller is not the owne'
+    liquidityFee = arg1
+    if arg1 + sub_5ccd370e < sub_5ccd370e:
+        revert with 0, 'SafeMath: addition overflow'
+    if marketingFee < 0:
+        revert with 0, 'SafeMath: addition overflow'
+    totalFees = marketingFee + arg1 + sub_5ccd370e
+}
+
+function setMarketingFee(uint256 arg1) {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'eOwnable: caller is not the owne'
+    marketingFee = arg1
+    if liquidityFee + sub_5ccd370e < sub_5ccd370e:
+        revert with 0, 'SafeMath: addition overflow'
+    if arg1 < 0:
+        revert with 0, 'SafeMath: addition overflow'
+    totalFees = arg1 + liquidityFee + sub_5ccd370e
+}
+
+function processDividendTracker(uint256 arg1) {
+    require calldata.size - 4 >= 32
+    require ext_code.size(dividendTrackerAddress)
+    call dividendTrackerAddress.process(uint256 arg1) with:
+         gas gas_remaining wei
+        args arg1
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 96
+    emit ProcessedDividendTracker(ext_call.return_data[0], ext_call.return_data[32], ext_call.return_data[64], arg1, 0, tx.origin);
+}
+
+function transferOwnership(address arg1) {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'eOwnable: caller is not the owne'
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    38,
+                    0x734f776e61626c653a206e6577206f776e657220697320746865207a65726f20616464726573,
+                    mem[202 len 26]
+    emit OwnershipTransferred(owner, arg1);
+    owner = arg1
+}
+
+function getAccountDividendsInfo(address arg1) {
+    require calldata.size - 4 >= 32
+    require ext_code.size(dividendTrackerAddress)
+    staticcall dividendTrackerAddress.getAccount(address arg1) with:
+            gas gas_remaining wei
+           args arg1
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 256
+    return ext_call.return_data[12 len 20], 
+           ext_call.return_data[32],
+           ext_call.return_data[64],
+           ext_call.return_data[96],
+           ext_call.return_data[128],
+           ext_call.return_data[160],
+           ext_call.return_data[192],
+           ext_call.return_data[224]
+}
+
+function getAccountDividendsInfoAtIndex(uint256 arg1) {
+    require calldata.size - 4 >= 32
+    require ext_code.size(dividendTrackerAddress)
+    staticcall dividendTrackerAddress.getAccountAtIndex(uint256 arg1) with:
+            gas gas_remaining wei
+           args arg1
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 256
+    return ext_call.return_data[12 len 20], 
+           ext_call.return_data[32],
+           ext_call.return_data[64],
+           ext_call.return_data[96],
+           ext_call.return_data[128],
+           ext_call.return_data[160],
+           ext_call.return_data[192],
+           ext_call.return_data[224]
+}
+
+function approve(address arg1, uint256 arg2) {
+    require calldata.size - 4 >= 64
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    36,
+                    0x7345524332303a20617070726f76652066726f6d20746865207a65726f20616464726573,
+                    mem[200 len 28]
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    34,
+                    0x7345524332303a20617070726f766520746f20746865207a65726f20616464726573,
+                    mem[198 len 30]
+    allowance[address(msg.sender)][address(arg1)] = arg2
+    emit Approval(arg2, msg.sender, arg1);
+    return 1
+}
+
+function decreaseAllowance(address arg1, uint256 arg2) {
+    require calldata.size - 4 >= 64
+    if arg2 > allowance[address(msg.sender)][address(arg1)]:
+        revert with 0, 
+                    32,
+                    37,
+                    0x6545524332303a2064656372656173656420616c6c6f77616e63652062656c6f77207a6572,
+                    mem[165 len 27],
+                    mem[219 len 5]
+    if not msg.sender:
+        revert with 0, 32, 36, 0x7345524332303a20617070726f76652066726f6d20746865207a65726f20616464726573, mem[296 len 28]
+    if not arg1:
+        revert with 0, 32, 34, 0x7345524332303a20617070726f766520746f20746865207a65726f20616464726573, mem[294 len 30]
+    allowance[address(msg.sender)][address(arg1)] -= arg2
+    emit Approval((allowance[address(msg.sender)][address(arg1)] - arg2), msg.sender, arg1);
+    return 1
+}
+
+function increaseAllowance(address arg1, uint256 arg2) {
+    require calldata.size - 4 >= 64
+    if arg2 + allowance[address(msg.sender)][address(arg1)] < allowance[address(msg.sender)][address(arg1)]:
+        revert with 0, 'SafeMath: addition overflow'
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    36,
+                    0x7345524332303a20617070726f76652066726f6d20746865207a65726f20616464726573,
+                    mem[200 len 28]
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    34,
+                    0x7345524332303a20617070726f766520746f20746865207a65726f20616464726573,
+                    mem[198 len 30]
+    allowance[address(msg.sender)][address(arg1)] += arg2
+    emit Approval((arg2 + allowance[address(msg.sender)][address(arg1)]), msg.sender, arg1);
+    return 1
+}
+
+function updateGasForProcessing(uint256 arg1) {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'eOwnable: caller is not the owne'
+    if arg1 < 200000:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    64,
+                    0x73626162795453484152453a20676173466f7250726f63657373696e67206d757374206265206265747765656e203230302c30303020616e64203530302c3030
+    if arg1 > 500000:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    64,
+                    0x73626162795453484152453a20676173466f7250726f63657373696e67206d757374206265206265747765656e203230302c30303020616e64203530302c3030
+    if arg1 == gasForProcessing:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    56,
+                    0x65626162795453484152453a2043616e6e6f742075706461746520676173466f7250726f63657373696e6720746f2073616d652076616c75,
+                    mem[220 len 8]
+    emit GasForProcessingUpdated(arg1, gasForProcessing);
+    gasForProcessing = arg1
+}
+
+function setAutomatedMarketMakerPair(address arg1, bool arg2) {
+    require calldata.size - 4 >= 64
+    if owner != msg.sender:
+        revert with 0, 'eOwnable: caller is not the owne'
+    if uniswapV2PairAddress == arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    76,
+                    0x73626162795453484152453a205468652053706f6f6b7920706169722063616e6e6f742062652072656d6f7665642066726f6d206175746f6d617465644d61726b65744d616b657250616972,
+                    mem[240 len 20]
+    if arg2 == bool(stor20[address(arg1)]):
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    68,
+                    0x73626162795453484152453a204175746f6d61746564206d61726b6574206d616b6572207061697220697320616c72656164792073657420746f20746861742076616c75,
+                    mem[232 len 28]
+    stor20[address(arg1)] = uint8(arg2)
+    if arg2:
+        require ext_code.size(dividendTrackerAddress)
+        call dividendTrackerAddress.0x31e79db0 with:
+             gas gas_remaining wei
+            args arg1
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+    emit SetAutomatedMarketMakerPair(arg1, arg2);
+}
+
+
+
+}
