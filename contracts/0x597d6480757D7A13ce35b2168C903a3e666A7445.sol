7c278d105a32172081693e24f70da2b5edc33940
bot <abagoforgans@users.noreply.github.com>
2022-05-21 19:14:04 +0000

    autocommit

---
+++ contracts/0x597d6480757D7A13ce35b2168C903a3e666A7445.sol
@@ -0,0 +1,440 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+#
+#  - unpause()
+#  - harvest()
+#  - panic()
+#  - pause()
+#
+const PERCENT_DIVISOR = 10000
+
+const MAX_FEE = 500
+
+
+uint8 paused; offset 160
+address owner;
+address wftmAddress;
+address rewardTokenAddress;
+address lpPairAddress;
+address lpToken0Address;
+address lpToken1Address;
+address uniRouterAddress;
+uint8 stor7;
+uint8 poolId; offset 160
+address masterChefAddress;
+address treasuryAddress;
+uint32 stor9;
+address vaultAddress;
+uint256 stor9;
+uint256 callFee;
+uint256 treasuryFee;
+uint256 sub_d68e1302;
+uint256 totalFee;
+array of address sub_65108ea4;
+array of address sub_d0f2e818;
+array of address sub_59a9d23a;
+
+function totalFee() payable {
+    return totalFee
+}
+
+function poolId() payable {
+    return poolId
+}
+
+function lpPair() payable {
+    return lpPairAddress
+}
+
+function masterChef() payable {
+    return masterChefAddress
+}
+
+function sub_59a9d23a(?) payable {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    require arg1 < sub_59a9d23a.length
+    return sub_59a9d23a[arg1]
+}
+
+function paused() payable {
+    return bool(paused)
+}
+
+function lpToken0() payable {
+    return lpToken0Address
+}
+
+function treasury() payable {
+    return treasuryAddress
+}
+
+function sub_65108ea4(?) payable {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    require arg1 < sub_65108ea4.length
+    return sub_65108ea4[arg1]
+}
+
+function lpToken1() payable {
+    return lpToken1Address
+}
+
+function owner() payable {
+    return owner
+}
+
+function callFee() payable {
+    return callFee
+}
+
+function uniRouter() payable {
+    return uniRouterAddress
+}
+
+function treasuryFee() payable {
+    return treasuryFee
+}
+
+function sub_d0f2e818(?) payable {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    require arg1 < sub_d0f2e818.length
+    return sub_d0f2e818[arg1]
+}
+
+function sub_d68e1302(?) payable {
+    return sub_d68e1302
+}
+
+function wftm() payable {
+    return wftmAddress
+}
+
+function rewardToken() payable {
+    return rewardTokenAddress
+}
+
+function vault() payable {
+    return address(vaultAddress)
+}
+
+function _fallback() payable {
+    revert
+}
+
+function renounceOwnership() payable {
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    owner = 0
+    emit OwnershipTransferred(owner, 0);
+}
+
+function sub_d32b9604(?) payable {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    if arg1 > 500:
+        revert with 0, 'Fee Too High'
+    totalFee = arg1
+    emit 0x2e59d502: totalFee
+}
+
+function sub_f88fb689(?) payable {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    callFee = arg1
+    if 10000 < callFee:
+        revert with 'NH{q', 17
+    treasuryFee = -callFee + 10000
+    emit 0x6a730aa6: callFee, treasuryFee
+}
+
+function transferOwnership(address arg1) payable {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'Ownable: new owner is the zero address'
+    owner = arg1
+    emit OwnershipTransferred(owner, arg1);
+}
+
+function balanceOfLpPair() payable {
+    require ext_code.size(lpPairAddress)
+    staticcall lpPairAddress.balanceOf(address arg1) with:
+            gas gas_remaining wei
+           args this.address
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_call.return_data[0] == ext_call.return_data[0]
+    return ext_call.return_data[0]
+}
+
+function balanceOfPool() payable {
+    require ext_code.size(masterChefAddress)
+    staticcall masterChefAddress.userInfo(uint256 arg1, address arg2) with:
+            gas gas_remaining wei
+           args stor7, this.address
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 64
+    require ext_call.return_data[0] == ext_call.return_data[0]
+    require ext_call.return_data[32] == ext_call.return_data[32]
+    return ext_call.return_data[0]
+}
+
+function deposit() payable {
+    if paused:
+        revert with 0, 'Pausable: paused'
+    require ext_code.size(lpPairAddress)
+    staticcall lpPairAddress.balanceOf(address arg1) with:
+            gas gas_remaining wei
+           args this.address
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_call.return_data[0] == ext_call.return_data[0]
+    if ext_call.return_data[0] > 0:
+        require ext_code.size(masterChefAddress)
+        call masterChefAddress.deposit(uint256 arg1, uint256 arg2) with:
+             gas gas_remaining wei
+            args stor7, ext_call.return_data[0]
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+}
+
+function balanceOf() payable {
+    require ext_code.size(masterChefAddress)
+    staticcall masterChefAddress.userInfo(uint256 arg1, address arg2) with:
+            gas gas_remaining wei
+           args stor7, this.address
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 64
+    require ext_call.return_data[0] == ext_call.return_data[0]
+    require ext_call.return_data[32] == ext_call.return_data[32]
+    require ext_code.size(lpPairAddress)
+    staticcall lpPairAddress.balanceOf(address arg1) with:
+            gas gas_remaining wei
+           args this.address
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_call.return_data[0] == ext_call.return_data[0]
+    if ext_call.return_data[0] > -ext_call.return_data[0] - 1:
+        revert with 'NH{q', 17
+    return (2 * ext_call.return_data[0])
+}
+
+function retireStrat() payable {
+    if address(vaultAddress) != msg.sender:
+        revert with 0, '!vault'
+    require ext_code.size(masterChefAddress)
+    call masterChefAddress.emergencyWithdraw(uint256 arg1) with:
+         gas gas_remaining wei
+        args poolId
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require ext_code.size(lpPairAddress)
+    staticcall lpPairAddress.balanceOf(address arg1) with:
+            gas gas_remaining wei
+           args this.address
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_call.return_data[0] == ext_call.return_data[0]
+    require ext_code.size(lpPairAddress)
+    call lpPairAddress.transfer(address arg1, uint256 arg2) with:
+         gas gas_remaining wei
+        args address(vaultAddress), ext_call.return_data[0]
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_call.return_data[0] == bool(ext_call.return_data[0])
+}
+
+function withdraw(uint256 arg1) payable {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    if address(vaultAddress) != msg.sender:
+        revert with 0, '!vault'
+    mem[100] = this.address
+    require ext_code.size(lpPairAddress)
+    staticcall lpPairAddress.balanceOf(address arg1) with:
+            gas gas_remaining wei
+           args this.address
+    mem[96] = ext_call.return_data[0]
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_call.return_data[0] == ext_call.return_data[0]
+    if ext_call.return_data[0] >= arg1:
+        if ext_call.return_data[0] <= arg1:
+            if ext_call.return_data[0] and sub_d68e1302 > -1 / ext_call.return_data[0]:
+                revert with 'NH{q', 17
+            if ext_call.return_data[0] < ext_call.return_data[0] * sub_d68e1302 / 10000:
+                revert with 'NH{q', 17
+            mem[ceil32(return_data.size) + 132] = address(vaultAddress)
+            mem[ceil32(return_data.size) + 164] = ext_call.return_data[0] - (ext_call.return_data[0] * sub_d68e1302 / 10000)
+            mem[ceil32(return_data.size) + 96] = 68
+            mem[ceil32(return_data.size) + 132 len 28] = Mask(224, 0, stor9)
+            mem[ceil32(return_data.size) + 128 len 4] = transfer(address arg1, uint256 arg2)
+            mem[ceil32(return_data.size) + 196] = 32
+            mem[ceil32(return_data.size) + 228] = 'SafeERC20: low-level call failed'
+            if eth.balance(this.address) < 0:
+                revert with 0, 'Address: insufficient balance for call'
+            if ext_code.size(lpPairAddress) <= 0:
+                revert with 0, 'Address: call to non-contract'
+            mem[ceil32(return_data.size) + 260 len 96] = transfer(address arg1, uint256 arg2), Mask(224, 0, stor9), uint32(stor9), ext_call.return_data[0] - (ext_call.return_data[0] * sub_d68e1302 / 10000), 0
+            mem[ceil32(return_data.size) + 328] = 0
+            call lpPairAddress with:
+               funct Mask(32, 224, transfer(address arg1, uint256 arg2), Mask(224, 0, stor9), uint32(stor9), ext_call.return_data[0] - (ext_call.return_data[0] * sub_d68e1302 / 10000), 0) >> 224
+                 gas gas_remaining wei
+                args (Mask(512, -288, transfer(address arg1, uint256 arg2), Mask(224, 0, stor9), uint32(stor9), ext_call.return_data[0] - (ext_call.return_data[0] * sub_d68e1302 / 10000), 0) << 288)
+        else:
+            if arg1 and sub_d68e1302 > -1 / arg1:
+                revert with 'NH{q', 17
+            if arg1 < arg1 * sub_d68e1302 / 10000:
+                revert with 'NH{q', 17
+            mem[ceil32(return_data.size) + 132] = address(vaultAddress)
+            mem[ceil32(return_data.size) + 164] = arg1 - (arg1 * sub_d68e1302 / 10000)
+            mem[ceil32(return_data.size) + 96] = 68
+            mem[ceil32(return_data.size) + 132 len 28] = Mask(224, 0, stor9)
+            mem[ceil32(return_data.size) + 128 len 4] = transfer(address arg1, uint256 arg2)
+            mem[ceil32(return_data.size) + 196] = 32
+            mem[ceil32(return_data.size) + 228] = 'SafeERC20: low-level call failed'
+            if eth.balance(this.address) < 0:
+                revert with 0, 'Address: insufficient balance for call'
+            if ext_code.size(lpPairAddress) <= 0:
+                revert with 0, 'Address: call to non-contract'
+            mem[ceil32(return_data.size) + 260 len 96] = transfer(address arg1, uint256 arg2), Mask(224, 0, stor9), uint32(stor9), arg1 - (arg1 * sub_d68e1302 / 10000), 0
+            mem[ceil32(return_data.size) + 328] = 0
+            call lpPairAddress with:
+               funct Mask(32, 224, transfer(address arg1, uint256 arg2), Mask(224, 0, stor9), uint32(stor9), arg1 - (arg1 * sub_d68e1302 / 10000), 0) >> 224
+                 gas gas_remaining wei
+                args (Mask(512, -288, transfer(address arg1, uint256 arg2), Mask(224, 0, stor9), uint32(stor9), arg1 - (arg1 * sub_d68e1302 / 10000), 0) << 288)
+        if not return_data.size:
+            if not ext_call.success:
+                if ext_call.return_data[0] > 0:
+                    revert with memory
+                      from 128
+                       len ext_call.return_data[0]
+                revert with 0, 'SafeERC20: low-level call failed'
+            if ext_call.return_data[0] > 0:
+                require ext_call.return_data[0] >= 32
+                require 0, mem[132 len 28] == bool(0, mem[132 len 28])
+                if not 0, mem[132 len 28]:
+                    revert with 0, 'SafeERC20: ERC20 operation did not succeed'
+        else:
+            mem[ceil32(return_data.size) + 292 len return_data.size] = ext_call.return_data[0 len return_data.size]
+            if not ext_call.success:
+                if return_data.size > 0:
+                    revert with ext_call.return_data[0 len return_data.size]
+                revert with 0, 'SafeERC20: low-level call failed'
+            if return_data.size > 0:
+                require return_data.size >= 32
+                require mem[ceil32(return_data.size) + 292] == bool(mem[ceil32(return_data.size) + 292])
+                if not mem[ceil32(return_data.size) + 292]:
+                    revert with 0, 'SafeERC20: ERC20 operation did not succeed'
+    else:
+        mem[ceil32(return_data.size) + 132] = arg1
+        require ext_code.size(masterChefAddress)
+        call masterChefAddress.withdraw(uint256 arg1, uint256 arg2) with:
+             gas gas_remaining wei
+            args stor7, arg1
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+        mem[ceil32(return_data.size) + 100] = this.address
+        require ext_code.size(lpPairAddress)
+        staticcall lpPairAddress.balanceOf(address arg1) with:
+                gas gas_remaining wei
+               args this.address
+        mem[ceil32(return_data.size) + 96] = ext_call.return_data[0]
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+        require return_data.size >= 32
+        require ext_call.return_data[0] == ext_call.return_data[0]
+        if ext_call.return_data[0] <= arg1:
+            if ext_call.return_data[0] and sub_d68e1302 > -1 / ext_call.return_data[0]:
+                revert with 'NH{q', 17
+            if ext_call.return_data[0] < ext_call.return_data[0] * sub_d68e1302 / 10000:
+                revert with 'NH{q', 17
+            mem[(2 * ceil32(return_data.size)) + 132] = address(vaultAddress)
+            mem[(2 * ceil32(return_data.size)) + 164] = ext_call.return_data[0] - (ext_call.return_data[0] * sub_d68e1302 / 10000)
+            mem[(2 * ceil32(return_data.size)) + 96] = 68
+            mem[(2 * ceil32(return_data.size)) + 132 len 28] = Mask(224, 0, stor9)
+            mem[(2 * ceil32(return_data.size)) + 128 len 4] = transfer(address arg1, uint256 arg2)
+            mem[(2 * ceil32(return_data.size)) + 196] = 32
+            mem[(2 * ceil32(return_data.size)) + 228] = 'SafeERC20: low-level call failed'
+            if eth.balance(this.address) < 0:
+                revert with 0, 'Address: insufficient balance for call'
+            if ext_code.size(lpPairAddress) <= 0:
+                revert with 0, 'Address: call to non-contract'
+            mem[(2 * ceil32(return_data.size)) + 260 len 96] = transfer(address arg1, uint256 arg2), Mask(224, 0, stor9), uint32(stor9), ext_call.return_data[0] - (ext_call.return_data[0] * sub_d68e1302 / 10000), 0
+            mem[(2 * ceil32(return_data.size)) + 328] = 0
+            call lpPairAddress with:
+               funct Mask(32, 224, transfer(address arg1, uint256 arg2), Mask(224, 0, stor9), uint32(stor9), ext_call.return_data[0] - (ext_call.return_data[0] * sub_d68e1302 / 10000), 0) >> 224
+                 gas gas_remaining wei
+                args (Mask(512, -288, transfer(address arg1, uint256 arg2), Mask(224, 0, stor9), uint32(stor9), ext_call.return_data[0] - (ext_call.return_data[0] * sub_d68e1302 / 10000), 0) << 288)
+        else:
+            if arg1 and sub_d68e1302 > -1 / arg1:
+                revert with 'NH{q', 17
+            if arg1 < arg1 * sub_d68e1302 / 10000:
+                revert with 'NH{q', 17
+            mem[(2 * ceil32(return_data.size)) + 132] = address(vaultAddress)
+            mem[(2 * ceil32(return_data.size)) + 164] = arg1 - (arg1 * sub_d68e1302 / 10000)
+            mem[(2 * ceil32(return_data.size)) + 96] = 68
+            mem[(2 * ceil32(return_data.size)) + 132 len 28] = Mask(224, 0, stor9)
+            mem[(2 * ceil32(return_data.size)) + 128 len 4] = transfer(address arg1, uint256 arg2)
+            mem[(2 * ceil32(return_data.size)) + 196] = 32
+            mem[(2 * ceil32(return_data.size)) + 228] = 'SafeERC20: low-level call failed'
+            if eth.balance(this.address) < 0:
+                revert with 0, 'Address: insufficient balance for call'
+            if ext_code.size(lpPairAddress) <= 0:
+                revert with 0, 'Address: call to non-contract'
+            mem[(2 * ceil32(return_data.size)) + 260 len 96] = transfer(address arg1, uint256 arg2), Mask(224, 0, stor9), uint32(stor9), arg1 - (arg1 * sub_d68e1302 / 10000), 0
+            mem[(2 * ceil32(return_data.size)) + 328] = 0
+            call lpPairAddress with:
+               funct Mask(32, 224, transfer(address arg1, uint256 arg2), Mask(224, 0, stor9), uint32(stor9), arg1 - (arg1 * sub_d68e1302 / 10000), 0) >> 224
+                 gas gas_remaining wei
+                args (Mask(512, -288, transfer(address arg1, uint256 arg2), Mask(224, 0, stor9), uint32(stor9), arg1 - (arg1 * sub_d68e1302 / 10000), 0) << 288)
+        if not return_data.size:
+            if not ext_call.success:
+                if ext_call.return_data[0] > 0:
+                    revert with memory
+                      from 128
+                       len ext_call.return_data[0]
+                revert with 0, 'SafeERC20: low-level call failed'
+            if ext_call.return_data[0] > 0:
+                require ext_call.return_data[0] >= 32
+                require 0, mem[132 len 28] == bool(0, mem[132 len 28])
+                if not 0, mem[132 len 28]:
+                    revert with 0, 'SafeERC20: ERC20 operation did not succeed'
+        else:
+            mem[(2 * ceil32(return_data.size)) + 292 len return_data.size] = ext_call.return_data[0 len return_data.size]
+            if not ext_call.success:
+                if return_data.size > 0:
+                    revert with ext_call.return_data[0 len return_data.size]
+                revert with 0, 'SafeERC20: low-level call failed'
+            if return_data.size > 0:
+                require return_data.size >= 32
+                require mem[(2 * ceil32(return_data.size)) + 292] == bool(mem[(2 * ceil32(return_data.size)) + 292])
+                if not mem[(2 * ceil32(return_data.size)) + 292]:
+                    revert with 0, 'SafeERC20: ERC20 operation did not succeed'
+}
+
+
+
+}
