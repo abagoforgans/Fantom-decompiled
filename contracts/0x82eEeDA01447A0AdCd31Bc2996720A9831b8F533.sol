c18c05905d8cebf1d6dc1fb64c22651286b67dce
bot <abagoforgans@users.noreply.github.com>
2022-05-23 02:02:16 +0000

    autocommit

---
+++ contracts/0x82eEeDA01447A0AdCd31Bc2996720A9831b8F533.sol
@@ -0,0 +1,64 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+address owner;
+address sub_75bae98aAddress;
+
+function sub_75bae98a(?) payable {
+    return sub_75bae98aAddress
+}
+
+function owner() payable {
+    return owner
+}
+
+function _fallback() payable {
+    revert
+}
+
+function renounceOwnership() payable {
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    owner = 0
+    emit OwnershipTransferred(owner, 0);
+}
+
+function transferOwnership(address arg1) payable {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'Ownable: new owner is the zero address'
+    owner = arg1
+    emit OwnershipTransferred(owner, arg1);
+}
+
+function getRewards() payable {
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    require ext_code.size(sub_75bae98aAddress)
+    staticcall sub_75bae98aAddress.balanceOf(address arg1) with:
+            gas gas_remaining wei
+           args this.address
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_code.size(sub_75bae98aAddress)
+    call sub_75bae98aAddress.transfer(address arg1, uint256 arg2) with:
+         gas gas_remaining wei
+        args owner, ext_call.return_data[0]
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_call.return_data[0] == bool(ext_call.return_data[0])
+}
+
+
+
+}
