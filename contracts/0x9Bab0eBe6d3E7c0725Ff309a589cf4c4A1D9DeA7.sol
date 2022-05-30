ddd15c611765d56b223e450534d454cecbf82d83
bot <abagoforgans@users.noreply.github.com>
2022-05-30 08:39:56 +0000

    autocommit

---
+++ contracts/0x9Bab0eBe6d3E7c0725Ff309a589cf4c4A1D9DeA7.sol
@@ -0,0 +1,28 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+const routerAddress = 0xf491e7b69e4244ad4002bc14e878a34207e38c29
+
+const sub_6c1a35be(?) = 0x2b4c76d0dc16be1c31d4c1dc53bf9b45987fc75c
+
+const farmPid = 2
+
+const sub_c1e32a3d(?) = 0xec7178f4c41f346b2721907f5cf7628e388a7a58
+
+const farmToken = 0x841fad6eae12c286d1fd18d1d525dffa75c7effe
+
+const farmAddress = 0x2b2929e785374c651a81a63878ab22742656dcdd
+
+
+function _fallback() payable {
+    revert
+}
+
+
+
+}
