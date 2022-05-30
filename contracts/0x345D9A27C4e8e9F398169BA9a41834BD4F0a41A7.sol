a21565e4d4e9fc20ec508b007f880bb6b117ddb0
bot <abagoforgans@users.noreply.github.com>
2022-05-30 20:26:55 +0000

    autocommit

---
+++ contracts/0x345D9A27C4e8e9F398169BA9a41834BD4F0a41A7.sol
@@ -0,0 +1,267 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+array of struct stor0;
+array of struct stor1;
+uint8 decimals;
+uint256 totalSupply;
+mapping of uint256 balanceOf;
+address stor5;
+mapping of uint256 allowance;
+
+function totalSupply() {
+    return totalSupply
+}
+
+function decimals() {
+    return decimals
+}
+
+function balanceOf(address arg1) {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    return balanceOf[arg1]
+}
+
+function allowance(address arg1, address arg2) {
+    require calldata.size - 4 >= 64
+    require arg1 == arg1
+    require arg2 == arg2
+    return allowance[arg1][arg2]
+}
+
+function _fallback() payable {
+    revert
+}
+
+function approve(address arg1, uint256 arg2) {
+    require calldata.size - 4 >= 64
+    require arg1 == arg1
+    require arg2 > 0
+    allowance[msg.sender][address(arg1)] = arg2
+    return 1
+}
+
+function withdrawEther(uint256 arg1) payable {
+    require calldata.size - 4 >= 32
+    require msg.sender == stor5
+    call stor5 with:
+       value arg1 wei
+         gas 2300 * is_zero(value) wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+}
+
+function transfer(address arg1, uint256 arg2) {
+    require calldata.size - 4 >= 64
+    require arg1 == arg1
+    require arg1
+    require arg2 > 0
+    require arg2 <= balanceOf[address(msg.sender)]
+    if balanceOf[address(arg1)] > !arg2:
+        revert with 0, 17
+    require balanceOf[address(arg1)] + arg2 > balanceOf[address(arg1)]
+    if arg2 > balanceOf[address(msg.sender)]:
+        revert with 0, 'SafeMath: subtraction overflow'
+    if balanceOf[address(msg.sender)] < arg2:
+        revert with 0, 17
+    balanceOf[address(msg.sender)] -= arg2
+    if balanceOf[arg1] > !arg2:
+        revert with 0, 17
+    if balanceOf[arg1] + arg2 < balanceOf[arg1]:
+        revert with 0, 'SafeMath: addition overflow'
+    balanceOf[address(arg1)] = balanceOf[arg1] + arg2
+    emit Transfer(arg2, msg.sender, arg1);
+    return 1
+}
+
+function transferFrom(address arg1, address arg2, uint256 arg3) {
+    require calldata.size - 4 >= 96
+    require arg1 == arg1
+    require arg2 == arg2
+    require arg3 <= allowance[address(arg1)][msg.sender]
+    require arg2
+    require arg3 > 0
+    require arg3 <= balanceOf[address(arg1)]
+    if balanceOf[address(arg2)] > !arg3:
+        revert with 0, 17
+    require balanceOf[address(arg2)] + arg3 > balanceOf[address(arg2)]
+    if arg3 > balanceOf[address(arg1)]:
+        revert with 0, 'SafeMath: subtraction overflow'
+    if balanceOf[address(arg1)] < arg3:
+        revert with 0, 17
+    balanceOf[address(arg1)] -= arg3
+    if balanceOf[arg2] > !arg3:
+        revert with 0, 17
+    if balanceOf[arg2] + arg3 < balanceOf[arg2]:
+        revert with 0, 'SafeMath: addition overflow'
+    balanceOf[address(arg2)] = balanceOf[arg2] + arg3
+    emit Transfer(arg3, arg1, arg2);
+    if arg3 > allowance[address(arg1)][msg.sender]:
+        revert with 0, 'SafeMath: subtraction overflow'
+    if allowance[address(arg1)][msg.sender] < arg3:
+        revert with 0, 17
+    allowance[address(arg1)][msg.sender] -= arg3
+    return 1
+}
+
+function name() {
+    if bool(stor0.length):
+        if bool(stor0.length) == stor0.length.field_1 < 32:
+            revert with 0, 34
+        if bool(stor0.length):
+            if bool(stor0.length) == stor0.length.field_1 < 32:
+                revert with 0, 34
+            if stor0.length.field_1:
+                if 31 < stor0.length.field_1:
+                    mem[128] = uint256(stor0.field_0)
+                    idx = 128
+                    s = 0
+                    while stor0.length.field_1 + 96 > idx:
+                        mem[idx + 32] = stor0[s].field_256
+                        idx = idx + 32
+                        s = s + 1
+                        continue 
+                    return Array(len=2 * Mask(256, -1, stor0.length.field_1), data=mem[128 len ceil32(stor0.length.field_1)])
+                mem[128] = 256 * stor0.length.field_8
+        else:
+            if bool(stor0.length) == stor0.length.field_1 < 32:
+                revert with 0, 34
+            if stor0.length.field_1:
+                if 31 < stor0.length.field_1:
+                    mem[128] = uint256(stor0.field_0)
+                    idx = 128
+                    s = 0
+                    while stor0.length.field_1 + 96 > idx:
+                        mem[idx + 32] = stor0[s].field_256
+                        idx = idx + 32
+                        s = s + 1
+                        continue 
+                    return Array(len=2 * Mask(256, -1, stor0.length.field_1), data=mem[128 len ceil32(stor0.length.field_1)])
+                mem[128] = 256 * stor0.length.field_8
+        mem[ceil32(stor0.length.field_1) + 192 len ceil32(stor0.length.field_1)] = mem[128 len ceil32(stor0.length.field_1)]
+        if ceil32(stor0.length.field_1) > stor0.length.field_1:
+            mem[ceil32(stor0.length.field_1) + stor0.length.field_1 + 192] = 0
+        return Array(len=2 * Mask(256, -1, stor0.length.field_1), data=mem[128 len ceil32(stor0.length.field_1)], mem[(2 * ceil32(stor0.length.field_1)) + 192 len 2 * ceil32(stor0.length.field_1)]), 
+    if bool(stor0.length) == stor0.length.field_1 < 32:
+        revert with 0, 34
+    if bool(stor0.length):
+        if bool(stor0.length) == stor0.length.field_1 < 32:
+            revert with 0, 34
+        if stor0.length.field_1:
+            if 31 < stor0.length.field_1:
+                mem[128] = uint256(stor0.field_0)
+                idx = 128
+                s = 0
+                while stor0.length.field_1 + 96 > idx:
+                    mem[idx + 32] = stor0[s].field_256
+                    idx = idx + 32
+                    s = s + 1
+                    continue 
+                return Array(len=stor0.length % 128, data=mem[128 len ceil32(stor0.length.field_1)])
+            mem[128] = 256 * stor0.length.field_8
+    else:
+        if bool(stor0.length) == stor0.length.field_1 < 32:
+            revert with 0, 34
+        if stor0.length.field_1:
+            if 31 < stor0.length.field_1:
+                mem[128] = uint256(stor0.field_0)
+                idx = 128
+                s = 0
+                while stor0.length.field_1 + 96 > idx:
+                    mem[idx + 32] = stor0[s].field_256
+                    idx = idx + 32
+                    s = s + 1
+                    continue 
+                return Array(len=stor0.length % 128, data=mem[128 len ceil32(stor0.length.field_1)])
+            mem[128] = 256 * stor0.length.field_8
+    mem[ceil32(stor0.length.field_1) + 192 len ceil32(stor0.length.field_1)] = mem[128 len ceil32(stor0.length.field_1)]
+    if ceil32(stor0.length.field_1) > stor0.length.field_1:
+        mem[ceil32(stor0.length.field_1) + stor0.length.field_1 + 192] = 0
+    return Array(len=stor0.length % 128, data=mem[128 len ceil32(stor0.length.field_1)], mem[(2 * ceil32(stor0.length.field_1)) + 192 len 2 * ceil32(stor0.length.field_1)]), 
+}
+
+function symbol() {
+    if bool(stor1.length):
+        if bool(stor1.length) == stor1.length.field_1 < 32:
+            revert with 0, 34
+        if bool(stor1.length):
+            if bool(stor1.length) == stor1.length.field_1 < 32:
+                revert with 0, 34
+            if stor1.length.field_1:
+                if 31 < stor1.length.field_1:
+                    mem[128] = uint256(stor1.field_0)
+                    idx = 128
+                    s = 0
+                    while stor1.length.field_1 + 96 > idx:
+                        mem[idx + 32] = stor1[s].field_256
+                        idx = idx + 32
+                        s = s + 1
+                        continue 
+                    return Array(len=2 * Mask(256, -1, stor1.length.field_1), data=mem[128 len ceil32(stor1.length.field_1)])
+                mem[128] = 256 * stor1.length.field_8
+        else:
+            if bool(stor1.length) == stor1.length.field_1 < 32:
+                revert with 0, 34
+            if stor1.length.field_1:
+                if 31 < stor1.length.field_1:
+                    mem[128] = uint256(stor1.field_0)
+                    idx = 128
+                    s = 0
+                    while stor1.length.field_1 + 96 > idx:
+                        mem[idx + 32] = stor1[s].field_256
+                        idx = idx + 32
+                        s = s + 1
+                        continue 
+                    return Array(len=2 * Mask(256, -1, stor1.length.field_1), data=mem[128 len ceil32(stor1.length.field_1)])
+                mem[128] = 256 * stor1.length.field_8
+        mem[ceil32(stor1.length.field_1) + 192 len ceil32(stor1.length.field_1)] = mem[128 len ceil32(stor1.length.field_1)]
+        if ceil32(stor1.length.field_1) > stor1.length.field_1:
+            mem[ceil32(stor1.length.field_1) + stor1.length.field_1 + 192] = 0
+        return Array(len=2 * Mask(256, -1, stor1.length.field_1), data=mem[128 len ceil32(stor1.length.field_1)], mem[(2 * ceil32(stor1.length.field_1)) + 192 len 2 * ceil32(stor1.length.field_1)]), 
+    if bool(stor1.length) == stor1.length.field_1 < 32:
+        revert with 0, 34
+    if bool(stor1.length):
+        if bool(stor1.length) == stor1.length.field_1 < 32:
+            revert with 0, 34
+        if stor1.length.field_1:
+            if 31 < stor1.length.field_1:
+                mem[128] = uint256(stor1.field_0)
+                idx = 128
+                s = 0
+                while stor1.length.field_1 + 96 > idx:
+                    mem[idx + 32] = stor1[s].field_256
+                    idx = idx + 32
+                    s = s + 1
+                    continue 
+                return Array(len=stor1.length % 128, data=mem[128 len ceil32(stor1.length.field_1)])
+            mem[128] = 256 * stor1.length.field_8
+    else:
+        if bool(stor1.length) == stor1.length.field_1 < 32:
+            revert with 0, 34
+        if stor1.length.field_1:
+            if 31 < stor1.length.field_1:
+                mem[128] = uint256(stor1.field_0)
+                idx = 128
+                s = 0
+                while stor1.length.field_1 + 96 > idx:
+                    mem[idx + 32] = stor1[s].field_256
+                    idx = idx + 32
+                    s = s + 1
+                    continue 
+                return Array(len=stor1.length % 128, data=mem[128 len ceil32(stor1.length.field_1)])
+            mem[128] = 256 * stor1.length.field_8
+    mem[ceil32(stor1.length.field_1) + 192 len ceil32(stor1.length.field_1)] = mem[128 len ceil32(stor1.length.field_1)]
+    if ceil32(stor1.length.field_1) > stor1.length.field_1:
+        mem[ceil32(stor1.length.field_1) + stor1.length.field_1 + 192] = 0
+    return Array(len=stor1.length % 128, data=mem[128 len ceil32(stor1.length.field_1)], mem[(2 * ceil32(stor1.length.field_1)) + 192 len 2 * ceil32(stor1.length.field_1)]), 
+}
+
+
+
+}
---
+++ contracts/0xA5b1fA9316C20C796FAB7fF6CE2c07daA59007BC.sol
@@ -0,0 +1,201 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+address sub_d4384dbeAddress;
+address sub_9f09f9d3Address;
+
+function sub_9f09f9d3(?) payable {
+    return sub_9f09f9d3Address
+}
+
+function sub_d4384dbe(?) payable {
+    return sub_d4384dbeAddress
+}
+
+function _fallback() payable {
+    revert
+}
+
+function sub_09ec27b7(?) payable {
+    require calldata.size - 4 >= 64
+    require ext_code.size(arg1)
+    staticcall arg1.getReserves() with:
+            gas gas_remaining wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 96
+    return ext_call.return_data[18 len 14]
+}
+
+function sub_b481e39e(?) payable {
+    require calldata.size - 4 >= 64
+    require ext_code.size(arg1)
+    staticcall arg1.getReserves() with:
+            gas gas_remaining wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 96
+    return ext_call.return_data[50 len 14]
+}
+
+function sub_f51fcad7(?) payable {
+    require calldata.size - 4 >= 64
+    require ext_code.size(arg1)
+    staticcall arg1.getReserves() with:
+            gas gas_remaining wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 96
+    return (ext_call.return_data[18 len 14] * ext_call.return_data[50 len 14])
+}
+
+function sub_da858dae(?) payable {
+    require calldata.size - 4 >= 64
+    require ext_code.size(arg1)
+    staticcall arg1.getReserves() with:
+            gas gas_remaining wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 96
+    require ext_call.return_data[18 len 14] + arg2
+    return (ext_call.return_data[18 len 14] * ext_call.return_data[50 len 14] / ext_call.return_data[18 len 14] + arg2)
+}
+
+function sub_b7aa2564(?) payable {
+    require calldata.size - 4 >= 64
+    require ext_code.size(arg1)
+    staticcall arg1.getReserves() with:
+            gas gas_remaining wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 96
+    require ext_call.return_data[18 len 14] + arg2
+    return (ext_call.return_data[50 len 14] - (ext_call.return_data[18 len 14] * ext_call.return_data[50 len 14] / ext_call.return_data[18 len 14] + arg2))
+}
+
+function sub_884ccf7c(?) payable {
+    require calldata.size - 4 >= 64
+    require ext_code.size(arg1)
+    staticcall arg1.token0() with:
+            gas gas_remaining wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_code.size(address(ext_call.return_data[0]))
+    staticcall address(ext_call.return_data[0]).0x313ce567 with:
+            gas gas_remaining wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    return ext_call.return_data[31 len 1]
+}
+
+function sub_ad1b700b(?) payable {
+    require calldata.size - 4 >= 64
+    require ext_code.size(arg1)
+    staticcall arg1.token1() with:
+            gas gas_remaining wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_code.size(address(ext_call.return_data[0]))
+    staticcall address(ext_call.return_data[0]).0x313ce567 with:
+            gas gas_remaining wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    return ext_call.return_data[31 len 1]
+}
+
+function sub_199b1e59(?) payable {
+    require calldata.size - 4 >= 64
+    require ext_code.size(arg1)
+    staticcall arg1.getReserves() with:
+            gas gas_remaining wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 96
+    require ext_call.return_data[18 len 14] + arg2
+    require ext_call.return_data[50 len 14] - (ext_call.return_data[18 len 14] * ext_call.return_data[50 len 14] / ext_call.return_data[18 len 14] + arg2)
+    return (arg2 / ext_call.return_data[50 len 14] - (ext_call.return_data[18 len 14] * ext_call.return_data[50 len 14] / ext_call.return_data[18 len 14] + arg2))
+}
+
+function sub_d8ad8a8f(?) payable {
+    require calldata.size - 4 >= 64
+    require ext_code.size(arg1)
+    staticcall arg1.getReserves() with:
+            gas gas_remaining wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 96
+    require ext_call.return_data[18 len 14] + arg2
+    require ext_call.return_data[50 len 14] - (ext_call.return_data[18 len 14] * ext_call.return_data[50 len 14] / ext_call.return_data[18 len 14] + arg2)
+    require ext_call.return_data[50 len 14]
+    require ext_call.return_data[18 len 14] / ext_call.return_data[50 len 14]
+    return ((100 * arg2 / ext_call.return_data[50 len 14] - (ext_call.return_data[18 len 14] * ext_call.return_data[50 len 14] / ext_call.return_data[18 len 14] + arg2) / ext_call.return_data[18 len 14] / ext_call.return_data[50 len 14]) - 100)
+}
+
+function sub_f994e6e9(?) payable {
+    require calldata.size - 4 >= 32
+    require ext_code.size(arg1)
+    staticcall arg1.token1() with:
+            gas gas_remaining wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_code.size(address(ext_call.return_data[0]))
+    staticcall address(ext_call.return_data[0]).0x313ce567 with:
+            gas gas_remaining wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_code.size(arg1)
+    staticcall arg1.getReserves() with:
+            gas gas_remaining wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 96
+    require ext_call.return_data[50 len 14]
+    return (ext_call.return_data[18 len 14] * 10^uint8(ext_call.return_data[0]) / ext_call.return_data[50 len 14])
+}
+
+function sub_619d4e2f(?) payable {
+    require calldata.size - 4 >= 96
+    require arg1 <= 4294967296
+    require arg1 + 36 <= calldata.size
+    require arg1.length <= 4294967296 and arg1 + arg1.length + 36 <= calldata.size
+    mem[128 len arg1.length] = arg1[all]
+    mem[arg1.length + 128] = 0
+    mem[ceil32(arg1.length) + 160] = 'spooky'
+    mem[ceil32(arg1.length) + 128] = 6
+    _5 = sha3(mem[ceil32(arg1.length) + 160 len Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]])
+    mem[ceil32(arg1.length) + 198 len floor32(arg1.length)] = call.data[arg1 + 36 len floor32(arg1.length)]
+    mem[ceil32(arg1.length) + floor32(arg1.length) + -(arg1.length % 32) + 230 len arg1.length % 32] = mem[-(arg1.length % 32) + floor32(arg1.length) + 160 len arg1.length % 32]
+    if sha3(call.data[arg1 + 36 len floor32(arg1.length)], mem[ceil32(arg1.length) + floor32(arg1.length) + 198 len arg1.length % 32]) == _5:
+        require ext_code.size(0x152ee697f2e276fa89e96742e9bb9ab1f2e61be3)
+        staticcall 0x152ee697f2e276fa89e96742e9bb9ab1f2e61be3.getPair(address arg1, address arg2) with:
+                gas gas_remaining wei
+               args address(arg2), arg3
+    else:
+        mem[arg1.length + ceil32(arg1.length) + 267 len floor32(arg1.length)] = call.data[arg1 + 36 len floor32(arg1.length)]
+        mem[(2 * floor32(arg1.length)) + ceil32(arg1.length) + 299 len arg1.length % 32] = mem[-(arg1.length % 32) + floor32(arg1.length) + 160 len arg1.length % 32]
+        if sha3(call.data[arg1 + 36 len floor32(arg1.length)], mem[arg1.length + ceil32(arg1.length) + floor32(arg1.length) + 267 len arg1.length % 32]) != sha3('hyper'):
+            return 0
+        require ext_code.size(0x991152411a7b5a14a8cf0cdde8439435328070df)
+        staticcall 0x991152411a7b5a14a8cf0cdde8439435328070df.getPair(address arg1, address arg2) with:
+                gas gas_remaining wei
+               args address(arg2), arg3
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    return ext_call.return_data[12 len 20]
+}
+
+
+
+}
