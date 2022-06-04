f52e4a3928c12ad2fe2bfe506f1a8dc6f8a5cc6f
bot <abagoforgans@users.noreply.github.com>
2022-06-04 23:10:47 +0000

    autocommit

---
+++ contracts/0x7Dc940B3120E3B35BeB52b84C3948cCb0E09006c.sol
@@ -0,0 +1,338 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+address uniswapRouterAddress;
+address stor1;
+
+function uniswapRouter() {
+    return uniswapRouterAddress
+}
+
+function sub_6f6d4acf(?) payable {
+    require calldata.size - 4 >= 64
+    mem[96] = 2
+    require ext_code.size(uniswapRouterAddress)
+    staticcall uniswapRouterAddress.WETH() with:
+            gas gas_remaining wei
+    mem[192] = ext_call.return_data[0]
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_call.return_data[0] == ext_call.return_data[12 len 20]
+    mem[128] = ext_call.return_data[12 len 20]
+    mem[160] = stor1
+    mem[ceil32(return_data.size) + 192] = 0xfb3bdb4100000000000000000000000000000000000000000000000000000000
+    mem[ceil32(return_data.size) + 196] = arg1
+    mem[ceil32(return_data.size) + 228] = 128
+    mem[ceil32(return_data.size) + 324] = 2
+    idx = 0
+    s = 128
+    t = ceil32(return_data.size) + 356
+    while idx < 2:
+        mem[t] = mem[s + 12 len 20]
+        idx = idx + 1
+        s = s + 32
+        t = t + 32
+        continue 
+    mem[ceil32(return_data.size) + 260] = msg.sender
+    mem[ceil32(return_data.size) + 292] = arg2
+    require ext_code.size(uniswapRouterAddress)
+    call uniswapRouterAddress.swapETHForExactTokens(uint256 arg1, address[] arg2, address arg3, uint256 arg4) with:
+       value msg.value wei
+         gas gas_remaining wei
+        args arg1, Array(len=2, data=mem[ceil32(return_data.size) + 356 len 64]), msg.sender, arg2
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    mem[ceil32(return_data.size) + 192 len return_data.size] = ext_call.return_data[0 len return_data.size]
+    mem[64] = (2 * ceil32(return_data.size)) + 192
+    require return_data.size >= 32
+    require mem[ceil32(return_data.size) + 192 len 4], Mask(224, 32, arg1) >> 32 <= test266151307()
+    require ceil32(return_data.size) + mem[ceil32(return_data.size) + 192 len 4], Mask(224, 32, arg1) >> 32 + 223 < ceil32(return_data.size) + return_data.size + 192
+    if mem[ceil32(return_data.size) + mem[ceil32(return_data.size) + 192 len 4], Mask(224, 32, arg1) >> 32 + 192] > test266151307():
+        revert with 0, 65
+    if (2 * ceil32(return_data.size)) + ceil32(32 * mem[ceil32(return_data.size) + mem[ceil32(return_data.size) + 192 len 4], Mask(224, 32, arg1) >> 32 + 192]) + 193 > test266151307() or ceil32(32 * mem[ceil32(return_data.size) + mem[ceil32(return_data.size) + 192 len 4], Mask(224, 32, arg1) >> 32 + 192]) + 1 < 0:
+        revert with 0, 65
+    require return_data.size >= mem[ceil32(return_data.size) + 192 len 4], Mask(224, 32, arg1) >> 32 + (32 * mem[ceil32(return_data.size) + mem[ceil32(return_data.size) + 192 len 4], Mask(224, 32, arg1) >> 32 + 192]) + 32
+    call msg.sender with:
+       value eth.balance(this.address) wei
+         gas gas_remaining wei
+    if not ext_call.success:
+        revert with 0, 'syfin zap ftm refund failed'
+}
+
+function sub_15721f07(?) {
+    require calldata.size - 4 >= 32
+    mem[96] = 2
+    require ext_code.size(uniswapRouterAddress)
+    staticcall uniswapRouterAddress.WETH() with:
+            gas gas_remaining wei
+    mem[192] = ext_call.return_data[0]
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_call.return_data[0] == ext_call.return_data[12 len 20]
+    mem[128] = ext_call.return_data[12 len 20]
+    mem[160] = stor1
+    mem[ceil32(return_data.size) + 192] = 0x1f00ca7400000000000000000000000000000000000000000000000000000000
+    mem[ceil32(return_data.size) + 196] = arg1
+    mem[ceil32(return_data.size) + 228] = 64
+    mem[ceil32(return_data.size) + 260] = 2
+    idx = 0
+    s = 128
+    t = ceil32(return_data.size) + 292
+    while idx < 2:
+        mem[t] = mem[s + 12 len 20]
+        idx = idx + 1
+        s = s + 32
+        t = t + 32
+        continue 
+    require ext_code.size(uniswapRouterAddress)
+    staticcall uniswapRouterAddress.getAmountsIn(uint256 arg1, address[] arg2) with:
+            gas gas_remaining wei
+           args arg1, Array(len=2, data=mem[ceil32(return_data.size) + 292 len 64])
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    mem[ceil32(return_data.size) + 192 len return_data.size] = ext_call.return_data[0 len return_data.size]
+    mem[64] = (2 * ceil32(return_data.size)) + 192
+    require return_data.size >= 32
+    _24 = mem[ceil32(return_data.size) + 192 len 4], Mask(224, 32, arg1) >> 32
+    require mem[ceil32(return_data.size) + 192 len 4], Mask(224, 32, arg1) >> 32 <= test266151307()
+    require ceil32(return_data.size) + mem[ceil32(return_data.size) + 192 len 4], Mask(224, 32, arg1) >> 32 + 223 < ceil32(return_data.size) + return_data.size + 192
+    _25 = mem[ceil32(return_data.size) + mem[ceil32(return_data.size) + 192 len 4], Mask(224, 32, arg1) >> 32 + 192]
+    if mem[ceil32(return_data.size) + mem[ceil32(return_data.size) + 192 len 4], Mask(224, 32, arg1) >> 32 + 192] > test266151307():
+        revert with 0, 65
+    if (2 * ceil32(return_data.size)) + ceil32(32 * mem[ceil32(return_data.size) + mem[ceil32(return_data.size) + 192 len 4], Mask(224, 32, arg1) >> 32 + 192]) + 193 > test266151307() or ceil32(32 * mem[ceil32(return_data.size) + mem[ceil32(return_data.size) + 192 len 4], Mask(224, 32, arg1) >> 32 + 192]) + 1 < 0:
+        revert with 0, 65
+    mem[64] = (2 * ceil32(return_data.size)) + ceil32(32 * mem[ceil32(return_data.size) + mem[ceil32(return_data.size) + 192 len 4], Mask(224, 32, arg1) >> 32 + 192]) + 193
+    mem[(2 * ceil32(return_data.size)) + 192] = _25
+    require return_data.size >= _24 + (32 * _25) + 32
+    mem[(2 * ceil32(return_data.size)) + 224 len 32 * _25] = mem[ceil32(return_data.size) + _24 + 224 len 32 * _25]
+    mem[mem[64]] = 32
+    mem[mem[64] + 32] = _25
+    mem[mem[64] + 64 len 32 * _25] = mem[(2 * ceil32(return_data.size)) + 224 len 32 * _25]
+    return Array(len=_25, data=mem[mem[64] + 64 len 32 * _25])
+}
+
+function sub_979aa408(?) {
+    require calldata.size - 4 >= 32
+    mem[96] = 2
+    mem[160] = stor1
+    require ext_code.size(uniswapRouterAddress)
+    staticcall uniswapRouterAddress.WETH() with:
+            gas gas_remaining wei
+    mem[192] = ext_call.return_data[0]
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_call.return_data[0] == ext_call.return_data[12 len 20]
+    mem[128] = ext_call.return_data[12 len 20]
+    mem[ceil32(return_data.size) + 192] = 0xd06ca61f00000000000000000000000000000000000000000000000000000000
+    mem[ceil32(return_data.size) + 196] = arg1
+    mem[ceil32(return_data.size) + 228] = 64
+    mem[ceil32(return_data.size) + 260] = 2
+    idx = 0
+    s = 128
+    t = ceil32(return_data.size) + 292
+    while idx < 2:
+        mem[t] = mem[s + 12 len 20]
+        idx = idx + 1
+        s = s + 32
+        t = t + 32
+        continue 
+    require ext_code.size(uniswapRouterAddress)
+    staticcall uniswapRouterAddress.getAmountsOut(uint256 arg1, address[] arg2) with:
+            gas gas_remaining wei
+           args arg1, Array(len=2, data=mem[ceil32(return_data.size) + 292 len 64])
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    mem[ceil32(return_data.size) + 192 len return_data.size] = ext_call.return_data[0 len return_data.size]
+    mem[64] = (2 * ceil32(return_data.size)) + 192
+    require return_data.size >= 32
+    _24 = mem[ceil32(return_data.size) + 192 len 4], Mask(224, 32, arg1) >> 32
+    require mem[ceil32(return_data.size) + 192 len 4], Mask(224, 32, arg1) >> 32 <= test266151307()
+    require ceil32(return_data.size) + mem[ceil32(return_data.size) + 192 len 4], Mask(224, 32, arg1) >> 32 + 223 < ceil32(return_data.size) + return_data.size + 192
+    _25 = mem[ceil32(return_data.size) + mem[ceil32(return_data.size) + 192 len 4], Mask(224, 32, arg1) >> 32 + 192]
+    if mem[ceil32(return_data.size) + mem[ceil32(return_data.size) + 192 len 4], Mask(224, 32, arg1) >> 32 + 192] > test266151307():
+        revert with 0, 65
+    if (2 * ceil32(return_data.size)) + ceil32(32 * mem[ceil32(return_data.size) + mem[ceil32(return_data.size) + 192 len 4], Mask(224, 32, arg1) >> 32 + 192]) + 193 > test266151307() or ceil32(32 * mem[ceil32(return_data.size) + mem[ceil32(return_data.size) + 192 len 4], Mask(224, 32, arg1) >> 32 + 192]) + 1 < 0:
+        revert with 0, 65
+    mem[64] = (2 * ceil32(return_data.size)) + ceil32(32 * mem[ceil32(return_data.size) + mem[ceil32(return_data.size) + 192 len 4], Mask(224, 32, arg1) >> 32 + 192]) + 193
+    mem[(2 * ceil32(return_data.size)) + 192] = _25
+    require return_data.size >= _24 + (32 * _25) + 32
+    mem[(2 * ceil32(return_data.size)) + 224 len 32 * _25] = mem[ceil32(return_data.size) + _24 + 224 len 32 * _25]
+    mem[mem[64]] = 32
+    mem[mem[64] + 32] = _25
+    mem[mem[64] + 64 len 32 * _25] = mem[(2 * ceil32(return_data.size)) + 224 len 32 * _25]
+    return Array(len=_25, data=mem[mem[64] + 64 len 32 * _25])
+}
+
+function _fallback() payable {
+    if calldata.size < 4:
+        require not calldata.size
+    if unknown_0x15721f07(?????) == uint32(call.func_hash) >> 224:
+        require not msg.value
+        require calldata.size - 4 >= 32
+        mem[128] = 2
+        require ext_code.size(uniswapRouterAddress)
+        staticcall uniswapRouterAddress.WETH() with:
+                gas gas_remaining wei
+        mem[224] = ext_call.return_data[0]
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+        require return_data.size >= 32
+        require ext_call.return_data[0] == ext_call.return_data[12 len 20]
+        mem[160] = ext_call.return_data[12 len 20]
+        mem[192] = stor1
+        mem[ceil32(return_data.size) + 224] = 0x1f00ca7400000000000000000000000000000000000000000000000000000000
+        mem[ceil32(return_data.size) + 228] = arg1
+        mem[ceil32(return_data.size) + 260] = 64
+        mem[ceil32(return_data.size) + 292] = 2
+        idx = 0
+        s = 160
+        t = ceil32(return_data.size) + 324
+        while idx < 2:
+            mem[t] = mem[s + 12 len 20]
+            idx = idx + 1
+            s = s + 32
+            t = t + 32
+            continue 
+        require ext_code.size(uniswapRouterAddress)
+        staticcall uniswapRouterAddress.getAmountsIn(uint256 arg1, address[] arg2) with:
+                gas gas_remaining wei
+               args arg1, Array(len=2, data=mem[ceil32(return_data.size) + 324 len 64])
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+        mem[ceil32(return_data.size) + 224 len return_data.size] = ext_call.return_data[0 len return_data.size]
+        mem[64] = (2 * ceil32(return_data.size)) + 224
+        require return_data.size >= 32
+        _74 = mem[ceil32(return_data.size) + 224 len 4], Mask(224, 32, arg1) >> 32
+        require mem[ceil32(return_data.size) + 224 len 4], Mask(224, 32, arg1) >> 32 <= test266151307()
+        require ceil32(return_data.size) + mem[ceil32(return_data.size) + 224 len 4], Mask(224, 32, arg1) >> 32 + 255 < ceil32(return_data.size) + return_data.size + 224
+        _77 = mem[ceil32(return_data.size) + mem[ceil32(return_data.size) + 224 len 4], Mask(224, 32, arg1) >> 32 + 224]
+        if mem[ceil32(return_data.size) + mem[ceil32(return_data.size) + 224 len 4], Mask(224, 32, arg1) >> 32 + 224] > test266151307():
+            revert with 0, 65
+        if (2 * ceil32(return_data.size)) + ceil32(32 * mem[ceil32(return_data.size) + mem[ceil32(return_data.size) + 224 len 4], Mask(224, 32, arg1) >> 32 + 224]) + 225 > test266151307() or ceil32(32 * mem[ceil32(return_data.size) + mem[ceil32(return_data.size) + 224 len 4], Mask(224, 32, arg1) >> 32 + 224]) + 1 < 0:
+            revert with 0, 65
+        mem[64] = (2 * ceil32(return_data.size)) + ceil32(32 * mem[ceil32(return_data.size) + mem[ceil32(return_data.size) + 224 len 4], Mask(224, 32, arg1) >> 32 + 224]) + 225
+        mem[(2 * ceil32(return_data.size)) + 224] = _77
+        require return_data.size >= _74 + (32 * _77) + 32
+        mem[(2 * ceil32(return_data.size)) + 256 len 32 * _77] = mem[ceil32(return_data.size) + _74 + 256 len 32 * _77]
+        mem[mem[64]] = 32
+        mem[mem[64] + 32] = _77
+        mem[mem[64] + 64 len 32 * _77] = mem[(2 * ceil32(return_data.size)) + 256 len 32 * _77]
+        return Array(len=_77, data=mem[mem[64] + 64 len 32 * _77])
+    if unknown_0x6f6d4acf(?????) == uint32(call.func_hash) >> 224:
+        require calldata.size - 4 >= 64
+        mem[128] = 2
+        require ext_code.size(uniswapRouterAddress)
+        staticcall uniswapRouterAddress.WETH() with:
+                gas gas_remaining wei
+        mem[224] = ext_call.return_data[0]
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+        require return_data.size >= 32
+        require ext_call.return_data[0] == ext_call.return_data[12 len 20]
+        mem[160] = ext_call.return_data[12 len 20]
+        mem[192] = stor1
+        mem[ceil32(return_data.size) + 224] = 0xfb3bdb4100000000000000000000000000000000000000000000000000000000
+        mem[ceil32(return_data.size) + 228] = arg1
+        mem[ceil32(return_data.size) + 260] = 128
+        mem[ceil32(return_data.size) + 356] = 2
+        idx = 0
+        s = 160
+        t = ceil32(return_data.size) + 388
+        while idx < 2:
+            mem[t] = mem[s + 12 len 20]
+            idx = idx + 1
+            s = s + 32
+            t = t + 32
+            continue 
+        mem[ceil32(return_data.size) + 292] = msg.sender
+        mem[ceil32(return_data.size) + 324] = arg2
+        require ext_code.size(uniswapRouterAddress)
+        call uniswapRouterAddress.swapETHForExactTokens(uint256 arg1, address[] arg2, address arg3, uint256 arg4) with:
+           value msg.value wei
+             gas gas_remaining wei
+            args arg1, Array(len=2, data=mem[ceil32(return_data.size) + 388 len 64]), msg.sender, arg2
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+        mem[ceil32(return_data.size) + 224 len return_data.size] = ext_call.return_data[0 len return_data.size]
+        mem[64] = (2 * ceil32(return_data.size)) + 224
+        require return_data.size >= 32
+        require mem[ceil32(return_data.size) + 224 len 4], Mask(224, 32, arg1) >> 32 <= test266151307()
+        require ceil32(return_data.size) + mem[ceil32(return_data.size) + 224 len 4], Mask(224, 32, arg1) >> 32 + 255 < ceil32(return_data.size) + return_data.size + 224
+        if mem[ceil32(return_data.size) + mem[ceil32(return_data.size) + 224 len 4], Mask(224, 32, arg1) >> 32 + 224] > test266151307():
+            revert with 0, 65
+        if (2 * ceil32(return_data.size)) + ceil32(32 * mem[ceil32(return_data.size) + mem[ceil32(return_data.size) + 224 len 4], Mask(224, 32, arg1) >> 32 + 224]) + 225 > test266151307() or ceil32(32 * mem[ceil32(return_data.size) + mem[ceil32(return_data.size) + 224 len 4], Mask(224, 32, arg1) >> 32 + 224]) + 1 < 0:
+            revert with 0, 65
+        require return_data.size >= mem[ceil32(return_data.size) + 224 len 4], Mask(224, 32, arg1) >> 32 + (32 * mem[ceil32(return_data.size) + mem[ceil32(return_data.size) + 224 len 4], Mask(224, 32, arg1) >> 32 + 224]) + 32
+        call msg.sender with:
+           value eth.balance(this.address) wei
+             gas gas_remaining wei
+        if not ext_call.success:
+            revert with 0, 'syfin zap ftm refund failed'
+    if unknown_0x735de9f7(?????) == uint32(call.func_hash) >> 224:
+        require not msg.value
+        return uniswapRouterAddress
+    require unknown_0x979aa408(?????) == uint32(call.func_hash) >> 224
+    require not msg.value
+    require calldata.size - 4 >= 32
+    mem[128] = 2
+    mem[192] = stor1
+    require ext_code.size(uniswapRouterAddress)
+    staticcall uniswapRouterAddress.WETH() with:
+            gas gas_remaining wei
+    mem[224] = ext_call.return_data[0]
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_call.return_data[0] == ext_call.return_data[12 len 20]
+    mem[160] = ext_call.return_data[12 len 20]
+    mem[ceil32(return_data.size) + 224] = 0xd06ca61f00000000000000000000000000000000000000000000000000000000
+    mem[ceil32(return_data.size) + 228] = arg1
+    mem[ceil32(return_data.size) + 260] = 64
+    mem[ceil32(return_data.size) + 292] = 2
+    idx = 0
+    s = 160
+    t = ceil32(return_data.size) + 324
+    while idx < 2:
+        mem[t] = mem[s + 12 len 20]
+        idx = idx + 1
+        s = s + 32
+        t = t + 32
+        continue 
+    require ext_code.size(uniswapRouterAddress)
+    staticcall uniswapRouterAddress.getAmountsOut(uint256 arg1, address[] arg2) with:
+            gas gas_remaining wei
+           args arg1, Array(len=2, data=mem[ceil32(return_data.size) + 324 len 64])
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    mem[ceil32(return_data.size) + 224 len return_data.size] = ext_call.return_data[0 len return_data.size]
+    mem[64] = (2 * ceil32(return_data.size)) + 224
+    require return_data.size >= 32
+    _76 = mem[ceil32(return_data.size) + 224 len 4], Mask(224, 32, arg1) >> 32
+    require mem[ceil32(return_data.size) + 224 len 4], Mask(224, 32, arg1) >> 32 <= test266151307()
+    require ceil32(return_data.size) + mem[ceil32(return_data.size) + 224 len 4], Mask(224, 32, arg1) >> 32 + 255 < ceil32(return_data.size) + return_data.size + 224
+    _79 = mem[ceil32(return_data.size) + mem[ceil32(return_data.size) + 224 len 4], Mask(224, 32, arg1) >> 32 + 224]
+    if mem[ceil32(return_data.size) + mem[ceil32(return_data.size) + 224 len 4], Mask(224, 32, arg1) >> 32 + 224] > test266151307():
+        revert with 0, 65
+    if (2 * ceil32(return_data.size)) + ceil32(32 * mem[ceil32(return_data.size) + mem[ceil32(return_data.size) + 224 len 4], Mask(224, 32, arg1) >> 32 + 224]) + 225 > test266151307() or ceil32(32 * mem[ceil32(return_data.size) + mem[ceil32(return_data.size) + 224 len 4], Mask(224, 32, arg1) >> 32 + 224]) + 1 < 0:
+        revert with 0, 65
+    mem[64] = (2 * ceil32(return_data.size)) + ceil32(32 * mem[ceil32(return_data.size) + mem[ceil32(return_data.size) + 224 len 4], Mask(224, 32, arg1) >> 32 + 224]) + 225
+    mem[(2 * ceil32(return_data.size)) + 224] = _79
+    require return_data.size >= _76 + (32 * _79) + 32
+    mem[(2 * ceil32(return_data.size)) + 256 len 32 * _79] = mem[ceil32(return_data.size) + _76 + 256 len 32 * _79]
+    mem[mem[64]] = 32
+    mem[mem[64] + 32] = _79
+    mem[mem[64] + 64 len 32 * _79] = mem[(2 * ceil32(return_data.size)) + 256 len 32 * _79]
+    return Array(len=_79, data=mem[mem[64] + 64 len 32 * _79])
+}
+
+
+
+}
