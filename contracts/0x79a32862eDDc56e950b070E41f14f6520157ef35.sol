52dd283d8e2bac57bfe9708c7ed9938221db52ea
bot <abagoforgans@users.noreply.github.com>
2022-06-06 03:13:40 +0000

    autocommit

---
+++ contracts/0x79a32862eDDc56e950b070E41f14f6520157ef35.sol
@@ -0,0 +1,18 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+#
+#  - sub_88f824d1(?)
+#
+function _fallback() payable {
+    revert
+}
+
+
+
+}
---
+++ contracts/0x9D113D8B9C1a106eE20618D7d4beA67699AbBB4D.sol
@@ -0,0 +1,18 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+#
+#  - sub_75b24cfa(?)
+#
+function _fallback() payable {
+    revert
+}
+
+
+
+}
