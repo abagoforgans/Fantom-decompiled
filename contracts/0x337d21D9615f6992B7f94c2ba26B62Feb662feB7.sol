fbffec2bfef850a4d3cbad30c758c8de25f188ed
bot <abagoforgans@users.noreply.github.com>
2022-06-28 11:46:08 +0000

    autocommit

---
+++ contracts/0x337d21D9615f6992B7f94c2ba26B62Feb662feB7.sol
@@ -0,0 +1,103 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+#
+#  - sub_6b4d4dc1(?)
+#  - liquidate(address arg1, address arg2)
+#
+address stor3;
+uint32 stor4;
+address owner;
+uint256 stor4;
+
+function owner() {
+    return address(owner)
+}
+
+function _fallback() payable {
+    revert
+}
+
+function changeOwner(address arg1) {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    if address(owner) != msg.sender:
+        revert with 0, 'Not owner'
+    address(owner) = arg1
+}
+
+function sub_6d29e7fc(?) {
+    require calldata.size - 4 >= 128
+    require arg1 == address(arg1)
+    require arg2 == address(arg2)
+    require arg3 == arg3
+    require arg4 <= test266151307()
+    require arg4 + 35 < calldata.size
+    require arg4.length <= test266151307()
+    require arg4 + arg4.length + 36 <= calldata.size
+    revert with 0, 'Not implemented'
+}
+
+function skim(address arg1) {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    mem[100] = this.address
+    require ext_code.size(arg1)
+    staticcall arg1.balanceOf(address arg1) with:
+            gas gas_remaining wei
+           args this.address
+    mem[96] = ext_call.return_data[0]
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_call.return_data[0] == ext_call.return_data[0]
+    if not ext_call.return_data[0]:
+        return 0
+    if arg1 == stor3:
+        require ext_code.size(stor3)
+        call stor3.withdraw(uint256 arg1) with:
+             gas gas_remaining wei
+            args ext_call.return_data[0]
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+        call address(owner) with:
+           value ext_call.return_data[0] wei
+             gas gas_remaining wei
+        if not ext_call.success:
+            revert with 0, 'TransferHelper::safeTransferETH: ETH transfer failed'
+    else:
+        mem[ceil32(return_data.size) + 96] = 68
+        mem[ceil32(return_data.size) + 132 len 28] = Mask(224, 0, stor4)
+        mem[ceil32(return_data.size) + 128 len 4] = transfer(address arg1, uint256 arg2)
+        mem[ceil32(return_data.size) + 196 len 96] = transfer(address arg1, uint256 arg2), Mask(224, 0, stor4), uint32(stor4), ext_call.return_data[0], mem[ceil32(return_data.size) + 196 len 28]
+        call arg1.mem[ceil32(return_data.size) + 196 len 4] with:
+             gas gas_remaining wei
+            args mem[ceil32(return_data.size) + 200 len 64]
+        if not return_data.size:
+            if not ext_call.success:
+                revert with 0, 'TransferHelper::safeTransfer: transfer failed'
+            if ext_call.return_data[0]:
+                require ext_call.return_data[0] >= 32
+                require 0, mem[132 len 28] == bool(0, mem[132 len 28])
+                if not 0, mem[132 len 28]:
+                    revert with 0, 'TransferHelper::safeTransfer: transfer failed'
+        else:
+            mem[ceil32(return_data.size) + 228 len return_data.size] = ext_call.return_data[0 len return_data.size]
+            if not ext_call.success:
+                revert with 0, 'TransferHelper::safeTransfer: transfer failed'
+            if return_data.size:
+                require return_data.size >= 32
+                require mem[ceil32(return_data.size) + 228] == bool(mem[ceil32(return_data.size) + 228])
+                if not mem[ceil32(return_data.size) + 228]:
+                    revert with 0, 'TransferHelper::safeTransfer: transfer failed'
+    return ext_call.return_data[0]
+}
+
+
+
+}
