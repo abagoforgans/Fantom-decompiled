0ed8586ebbd418cbbe30ce6626abc9e9cf9e77f8
bot <abagoforgans@users.noreply.github.com>
2022-06-30 17:20:02 +0000

    autocommit

---
+++ contracts/0x469Cf0f7DC8281379C50656C7473029D02BD87E0.sol
@@ -0,0 +1,155 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+address owner;
+address stor1;
+mapping of uint256 fees;
+uint256 feeMultiplier;
+
+function owner() {
+    return owner
+}
+
+function feeMultiplier() {
+    return feeMultiplier
+}
+
+function fees(address arg1) {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    return fees[arg1]
+}
+
+function _fallback() payable {
+    revert
+}
+
+function renounceOwnership() {
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    owner = 0
+    emit OwnershipTransferred(owner, 0);
+}
+
+function setFeeMultiplier(uint256 arg1) {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    if arg1 > 100:
+        revert with 0, 'MULTIPLIER_OVERFLOW'
+    feeMultiplier = arg1
+}
+
+function transferOwnership(address arg1) {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'Ownable: new owner is the zero address'
+    owner = arg1
+    emit OwnershipTransferred(owner, arg1);
+}
+
+function withdrawFees(address arg1) {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    if fees[address(this.address)] < 0:
+        revert with 0, 'ZERO_FEE'
+    fees[address(this.address)] = 0
+    call arg1 with:
+       value fees[address(this.address)] wei
+         gas 2300 * is_zero(value) wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+}
+
+function sub_853ec87e(?) payable {
+    require calldata.size - 4 >= 32
+    require cd[4] <= test266151307()
+    require cd[4] + 35 < calldata.size
+    require ('cd', 4).length <= test266151307()
+    require cd[4] + (32 * ('cd', 4).length) + 36 <= calldata.size
+    idx = 0
+    while idx < ('cd', 4).length:
+        mem[96] = 0x9024944800000000000000000000000000000000000000000000000000000000
+        mem[100] = cd[((32 * idx) + cd[4] + 36)]
+        require ext_code.size(stor1)
+        call stor1.level_up(uint256 arg1) with:
+             gas gas_remaining wei
+            args cd[((32 * idx) + cd[4] + 36)]
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+        if idx == -1:
+            revert with 'NH{q', 17
+        idx = idx + 1
+        continue 
+    if gas_remaining < gas_remaining:
+        revert with 'NH{q', 17
+    if False and block.gasprice > 0:
+        revert with 'NH{q', 17
+    if False and feeMultiplier > 0:
+        revert with 'NH{q', 17
+    if msg.value <= 0:
+        revert with 0, 'INSUFFICIENT_FEE'
+    if fees[address(this.address)] > -1:
+        revert with 'NH{q', 17
+    if msg.value < 0:
+        revert with 'NH{q', 17
+    call msg.sender with:
+       value msg.value wei
+         gas 2300 * is_zero(value) wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+}
+
+function sub_3a38aeb1(?) payable {
+    require calldata.size - 4 >= 32
+    require cd[4] <= test266151307()
+    require cd[4] + 35 < calldata.size
+    require ('cd', 4).length <= test266151307()
+    require cd[4] + (32 * ('cd', 4).length) + 36 <= calldata.size
+    idx = 0
+    while idx < ('cd', 4).length:
+        mem[96] = 0xb00b52f100000000000000000000000000000000000000000000000000000000
+        mem[100] = cd[((32 * idx) + cd[4] + 36)]
+        require ext_code.size(stor1)
+        call stor1.adventure(uint256 arg1) with:
+             gas gas_remaining wei
+            args cd[((32 * idx) + cd[4] + 36)]
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+        if idx == -1:
+            revert with 'NH{q', 17
+        idx = idx + 1
+        continue 
+    if gas_remaining < gas_remaining:
+        revert with 'NH{q', 17
+    if False and block.gasprice > 0:
+        revert with 'NH{q', 17
+    if False and feeMultiplier > 0:
+        revert with 'NH{q', 17
+    if msg.value <= 0:
+        revert with 0, 'INSUFFICIENT_FEE'
+    if fees[address(this.address)] > -1:
+        revert with 'NH{q', 17
+    if msg.value < 0:
+        revert with 'NH{q', 17
+    call msg.sender with:
+       value msg.value wei
+         gas 2300 * is_zero(value) wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+}
+
+
+
+}
