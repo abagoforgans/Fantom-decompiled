6615b096a659f0b3d376c6b9d6f31fd8c07c3216
bot <abagoforgans@users.noreply.github.com>
2022-07-04 19:23:49 +0000

    autocommit

---
+++ contracts/0x52B39f97F3aa20c2876d8De9a3B178ad384FBa34.sol
@@ -0,0 +1,320 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+function _fallback() payable {
+    revert
+}
+
+function sub_95fd1252(?) payable {
+    require calldata.size - 4 >= 32
+    require cd[4] <= test266151307()
+    require cd[4] + 35 < calldata.size
+    require ('cd', 4).length <= test266151307()
+    require cd[4] + (32 * ('cd', 4).length) + 36 <= calldata.size
+    if ('cd', 4).length and 11 > -1 / ('cd', 4).length:
+        revert with 'NH{q', 17
+    if 11 * ('cd', 4).length > test266151307():
+        revert with 'NH{q', 65
+    mem[96] = 11 * ('cd', 4).length
+    mem[64] = (32 * 11 * ('cd', 4).length) + 128
+    if not 11 * ('cd', 4).length:
+        idx = 0
+        while idx < ('cd', 4).length:
+            if idx and 11 > -1 / idx:
+                revert with 'NH{q', 17
+            if 11 * idx >= mem[96]:
+                revert with 'NH{q', 50
+            mem[(32 * 11 * idx) + 128] = cd[((32 * idx) + cd[4] + 36)]
+            _138 = mem[64]
+            mem[mem[64]] = 4
+            if idx >= ('cd', 4).length:
+                revert with 'NH{q', 50
+            require ext_code.size(0xce761d788df608bd21bdd59d6f4b54b2e27f25bb)
+            staticcall 0xce761d788df608bd21bdd59d6f4b54b2e27f25bb.summoner(uint256 arg1) with:
+                    gas gas_remaining wei
+                   args cd[((32 * idx) + cd[4] + 36)]
+            mem[mem[64] + 160 len 128] = ext_call.return_data[0 len 128]
+            if not ext_call.success:
+                revert with ext_call.return_data[0 len return_data.size]
+            mem[64] = mem[64] + ceil32(return_data.size) + 160
+            require return_data.size >= 128
+            require ext_call.return_data[0] == ext_call.return_data[0]
+            require ext_call.return_data[32] == ext_call.return_data[32]
+            require ext_call.return_data[64] == ext_call.return_data[64]
+            require ext_call.return_data[96] == ext_call.return_data[96]
+            if 0 >= mem[_138]:
+                revert with 'NH{q', 50
+            if 1 >= mem[_138]:
+                revert with 'NH{q', 50
+            if 2 >= mem[_138]:
+                revert with 'NH{q', 50
+            if 3 >= mem[_138]:
+                revert with 'NH{q', 50
+            mem[_138 + 128] = ext_call.return_data[96]
+            mem[_138 + 96] = ext_call.return_data[64]
+            mem[_138 + 64] = ext_call.return_data[32]
+            mem[_138 + 32] = ext_call.return_data[0]
+            mem[_138 + ceil32(return_data.size) + 160] = 6
+            if idx >= ('cd', 4).length:
+                revert with 'NH{q', 50
+            require ext_code.size(0xb5f5af1087a8da62a23b08c00c6ec9af21f397a1)
+            staticcall 0xb5f5af1087a8da62a23b08c00c6ec9af21f397a1.ability_scores(uint256 arg1) with:
+                    gas gas_remaining wei
+                   args cd[((32 * idx) + cd[4] + 36)]
+            mem[_138 + ceil32(return_data.size) + 384 len 192] = ext_call.return_data[0 len 192]
+            if not ext_call.success:
+                revert with ext_call.return_data[0 len return_data.size]
+            mem[64] = _138 + (2 * ceil32(return_data.size)) + 384
+            require return_data.size >= 192
+            require ext_call.return_data[0] == ext_call.return_data[28 len 4]
+            require ext_call.return_data[32] == ext_call.return_data[60 len 4]
+            require ext_call.return_data[64] == ext_call.return_data[92 len 4]
+            require ext_call.return_data[96] == ext_call.return_data[124 len 4]
+            require ext_call.return_data[128] == ext_call.return_data[156 len 4]
+            require ext_call.return_data[160] == ext_call.return_data[188 len 4]
+            mem[_138 + ceil32(return_data.size) + 352] = ext_call.return_data[188 len 4]
+            mem[_138 + ceil32(return_data.size) + 320] = ext_call.return_data[156 len 4]
+            mem[_138 + ceil32(return_data.size) + 288] = ext_call.return_data[124 len 4]
+            mem[_138 + ceil32(return_data.size) + 256] = ext_call.return_data[92 len 4]
+            mem[_138 + ceil32(return_data.size) + 224] = ext_call.return_data[60 len 4]
+            mem[_138 + ceil32(return_data.size) + 192] = ext_call.return_data[28 len 4]
+            if 0 >= mem[_138]:
+                revert with 'NH{q', 50
+            if idx and 11 > -1 / idx:
+                revert with 'NH{q', 17
+            if 11 * idx > -2:
+                revert with 'NH{q', 17
+            if (11 * idx) + 1 >= mem[96]:
+                revert with 'NH{q', 50
+            mem[(32 * (11 * idx) + 1) + 128] = ext_call.return_data[0]
+            if 1 >= mem[_138]:
+                revert with 'NH{q', 50
+            if idx and 11 > -1 / idx:
+                revert with 'NH{q', 17
+            if 11 * idx > -3:
+                revert with 'NH{q', 17
+            if (11 * idx) + 2 >= mem[96]:
+                revert with 'NH{q', 50
+            mem[(32 * (11 * idx) + 2) + 128] = ext_call.return_data[32]
+            if 2 >= mem[_138]:
+                revert with 'NH{q', 50
+            if idx and 11 > -1 / idx:
+                revert with 'NH{q', 17
+            if 11 * idx > -4:
+                revert with 'NH{q', 17
+            if (11 * idx) + 3 >= mem[96]:
+                revert with 'NH{q', 50
+            mem[(32 * (11 * idx) + 3) + 128] = ext_call.return_data[64]
+            if 3 >= mem[_138]:
+                revert with 'NH{q', 50
+            if idx and 11 > -1 / idx:
+                revert with 'NH{q', 17
+            if 11 * idx > -5:
+                revert with 'NH{q', 17
+            if (11 * idx) + 4 >= mem[96]:
+                revert with 'NH{q', 50
+            mem[(32 * (11 * idx) + 4) + 128] = ext_call.return_data[96]
+            if idx and 11 > -1 / idx:
+                revert with 'NH{q', 17
+            if 11 * idx > -6:
+                revert with 'NH{q', 17
+            if (11 * idx) + 5 >= mem[96]:
+                revert with 'NH{q', 50
+            mem[(32 * (11 * idx) + 5) + 128] = uint32(ext_call.return_data[0])
+            if idx and 11 > -1 / idx:
+                revert with 'NH{q', 17
+            if 11 * idx > -7:
+                revert with 'NH{q', 17
+            if (11 * idx) + 6 >= mem[96]:
+                revert with 'NH{q', 50
+            mem[(32 * (11 * idx) + 6) + 128] = uint32(ext_call.return_data[32])
+            if idx and 11 > -1 / idx:
+                revert with 'NH{q', 17
+            if 11 * idx > -8:
+                revert with 'NH{q', 17
+            if (11 * idx) + 7 >= mem[96]:
+                revert with 'NH{q', 50
+            mem[(32 * (11 * idx) + 7) + 128] = uint32(ext_call.return_data[64])
+            if idx and 11 > -1 / idx:
+                revert with 'NH{q', 17
+            if 11 * idx > -9:
+                revert with 'NH{q', 17
+            if (11 * idx) + 8 >= mem[96]:
+                revert with 'NH{q', 50
+            mem[(32 * (11 * idx) + 8) + 128] = uint32(ext_call.return_data[96])
+            if idx and 11 > -1 / idx:
+                revert with 'NH{q', 17
+            if 11 * idx > -10:
+                revert with 'NH{q', 17
+            if (11 * idx) + 9 >= mem[96]:
+                revert with 'NH{q', 50
+            mem[(32 * (11 * idx) + 9) + 128] = uint32(ext_call.return_data[128])
+            if idx and 11 > -1 / idx:
+                revert with 'NH{q', 17
+            if 11 * idx > -11:
+                revert with 'NH{q', 17
+            if (11 * idx) + 10 >= mem[96]:
+                revert with 'NH{q', 50
+            mem[(32 * (11 * idx) + 10) + 128] = ext_call.return_data[188 len 4]
+            if idx == -1:
+                revert with 'NH{q', 17
+            idx = idx + 1
+            continue 
+    else:
+        mem[128 len 32 * 11 * ('cd', 4).length] = call.data[calldata.size len 32 * 11 * ('cd', 4).length]
+        idx = 0
+        while idx < ('cd', 4).length:
+            if idx and 11 > -1 / idx:
+                revert with 'NH{q', 17
+            if 11 * idx >= mem[96]:
+                revert with 'NH{q', 50
+            mem[(32 * 11 * idx) + 128] = cd[((32 * idx) + cd[4] + 36)]
+            _139 = mem[64]
+            mem[mem[64]] = 4
+            if idx >= ('cd', 4).length:
+                revert with 'NH{q', 50
+            require ext_code.size(0xce761d788df608bd21bdd59d6f4b54b2e27f25bb)
+            staticcall 0xce761d788df608bd21bdd59d6f4b54b2e27f25bb.summoner(uint256 arg1) with:
+                    gas gas_remaining wei
+                   args cd[((32 * idx) + cd[4] + 36)]
+            mem[mem[64] + 160 len 128] = ext_call.return_data[0 len 128]
+            if not ext_call.success:
+                revert with ext_call.return_data[0 len return_data.size]
+            mem[64] = mem[64] + ceil32(return_data.size) + 160
+            require return_data.size >= 128
+            require ext_call.return_data[0] == ext_call.return_data[0]
+            require ext_call.return_data[32] == ext_call.return_data[32]
+            require ext_call.return_data[64] == ext_call.return_data[64]
+            require ext_call.return_data[96] == ext_call.return_data[96]
+            if 0 >= mem[_139]:
+                revert with 'NH{q', 50
+            if 1 >= mem[_139]:
+                revert with 'NH{q', 50
+            if 2 >= mem[_139]:
+                revert with 'NH{q', 50
+            if 3 >= mem[_139]:
+                revert with 'NH{q', 50
+            mem[_139 + 128] = ext_call.return_data[96]
+            mem[_139 + 96] = ext_call.return_data[64]
+            mem[_139 + 64] = ext_call.return_data[32]
+            mem[_139 + 32] = ext_call.return_data[0]
+            mem[_139 + ceil32(return_data.size) + 160] = 6
+            if idx >= ('cd', 4).length:
+                revert with 'NH{q', 50
+            require ext_code.size(0xb5f5af1087a8da62a23b08c00c6ec9af21f397a1)
+            staticcall 0xb5f5af1087a8da62a23b08c00c6ec9af21f397a1.ability_scores(uint256 arg1) with:
+                    gas gas_remaining wei
+                   args cd[((32 * idx) + cd[4] + 36)]
+            mem[_139 + ceil32(return_data.size) + 384 len 192] = ext_call.return_data[0 len 192]
+            if not ext_call.success:
+                revert with ext_call.return_data[0 len return_data.size]
+            mem[64] = _139 + (2 * ceil32(return_data.size)) + 384
+            require return_data.size >= 192
+            require ext_call.return_data[0] == ext_call.return_data[28 len 4]
+            require ext_call.return_data[32] == ext_call.return_data[60 len 4]
+            require ext_call.return_data[64] == ext_call.return_data[92 len 4]
+            require ext_call.return_data[96] == ext_call.return_data[124 len 4]
+            require ext_call.return_data[128] == ext_call.return_data[156 len 4]
+            require ext_call.return_data[160] == ext_call.return_data[188 len 4]
+            mem[_139 + ceil32(return_data.size) + 352] = ext_call.return_data[188 len 4]
+            mem[_139 + ceil32(return_data.size) + 320] = ext_call.return_data[156 len 4]
+            mem[_139 + ceil32(return_data.size) + 288] = ext_call.return_data[124 len 4]
+            mem[_139 + ceil32(return_data.size) + 256] = ext_call.return_data[92 len 4]
+            mem[_139 + ceil32(return_data.size) + 224] = ext_call.return_data[60 len 4]
+            mem[_139 + ceil32(return_data.size) + 192] = ext_call.return_data[28 len 4]
+            if 0 >= mem[_139]:
+                revert with 'NH{q', 50
+            if idx and 11 > -1 / idx:
+                revert with 'NH{q', 17
+            if 11 * idx > -2:
+                revert with 'NH{q', 17
+            if (11 * idx) + 1 >= mem[96]:
+                revert with 'NH{q', 50
+            mem[(32 * (11 * idx) + 1) + 128] = ext_call.return_data[0]
+            if 1 >= mem[_139]:
+                revert with 'NH{q', 50
+            if idx and 11 > -1 / idx:
+                revert with 'NH{q', 17
+            if 11 * idx > -3:
+                revert with 'NH{q', 17
+            if (11 * idx) + 2 >= mem[96]:
+                revert with 'NH{q', 50
+            mem[(32 * (11 * idx) + 2) + 128] = ext_call.return_data[32]
+            if 2 >= mem[_139]:
+                revert with 'NH{q', 50
+            if idx and 11 > -1 / idx:
+                revert with 'NH{q', 17
+            if 11 * idx > -4:
+                revert with 'NH{q', 17
+            if (11 * idx) + 3 >= mem[96]:
+                revert with 'NH{q', 50
+            mem[(32 * (11 * idx) + 3) + 128] = ext_call.return_data[64]
+            if 3 >= mem[_139]:
+                revert with 'NH{q', 50
+            if idx and 11 > -1 / idx:
+                revert with 'NH{q', 17
+            if 11 * idx > -5:
+                revert with 'NH{q', 17
+            if (11 * idx) + 4 >= mem[96]:
+                revert with 'NH{q', 50
+            mem[(32 * (11 * idx) + 4) + 128] = ext_call.return_data[96]
+            if idx and 11 > -1 / idx:
+                revert with 'NH{q', 17
+            if 11 * idx > -6:
+                revert with 'NH{q', 17
+            if (11 * idx) + 5 >= mem[96]:
+                revert with 'NH{q', 50
+            mem[(32 * (11 * idx) + 5) + 128] = uint32(ext_call.return_data[0])
+            if idx and 11 > -1 / idx:
+                revert with 'NH{q', 17
+            if 11 * idx > -7:
+                revert with 'NH{q', 17
+            if (11 * idx) + 6 >= mem[96]:
+                revert with 'NH{q', 50
+            mem[(32 * (11 * idx) + 6) + 128] = uint32(ext_call.return_data[32])
+            if idx and 11 > -1 / idx:
+                revert with 'NH{q', 17
+            if 11 * idx > -8:
+                revert with 'NH{q', 17
+            if (11 * idx) + 7 >= mem[96]:
+                revert with 'NH{q', 50
+            mem[(32 * (11 * idx) + 7) + 128] = uint32(ext_call.return_data[64])
+            if idx and 11 > -1 / idx:
+                revert with 'NH{q', 17
+            if 11 * idx > -9:
+                revert with 'NH{q', 17
+            if (11 * idx) + 8 >= mem[96]:
+                revert with 'NH{q', 50
+            mem[(32 * (11 * idx) + 8) + 128] = uint32(ext_call.return_data[96])
+            if idx and 11 > -1 / idx:
+                revert with 'NH{q', 17
+            if 11 * idx > -10:
+                revert with 'NH{q', 17
+            if (11 * idx) + 9 >= mem[96]:
+                revert with 'NH{q', 50
+            mem[(32 * (11 * idx) + 9) + 128] = uint32(ext_call.return_data[128])
+            if idx and 11 > -1 / idx:
+                revert with 'NH{q', 17
+            if 11 * idx > -11:
+                revert with 'NH{q', 17
+            if (11 * idx) + 10 >= mem[96]:
+                revert with 'NH{q', 50
+            mem[(32 * (11 * idx) + 10) + 128] = ext_call.return_data[188 len 4]
+            if idx == -1:
+                revert with 'NH{q', 17
+            idx = idx + 1
+            continue 
+    mem[mem[64]] = 32
+    mem[mem[64] + 32] = mem[96]
+    mem[mem[64] + 64 len 32 * mem[96]] = mem[128 len 32 * mem[96]]
+    return 32, mem[mem[64] + 32 len (32 * mem[96]) + 32]
+}
+
+
+
+}
---
+++ contracts/0xBb0C978Beb37433BD13006D83bf1Ea8B8eBEaE9D.sol
@@ -0,0 +1,54 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+address owner;
+mapping of address stor1;
+array of uint256 sub_d04d399b;
+mapping of address stor99;
+
+function owner() payable {
+    return owner
+}
+
+function sub_d04d399b(?) payable {
+    return sub_d04d399b[arg1][arg2][0 len sub_d04d399b[arg1][arg2].length]
+}
+
+function _fallback() payable {
+    revert
+}
+
+function nameToAddress(string arg1) payable {
+    require calldata.size - 4 >= 32
+    require arg1 <= 4294967296
+    require arg1 + 36 <= calldata.size
+    require arg1.length <= 4294967296 and arg1 + arg1.length + 36 <= calldata.size
+    mem[ceil32(arg1.length) + 128] = stor1[arg1[all]]
+    return Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]
+}
+
+function add(string arg1, address arg2) payable {
+    require calldata.size - 4 >= 64
+    require arg1 <= 4294967296
+    require arg1 + 36 <= calldata.size
+    require arg1.length <= 4294967296 and arg1 + arg1.length + 36 <= calldata.size
+    mem[128 len arg1.length] = arg1[all]
+    mem[arg1.length + 128] = 0
+    mem[ceil32(arg1.length) + 128 len floor32(arg1.length)] = call.data[arg1 + 36 len floor32(arg1.length)]
+    mem[ceil32(arg1.length) + floor32(arg1.length) + -(arg1.length % 32) + 160 len arg1.length % 32] = mem[-(arg1.length % 32) + floor32(arg1.length) + 160 len arg1.length % 32]
+    mem[arg1.length + ceil32(arg1.length) + 128] = 1
+    stor[mem[arg1.length + 160 len ceil32(arg1.length)]][Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0)] = arg2
+    sub_d04d399b[address(arg2)]++
+    sub_d04d399b[address(arg2)][sub_d04d399b[address(arg2)]][] = Array(len=arg1.length, data=arg1[all])
+    mem[ceil32(arg1.length) + 128] = 0
+    return Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]
+}
+
+
+
+}
