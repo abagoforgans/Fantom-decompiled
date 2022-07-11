cbd27bbd519a8ffee8f14c1c701b0c09929edf70
bot <abagoforgans@users.noreply.github.com>
2022-07-11 19:01:00 +0000

    autocommit

---
+++ contracts/0xA12D5eF9A77874716b661147f219caB91f4B3185.sol
@@ -0,0 +1,86 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+uint256 totalShares;
+uint256 totalReleased;
+mapping of uint256 shares;
+mapping of uint256 released;
+array of address payee;
+
+function totalShares() {
+    return totalShares
+}
+
+function payee(uint256 arg1) {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    if arg1 >= payee.length:
+        revert with 'NH{q', 50
+    return payee[arg1]
+}
+
+function released(address arg1) {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    return released[address(arg1)]
+}
+
+function shares(address arg1) {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    return shares[address(arg1)]
+}
+
+function totalReleased() {
+    return totalReleased
+}
+
+function _fallback() payable {
+    revert
+}
+
+function release(address arg1) {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    if shares[address(arg1)] <= 0:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'PaymentSplitter: account has no shares'
+    if eth.balance(this.address) > -totalReleased - 1:
+        revert with 'NH{q', 17
+    if eth.balance(this.address) + totalReleased and shares[address(arg1)] > -1 / eth.balance(this.address) + totalReleased:
+        revert with 'NH{q', 17
+    if not totalShares:
+        revert with 'NH{q', 18
+    if (eth.balance(this.address) * shares[address(arg1)]) + (totalReleased * shares[address(arg1)]) / totalShares < released[address(arg1)]:
+        revert with 'NH{q', 17
+    if not ((eth.balance(this.address) * shares[address(arg1)]) + (totalReleased * shares[address(arg1)]) / totalShares) - released[address(arg1)]:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'PaymentSplitter: account is not due payment'
+    if released[address(arg1)] > -((eth.balance(this.address) * shares[address(arg1)]) + (totalReleased * shares[address(arg1)]) / totalShares) + released[address(arg1)] - 1:
+        revert with 'NH{q', 17
+    released[address(arg1)] = (eth.balance(this.address) * shares[address(arg1)]) + (totalReleased * shares[address(arg1)]) / totalShares
+    if totalReleased > -((eth.balance(this.address) * shares[address(arg1)]) + (totalReleased * shares[address(arg1)]) / totalShares) + released[address(arg1)] - 1:
+        revert with 'NH{q', 17
+    totalReleased = totalReleased + ((eth.balance(this.address) * shares[address(arg1)]) + (totalReleased * shares[address(arg1)]) / totalShares) - released[address(arg1)]
+    if eth.balance(this.address) < ((eth.balance(this.address) * shares[address(arg1)]) + (totalReleased * shares[address(arg1)]) / totalShares) - released[address(arg1)]:
+        revert with 0, 'Address: insufficient balance'
+    call arg1 with:
+       value ((eth.balance(this.address) * shares[address(arg1)]) + (totalReleased * shares[address(arg1)]) / totalShares) - released[address(arg1)] wei
+         gas gas_remaining wei
+    if not return_data.size:
+        if not ext_call.success:
+            revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                        'Address: unable to send value, recipient may have reverted'
+    else:
+        if not ext_call.success:
+            revert with 0, 'Address: unable to send value, recipient may have reverted'
+    ('bool', 'ext_call.success')
+    emit PaymentReleased(address(arg1), ((eth.balance(this.address) * shares[address(arg1)]) + (totalReleased * shares[address(arg1)]) / totalShares) - released[address(arg1)]);
+}
+
+
+
+}
