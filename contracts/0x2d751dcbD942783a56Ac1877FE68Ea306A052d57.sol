a051e8e9418843316861792ea586fa060d647efd
bot <abagoforgans@users.noreply.github.com>
2022-07-13 15:22:27 +0000

    autocommit

---
+++ contracts/0x2d751dcbD942783a56Ac1877FE68Ea306A052d57.sol
@@ -0,0 +1,447 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+address owner;
+
+function owner() {
+    return owner
+}
+
+function _fallback() payable {
+    revert
+}
+
+function renounceOwnership() {
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    owner = 0
+    emit OwnershipTransferred(owner, 0);
+}
+
+function withdrawFees(address arg1) {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    call arg1 with:
+       value eth.balance(this.address) wei
+         gas 2300 * is_zero(value) wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+}
+
+function transferOwnership(address arg1) {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'Ownable: new owner is the zero address'
+    owner = arg1
+    emit OwnershipTransferred(owner, arg1);
+}
+
+function summon(uint256 arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    require arg1 == arg1
+    require arg2 == arg2
+    idx = 0
+    while idx < arg2:
+        mem[96] = 0x35d9f2a00000000000000000000000000000000000000000000000000000000
+        mem[100] = arg1
+        require ext_code.size(0xce761d788df608bd21bdd59d6f4b54b2e27f25bb)
+        call 0xce761d788df608bd21bdd59d6f4b54b2e27f25bb.summon(uint256 arg1) with:
+             gas gas_remaining wei
+            args arg1
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+        if idx == -1:
+            revert with 'NH{q', 17
+        idx = idx + 1
+        continue 
+}
+
+function sub_13217f0e(?) payable {
+    require calldata.size - 4 >= 32
+    require cd[4] <= test266151307()
+    require cd[4] + 35 < calldata.size
+    require ('cd', 4).length <= test266151307()
+    require cd[4] + (32 * ('cd', 4).length) + 36 <= calldata.size
+    idx = 0
+    while idx < ('cd', 4).length:
+        mem[96] = 0x9024944800000000000000000000000000000000000000000000000000000000
+        mem[100] = cd[((32 * idx) + cd[4] + 36)]
+        require ext_code.size(0xce761d788df608bd21bdd59d6f4b54b2e27f25bb)
+        call 0xce761d788df608bd21bdd59d6f4b54b2e27f25bb.level_up(uint256 arg1) with:
+             gas gas_remaining wei
+            args cd[((32 * idx) + cd[4] + 36)]
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+        if idx == -1:
+            revert with 'NH{q', 17
+        idx = idx + 1
+        continue 
+}
+
+function sub_3a091650(?) payable {
+    require calldata.size - 4 >= 32
+    require cd[4] <= test266151307()
+    require cd[4] + 35 < calldata.size
+    require ('cd', 4).length <= test266151307()
+    require cd[4] + (32 * ('cd', 4).length) + 36 <= calldata.size
+    idx = 0
+    while idx < ('cd', 4).length:
+        mem[96] = 0xb00b52f100000000000000000000000000000000000000000000000000000000
+        mem[100] = cd[((32 * idx) + cd[4] + 36)]
+        require ext_code.size(0xce761d788df608bd21bdd59d6f4b54b2e27f25bb)
+        call 0xce761d788df608bd21bdd59d6f4b54b2e27f25bb.adventure(uint256 arg1) with:
+             gas gas_remaining wei
+            args cd[((32 * idx) + cd[4] + 36)]
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+        if idx == -1:
+            revert with 'NH{q', 17
+        idx = idx + 1
+        continue 
+}
+
+function sub_33dc76f9(?) payable {
+    require calldata.size - 4 >= 32
+    require cd[4] <= test266151307()
+    require cd[4] + 35 < calldata.size
+    require ('cd', 4).length <= test266151307()
+    require cd[4] + (32 * ('cd', 4).length) + 36 <= calldata.size
+    idx = 0
+    while idx < ('cd', 4).length:
+        mem[96] = 0x95ea7b300000000000000000000000000000000000000000000000000000000
+        mem[100] = this.address
+        mem[132] = cd[((32 * idx) + cd[4] + 36)]
+        require ext_code.size(0xce761d788df608bd21bdd59d6f4b54b2e27f25bb)
+        call 0xce761d788df608bd21bdd59d6f4b54b2e27f25bb.approve(address arg1, uint256 arg2) with:
+             gas gas_remaining wei
+            args address(this.address), cd[((32 * idx) + cd[4] + 36)]
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+        if idx == -1:
+            revert with 'NH{q', 17
+        idx = idx + 1
+        continue 
+}
+
+function sub_6ba5d979(?) payable {
+    require calldata.size - 4 >= 64
+    require cd[4] <= test266151307()
+    require cd[4] + 35 < calldata.size
+    require ('cd', 4).length <= test266151307()
+    require cd[4] + (32 * ('cd', 4).length) + 36 <= calldata.size
+    require cd[36] <= test266151307()
+    require cd[36] + 35 < calldata.size
+    require ('cd', 36).length <= test266151307()
+    require cd[36] + (32 * ('cd', 36).length) + 36 <= calldata.size
+    idx = 0
+    while idx < ('cd', 36).length:
+        mem[96] = 0x95ea7b300000000000000000000000000000000000000000000000000000000
+        mem[100] = this.address
+        mem[132] = cd[((32 * idx) + cd[36] + 36)]
+        require ext_code.size(0xce761d788df608bd21bdd59d6f4b54b2e27f25bb)
+        call 0xce761d788df608bd21bdd59d6f4b54b2e27f25bb.approve(address arg1, uint256 arg2) with:
+             gas gas_remaining wei
+            args address(this.address), cd[((32 * idx) + cd[36] + 36)]
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+        if idx == -1:
+            revert with 'NH{q', 17
+        idx = idx + 1
+        continue 
+    idx = 0
+    while idx < ('cd', 4).length:
+        mem[96] = 0x379607f500000000000000000000000000000000000000000000000000000000
+        mem[100] = cd[((32 * idx) + cd[4] + 36)]
+        require ext_code.size(0x2069b76afe6b734fb65d1d099e7ec64ee9cc76b2)
+        call 0x2069b76afe6b734fb65d1d099e7ec64ee9cc76b2.claim(uint256 arg1) with:
+             gas gas_remaining wei
+            args cd[((32 * idx) + cd[4] + 36)]
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+        if idx == -1:
+            revert with 'NH{q', 17
+        idx = idx + 1
+        continue 
+}
+
+function sub_be606afd(?) payable {
+    require calldata.size - 4 >= 64
+    require cd[4] <= test266151307()
+    require cd[4] + 35 < calldata.size
+    require ('cd', 4).length <= test266151307()
+    require cd[4] + (32 * ('cd', 4).length) + 36 <= calldata.size
+    require cd[36] <= test266151307()
+    require cd[36] + 35 < calldata.size
+    require ('cd', 36).length <= test266151307()
+    require cd[36] + (32 * ('cd', 36).length) + 36 <= calldata.size
+    idx = 0
+    while idx < ('cd', 36).length:
+        mem[96] = 0x95ea7b300000000000000000000000000000000000000000000000000000000
+        mem[100] = this.address
+        mem[132] = cd[((32 * idx) + cd[36] + 36)]
+        require ext_code.size(0xce761d788df608bd21bdd59d6f4b54b2e27f25bb)
+        call 0xce761d788df608bd21bdd59d6f4b54b2e27f25bb.approve(address arg1, uint256 arg2) with:
+             gas gas_remaining wei
+            args address(this.address), cd[((32 * idx) + cd[36] + 36)]
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+        if idx == -1:
+            revert with 'NH{q', 17
+        idx = idx + 1
+        continue 
+    idx = 0
+    while idx < ('cd', 4).length:
+        mem[96] = 0xb00b52f100000000000000000000000000000000000000000000000000000000
+        mem[100] = cd[((32 * idx) + cd[4] + 36)]
+        require ext_code.size(0x2a0f1cb17680161cf255348ddfdee94ea8ca196a)
+        call 0x2a0f1cb17680161cf255348ddfdee94ea8ca196a.adventure(uint256 arg1) with:
+             gas gas_remaining wei
+            args cd[((32 * idx) + cd[4] + 36)]
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+        if idx == -1:
+            revert with 'NH{q', 17
+        idx = idx + 1
+        continue 
+}
+
+function sub_da9c6c0a(?) payable {
+    mem[64] = 96
+    require calldata.size - 4 >= 96
+    require cd[4] <= test266151307()
+    require cd[4] + 35 < calldata.size
+    require ('cd', 4).length <= test266151307()
+    require cd[4] + (32 * ('cd', 4).length) + 36 <= calldata.size
+    require cd[36] <= test266151307()
+    require cd[36] + 35 < calldata.size
+    require ('cd', 36).length <= test266151307()
+    require cd[36] + (32 * ('cd', 36).length) + 36 <= calldata.size
+    require cd[68] <= test266151307()
+    require cd[68] + 35 < calldata.size
+    require ('cd', 68).length <= test266151307()
+    require cd[68] + (1152 * ('cd', 68).length) + 36 <= calldata.size
+    idx = 0
+    while idx < ('cd', 36).length:
+        mem[96] = 0x95ea7b300000000000000000000000000000000000000000000000000000000
+        mem[100] = this.address
+        mem[132] = cd[((32 * idx) + cd[36] + 36)]
+        require ext_code.size(0xce761d788df608bd21bdd59d6f4b54b2e27f25bb)
+        call 0xce761d788df608bd21bdd59d6f4b54b2e27f25bb.approve(address arg1, uint256 arg2) with:
+             gas gas_remaining wei
+            args address(this.address), cd[((32 * idx) + cd[36] + 36)]
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+        if idx == -1:
+            revert with 'NH{q', 17
+        idx = idx + 1
+        continue 
+    idx = 0
+    while idx < ('cd', 4).length:
+        if idx >= ('cd', 68).length:
+            revert with 'NH{q', 50
+        mem[mem[64]] = 0x54d3652a00000000000000000000000000000000000000000000000000000000
+        mem[mem[64] + 4] = cd[((32 * idx) + cd[4] + 36)]
+        s = 0
+        t = (1152 * idx) + cd[68] + 36
+        u = mem[64] + 36
+        while s < 36:
+            require cd[t] == uint8(cd[t])
+            mem[u] = uint8(cd[t])
+            s = s + 1
+            t = t + 32
+            u = u + 32
+            continue 
+        require ext_code.size(0x51c0b29a1d84611373ba301706c6b4b72283c80f)
+        call 0x51c0b29a1d84611373ba301706c6b4b72283c80f.0x54d3652a with:
+             gas gas_remaining wei
+            args cd[((32 * idx) + cd[4] + 36)], mem[mem[64] + 36 len 1152]
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+        if idx == -1:
+            revert with 'NH{q', 17
+        idx = idx + 1
+        continue 
+}
+
+function sub_e708c494(?) {
+    require calldata.size - 4 >= 32
+    require cd[4] <= test266151307()
+    require cd[4] + 35 < calldata.size
+    require ('cd', 4).length <= test266151307()
+    require cd[4] + (32 * ('cd', 4).length) + 36 <= calldata.size
+    if ('cd', 4).length > test266151307():
+        revert with 'NH{q', 65
+    mem[96] = ('cd', 4).length
+    mem[64] = (32 * ('cd', 4).length) + 128
+    if not ('cd', 4).length:
+        idx = 0
+        while idx < ('cd', 4).length:
+            mem[mem[64] + 4] = cd[((32 * idx) + cd[4] + 36)]
+            require ext_code.size(0xce761d788df608bd21bdd59d6f4b54b2e27f25bb)
+            staticcall 0xce761d788df608bd21bdd59d6f4b54b2e27f25bb.getApproved(uint256 arg1) with:
+                    gas gas_remaining wei
+                   args cd[((32 * idx) + cd[4] + 36)]
+            mem[mem[64]] = ext_call.return_data[0]
+            if not ext_call.success:
+                revert with ext_call.return_data[0 len return_data.size]
+            _28 = mem[64]
+            mem[64] = mem[64] + ceil32(return_data.size)
+            require return_data.size >= 32
+            require mem[_28] == mem[_28 + 12 len 20]
+            if idx >= mem[96]:
+                revert with 'NH{q', 50
+            mem[(32 * idx) + 128] = mem[_28 + 12 len 20] == this.address
+            if idx == -1:
+                revert with 'NH{q', 17
+            idx = idx + 1
+            continue 
+        _20 = mem[64]
+        mem[mem[64]] = 32
+        _24 = mem[96]
+        mem[mem[64] + 32] = mem[96]
+        idx = 0
+        s = 128
+        t = mem[64] + 64
+        while idx < _24:
+            mem[t] = bool(mem[s])
+            idx = idx + 1
+            s = s + 32
+            t = t + 32
+            continue 
+        return memory
+          from mem[64]
+           len _20 + (32 * _24) + -mem[64] + 64
+    mem[128 len 32 * ('cd', 4).length] = call.data[calldata.size len 32 * ('cd', 4).length]
+    idx = 0
+    while idx < ('cd', 4).length:
+        mem[mem[64] + 4] = cd[((32 * idx) + cd[4] + 36)]
+        require ext_code.size(0xce761d788df608bd21bdd59d6f4b54b2e27f25bb)
+        staticcall 0xce761d788df608bd21bdd59d6f4b54b2e27f25bb.getApproved(uint256 arg1) with:
+                gas gas_remaining wei
+               args cd[((32 * idx) + cd[4] + 36)]
+        mem[mem[64]] = ext_call.return_data[0]
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+        _29 = mem[64]
+        mem[64] = mem[64] + ceil32(return_data.size)
+        require return_data.size >= 32
+        require mem[_29] == mem[_29 + 12 len 20]
+        if idx >= mem[96]:
+            revert with 'NH{q', 50
+        mem[(32 * idx) + 128] = mem[_29 + 12 len 20] == this.address
+        if idx == -1:
+            revert with 'NH{q', 17
+        idx = idx + 1
+        continue 
+    _22 = mem[64]
+    mem[mem[64]] = 32
+    _25 = mem[96]
+    mem[mem[64] + 32] = mem[96]
+    idx = 0
+    s = 128
+    t = mem[64] + 64
+    while idx < mem[96]:
+        mem[t] = bool(mem[s])
+        idx = idx + 1
+        s = s + 32
+        t = t + 32
+        continue 
+    return memory
+      from mem[64]
+       len _22 + (32 * _25) + -mem[64] + 64
+}
+
+function sub_9c0327dc(?) payable {
+    require calldata.size - 4 >= 96
+    require cd[4] <= test266151307()
+    require cd[4] + 35 < calldata.size
+    require ('cd', 4).length <= test266151307()
+    require cd[4] + (32 * ('cd', 4).length) + 36 <= calldata.size
+    require cd[36] <= test266151307()
+    require cd[36] + 35 < calldata.size
+    require ('cd', 36).length <= test266151307()
+    require cd[36] + (32 * ('cd', 36).length) + 36 <= calldata.size
+    require cd[68] <= test266151307()
+    require cd[68] + 35 < calldata.size
+    require ('cd', 68).length <= test266151307()
+    require cd[68] + (192 * ('cd', 68).length) + 36 <= calldata.size
+    idx = 0
+    while idx < ('cd', 36).length:
+        mem[96] = 0x95ea7b300000000000000000000000000000000000000000000000000000000
+        mem[100] = this.address
+        mem[132] = cd[((32 * idx) + cd[36] + 36)]
+        require ext_code.size(0xce761d788df608bd21bdd59d6f4b54b2e27f25bb)
+        call 0xce761d788df608bd21bdd59d6f4b54b2e27f25bb.approve(address arg1, uint256 arg2) with:
+             gas gas_remaining wei
+            args address(this.address), cd[((32 * idx) + cd[36] + 36)]
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+        if idx == -1:
+            revert with 'NH{q', 17
+        idx = idx + 1
+        continue 
+    idx = 0
+    while idx < ('cd', 4).length:
+        if 0 >= ('cd', 68).length:
+            revert with 'NH{q', 50
+        if idx >= 6:
+            revert with 'NH{q', 50
+        require cd[((32 * idx) + cd[68] + 36)] == uint32(cd[((32 * idx) + cd[68] + 36)])
+        if 1 >= ('cd', 68).length:
+            revert with 'NH{q', 50
+        if idx >= 6:
+            revert with 'NH{q', 50
+        require cd[((32 * idx) + cd[68] + 228)] == uint32(cd[((32 * idx) + cd[68] + 228)])
+        if 2 >= ('cd', 68).length:
+            revert with 'NH{q', 50
+        if idx >= 6:
+            revert with 'NH{q', 50
+        require cd[((32 * idx) + cd[68] + 420)] == uint32(cd[((32 * idx) + cd[68] + 420)])
+        if 3 >= ('cd', 68).length:
+            revert with 'NH{q', 50
+        if idx >= 6:
+            revert with 'NH{q', 50
+        require cd[((32 * idx) + cd[68] + 612)] == uint32(cd[((32 * idx) + cd[68] + 612)])
+        if 4 >= ('cd', 68).length:
+            revert with 'NH{q', 50
+        if idx >= 6:
+            revert with 'NH{q', 50
+        require cd[((32 * idx) + cd[68] + 804)] == uint32(cd[((32 * idx) + cd[68] + 804)])
+        if 5 >= ('cd', 68).length:
+            revert with 'NH{q', 50
+        if idx >= 6:
+            revert with 'NH{q', 50
+        require cd[((32 * idx) + cd[68] + 996)] == uint32(cd[((32 * idx) + cd[68] + 996)])
+        mem[96] = 0xc3c2407c00000000000000000000000000000000000000000000000000000000
+        mem[100] = cd[((32 * idx) + cd[4] + 36)]
+        mem[132] = uint32(cd[((32 * idx) + cd[68] + 36)])
+        mem[164] = uint32(cd[((32 * idx) + cd[68] + 228)])
+        mem[196] = uint32(cd[((32 * idx) + cd[68] + 420)])
+        mem[228] = uint32(cd[((32 * idx) + cd[68] + 612)])
+        mem[260] = uint32(cd[((32 * idx) + cd[68] + 804)])
+        mem[292] = uint32(cd[((32 * idx) + cd[68] + 996)])
+        require ext_code.size(0xb5f5af1087a8da62a23b08c00c6ec9af21f397a1)
+        call 0xb5f5af1087a8da62a23b08c00c6ec9af21f397a1.point_buy(uint256 arg1, uint32 arg2, uint32 arg3, uint32 arg4, uint32 arg5, uint32 arg6, uint32 arg7) with:
+             gas gas_remaining wei
+            args cd[((32 * idx) + cd[4] + 36)], cd[((32 * idx) + cd[68] + 36)] << 224, cd[((32 * idx) + cd[68] + 228)] << 224, cd[((32 * idx) + cd[68] + 420)] << 224, cd[((32 * idx) + cd[68] + 612)] << 224, cd[((32 * idx) + cd[68] + 804)] << 224, uint32(cd[((32 * idx) + cd[68] + 996)])
+        if not ext_call.success:
+            revert with ext_call.return_data[0 len return_data.size]
+        if idx == -1:
+            revert with 'NH{q', 17
+        idx = idx + 1
+        continue 
+}
+
+
+
+}
